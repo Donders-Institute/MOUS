6c709d9a2d5681b7b094f316d3d157efee8dd49a
Nietzsche Ho Lok Lam <nielam@mentat289.dccn.nl>
2012-06-07 11:59:23 +0200

    this file can now read TFR or tfr

--- meg/analysis_functions/mous_db_getfilename.m
+++ meg/analysis_functions/mous_db_getfilename.m
@@ -162,7 +162,7 @@ switch type{2}
           suff = [suff type{k} , '_'];
         end
         suff = suff(2:end-2);
-        if ~isempty(strfind(suff, 'tfr'))
+        if (~isempty(strfind(suff, 'tfr'))) || (~isempty(strfind(suff, 'TFR')))
           D = [D 'TFR/'];
         elseif ~isempty(strfind(suff, 'ERF'))
           D = [D 'ERF/'];
