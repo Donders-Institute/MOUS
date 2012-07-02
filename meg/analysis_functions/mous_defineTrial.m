4dd20b0bdbb7a07a82fa7b832796318aefae9706
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2012-07-02 11:44:31 +0200

    some formatting changes

--- meg/analysis_functions/mous_defineTrial.m
+++ meg/analysis_functions/mous_defineTrial.m
@@ -1,11 +1,9 @@
-
 function [trl] = mous_defineTrial(filename, prestim, poststim)
 
-
-cfg = [];
+cfg                   = [];
 cfg.dataset           = filename;
-cfg.trialdef.prestim  = prestim;              % baseline
-cfg.trialdef.poststim = poststim-1./1200;      % pad
-cfg.trialfun          = 'visual_word';    % 1s duration from onset of target word
-[cfg]                 = ft_definetrial(cfg);      % ft_definetrial defines trials which are created in cfg.trl which is a parameter for ft_preprocessing
+cfg.trialdef.prestim  = prestim;             % baseline
+cfg.trialdef.poststim = poststim-1./1200;    % pad FIXME the subtraction of 1./1200 assumes the sampling frequency to always be 1200 Hz
+cfg.trialfun          = 'visual_word';       % 1s duration from onset of target word
+cfg                   = ft_definetrial(cfg); % ft_definetrial defines trials which are created in cfg.trl which is a parameter for ft_preprocessing
 trl                   = cfg.trl;
