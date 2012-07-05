e259a25f36128da94a9c904149fad70d5b5c2957
Julia Udden <juludd@mentat304.dccn.nl>
2012-07-05 17:06:16 +0200

    Erase dummy scans before Preprocessing

---
+++ mri/mous_eraseDummies.m
@@ -0,0 +1,20 @@
+function [] = mous_eraseDummies()
+
+%--------------------------------------------------------------------------
+% ERASE DUMMY SCANS FROM PREPROC SUBJECT FOLDERS BEFORE PREPROCESSING
+%--------------------------------------------------------------------------
+
+cd('/home/language/juludd/MOUS/preprocdata/');
+infoPreproc;
+
+    for sbjnr=1:length(info.subjects)
+          cd(char(strcat(info.rootdir,filesep,info.subjects(sbjnr), ...
+              filesep,info.fctdir)));
+          
+          delete(char(strcat('fct-',info.subjects(sbjnr),'-001.nii')))
+          delete(char(strcat('fct-',info.subjects(sbjnr),'-002.nii')))
+          delete(char(strcat('fct-',info.subjects(sbjnr),'-003.nii')))
+   
+    end
+
+end
