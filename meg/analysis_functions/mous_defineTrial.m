7a5110f16d8e2f93f2647138a566ffb09b63ec72
Nietzsche Ho Lok Lam <nielam@mentat301.dccn.nl>
2012-07-05 13:07:53 +0200

    if-loop added to account for the fact that in some of the earlier stimuli set that the target was the ultimate or penultimate word (accounting for the last pair of triggers not having a word)because the stimuli had errors (was truncated too early).

--- meg/analysis_functions/mous_defineTrial.m
+++ meg/analysis_functions/mous_defineTrial.m
@@ -1,7 +1,7 @@
 function [trl] = mous_defineTrial(filename, prestim, poststim, wordType)
 
 cfg                   = [];
-cfg.dataset           = filename;
+cfg.dataset           = filename{1};
 cfg.trialdef.prestim  = prestim;              % baseline
 cfg.trialdef.poststim = poststim-1./1200;     % pad
 cfg.trialfun          = 'visual_word';        % 1s duration from onset of target word
@@ -11,17 +11,24 @@ trl                   = cfg.trl;
 if (strcmp(wordType,'target') > 0)           % focus on the target words
     sel = mod(trl(:,5),2)==0; 
     trl = trl(sel,:);
+    
 elseif (strcmp(wordType,'tarplusOne') > 0)   % focus on the 1st word after the target
     sel_tar = mod(trl(:,5),2)==0;            % all triggers, 1 = target
+    if sel_tar(end) == 1                     % if ultimate word is a target, then remove the last target's index. 
+        sel_tar = sel_tar(1:end-1);          % Otherwise trl(sel_tar,4) will not be the same size as trl(tarOne_tmp,4)
+    end
     tarOne_tmp = [false;sel_tar(1:end-1)];   % all triggers, 1 = tar+1 
-    sel_tarOne = find(tarOne_tmp);           % index position of all tar+1
+    sel_tarOne = find(tarOne_tmp);           % index position of all tar+1 
     sel_tarOne = sel_tarOne(trl(sel_tar,4)==trl(tarOne_tmp,4));  % trial of tar+1 = trial of target % ORIG4;.js
     trl        = trl(sel_tarOne,:);
     
 elseif (strcmp(wordType,'tarplusTwo') > 0)   % focus on the 2nd word after the target
     sel_tar = mod(trl(:,5),2)==0;
+    if sel_tar(end) == 1                     % if penultimate is a target, then remove the last target's index. 
+        sel_tar = sel_tar(1:end-2);          % Otherwise trl(sel_tar,4) will not be the same size as trl(tarOne_tmp,4)
+    end    
     tarTwo_tmp = [false; false; sel_tar(1:end-2)];  
-    sel_tarTwo = find(tarTwo_tmp);
-    sel_tarTwo = sel_tarTwo(trl(sel_tar,4)==trl(tarTwo_tmp,4));
-    trl      = trl(sel_tarTwo,:);
+    sel_tarTwo = find(tarTwo_tmp);    
+    sel_tarTwo = sel_tarTwo(trl(sel_tar,4)==trl(tarTwo_tmp,4)); % doesn't work if target is penultimate word in sentence 
+    trl      = trl(sel_tarTwo,:);                               % accounting that the last X-15 pair does not actually have a word.
 end 
