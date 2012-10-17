79cf9833478d8a4925e8f08c38a3689bbbd12c20
Nietzsche Ho Lok Lam <nielam@mentat319.dccn.nl>
2012-10-17 16:54:47 +0200

    updated to choose to have all words (sentence) or one word trialfun is now an additional argin to mous_defineTrial.m

--- meg/analysis_scripts/mous_preprocessing_pipeline.m
+++ meg/analysis_scripts/mous_preprocessing_pipeline.m
@@ -1,12 +1,9 @@
-% Pipeline to run all preprocessing stages for the TFR and ERFs
+function mous_preprocessing_pipeline(subjectname)% Pipeline to run all preprocessing stages for the TFR and ERFs
 % Annika 1.6 2012 | edited 2.7.2012 NL
 
 % addpath('/home/common/matlab/fieldtrip/qsub');
 % clear all
 
-function mous_preprocessing_pipeline(subjectname)
-
-  
     % subjectname = subjlist{n};    %  comment out when running qsub
     fprintf('Preprocessing subject %s  \n', subjectname);
     mous_db_makesubjdir(subjectname)
@@ -17,13 +14,19 @@ function mous_preprocessing_pipeline(subjectname)
     % get the description of the artifacts
     tmp = mous_db_getdata(subjectname, 'meg_artifactcfg');
     
-    wordType = 'target'; % 'tarplusOne'; 'tarplusTwo';
+    wordType = 'all';   % entire sentence / sequence
+    % other options:
+        % 'target'; 
+        % 'tarplusOne'; 'tarplusTwo';
+               
+    trialfun = 'visual_sentence';
+              % visual_word
     
     %% TFR %%%%%%%%%%%%%%%%%%%%%
      
     % define trial window (includes pre -500ms and post 3s)
     
-    [trl] = mous_defineTrial(filename{1}, 0.5, 3.0, wordType); 
+    [trl] = mous_defineTrial(filename{1}, 0.5, 3.0, wordType, trialfun); 
 
     % remove the artifacts that have been defined/detected
     [trl] = mous_artifact_remove(trl, filename{1}, tmp);
@@ -82,5 +85,3 @@ function mous_preprocessing_pipeline(subjectname)
     elseif (strcmp(wordType,'tarplusTwo') > 0) 
         mous_db_putdata(subjectname, 'meg_processed_{rawERF_tarplusTwo_02-1ds}', data);
     end
-
-% end % comment out when running qsub
