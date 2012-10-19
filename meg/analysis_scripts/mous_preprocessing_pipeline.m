45e0f37534bdd7d474f4fce2d2d215125b423253
Annika Hulten <annika.hulten@mpi.nl>
2012-10-19 16:53:13 +0200

    optimizesd saving part and fixed some bugs

--- meg/analysis_scripts/mous_preprocessing_pipeline.m
+++ meg/analysis_scripts/mous_preprocessing_pipeline.m
@@ -14,38 +14,36 @@ function mous_preprocessing_pipeline(subjectname)% Pipeline to run all preproces
     % get the description of the artifacts
     tmp = mous_db_getdata(subjectname, 'meg_artifactcfg');
     
-    wordType = 'all';   % entire sentence / sequence
-    % other options:
-        % 'target'; 
-        % 'tarplusOne'; 'tarplusTwo';
+    wordType = 'all';   % all words in a  sentence / sequence
+        % other options:
+        % 'target'; 'tarplusOne'; 'tarplusTwo';
+        % to be implemented: 'nouns'; 'verbs' ; firstWord etc
                
-    trialfun = 'visual_sentence';
-              % visual_word
+    % the trial funs need to be renamed
+    trialfun =  'visual_word';       % does not include fixation
+    %           'visual_sentence';  %includes onset of fixation cross
+         
+   % FIXME
+   % defining the pre and post stimulus windows here (instead of at multple
+   % places below
     
-    %% TFR %%%%%%%%%%%%%%%%%%%%%
-     
     % define trial window (includes pre -500ms and post 3s)
-    
     [trl] = mous_defineTrial(filename{1}, 0.5, 3.0, wordType, trialfun); 
 
     % remove the artifacts that have been defined/detected
     [trl] = mous_artifact_remove(trl, filename{1}, tmp);
+   
+              
+    %% TFR %%%%%%%%%%%%%%%%%%%%%     
        
     % PREPROCESS data 
-    % (filename = subject, trl = data, 300 = downsample target frequency,
+    % filename = subject, trl = data, 300 = downsample target frequency,
 
     data = mous_preprocessing(filename{1}, trl, 300, 'TFR');
   
-    %save preprocessed data
-    mous_db_putdata(subjectname, 'meg_processed_{rawTFR05-3ds}', data);
-    if (strcmp(wordType,'target') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawTFR_targetword_05-3ds}', data);
-    elseif (strcmp(wordType,'tarplusOne') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawTFR_tarplusOne_05-3ds}', data);  
-    elseif (strcmp(wordType,'tarplusTwo') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawTFR_tarplusTwo_05-3ds}', data);
-    end
-       
+    %save preprocessed data 
+    mous_db_putdata(subjectname, ['meg_processed_{TESTpreProcTFR' trialfun wordType '05-3ds}'], data);
+    
     % go to TFR pipeline: "mous_tfr_pipline" 
 
     %% ERF %%%%%%%%%%%%%%%%%%%%
@@ -59,29 +57,19 @@ function mous_preprocessing_pipeline(subjectname)% Pipeline to run all preproces
     data = mous_preprocessing(filename{1}, trl, 300, 'ERF', -0.5);
     
     % save preprocessed data
-    if (strcmp(wordType,'target') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_targetword_05-3ds}', data);
-    elseif (strcmp(wordType,'tarplusOne') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_tarplusOne_05-3ds}', data);  
-    elseif (strcmp(wordType,'tarplusTwo') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_tarplusTwo_05-3ds}', data);
-    end
+        mous_db_putdata(subjectname, ['meg_processed_{preProcERF' trialfun wordType '05-3ds}'], data);
 
     %% short time window
 
+    % redefine the trial window and remove the artefacts for the short
+    % window
     % define trial window (includes pre -500ms and post 3s)
-    [trl] = mous_defineTrial(filename{1}, 0.2, 1.0, wordType); 
-
+    [trl] = mous_defineTrial(filename{1}, 0.2, 1.0, wordType, trialfun); 
     % remove the artifacts that have been defined/detected
     [trl] = mous_artifact_remove(trl, filename{1}, tmp);
 
     data = mous_preprocessing(filename{1}, trl, 300, 'ERF', -0.2);
     
     % save preprocessed data  
-    if (strcmp(wordType,'target') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_targetword_02-1ds}', data);
-    elseif (strcmp(wordType,'tarplusOne') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_tarplusOne_02-1ds}', data);  
-    elseif (strcmp(wordType,'tarplusTwo') > 0) 
-        mous_db_putdata(subjectname, 'meg_processed_{rawERF_tarplusTwo_02-1ds}', data);
-    end
+    mous_db_putdata(subjectname, ['meg_processed_{preProcERF' trialfun wordType '02-1ds}'], data);
+
