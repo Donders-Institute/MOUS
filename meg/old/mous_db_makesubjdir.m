1f8d00f0b601a41e0e4a52d457769bccb2251228
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2012-10-23 11:43:54 +0200

    created old directory to contain functions up to a certain date

---
+++ meg/old/mous_db_makesubjdir.m
@@ -0,0 +1,56 @@
+function mous_db_makesubjdir(subject)
+
+% function to create a directory (with subdirectories) for the Processed
+% data for a particular named subject. If not already existing, the
+% directory will be created in ~annhul/MOUS/Processed/
+%
+% $Id: mous_db_makesubjdir.m 43 2012-05-16 10:40:37Z jansch $
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject]));
+subjdir  = ['/home/language/annhul/MOUS/Processed/',subject];
+if ~existdir
+  fprintf(['creating subject specific directory: ', subjdir,'\n']);
+  mkdir(subjdir);
+  cmd = ['chmod g+w ' subjdir];
+  system(cmd);
+end
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject,'/meg_anatomy']));
+if ~existdir
+  fprintf(['creating subject specific subdirectory: ', subjdir, '/meg_anatomy\n']);
+  mkdir([subjdir, '/meg_anatomy']);
+  cmd = ['chmod g+w ' subjdir];
+  system(cmd);
+end
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject,'/other']));
+if ~existdir
+  fprintf(['creating subject specific subdirectory: ', subjdir, '/other\n']);
+  mkdir([subjdir, '/other']);
+  cmd = ['chmod g+w ' subjdir '/other'];
+  system(cmd);
+end
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject,'/TFR']));
+if ~existdir
+  fprintf(['creating subject specific subdirectory: ', subjdir, '/TFR\n']);
+  mkdir([subjdir, '/TFR']);
+  cmd = ['chmod g+w ' subjdir '/TFR'];
+  system(cmd);
+end
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject,'/ERF']));
+if ~existdir
+  fprintf(['creating subject specific subdirectory: ', subjdir, '/ERF\n']);
+  mkdir([subjdir, '/ERF']);
+  cmd = ['chmod g+w ' subjdir '/ERF'];
+  system(cmd);
+end
+
+existdir = ~isempty(dir(['/home/language/annhul/MOUS/Processed/',subject,'/MNE']));
+if ~existdir
+  fprintf(['creating subject specific subdirectory: ', subjdir, '/MNE\n']);
+  mkdir([subjdir, '/MNE']);
+  cmd = ['chmod g+w ' subjdir '/MNE'];
+  system(cmd);
+end
