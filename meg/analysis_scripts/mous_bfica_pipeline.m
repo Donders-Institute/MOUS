227a46b26e77a15d819a90bda3084fa8aa52145c
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2012-10-23 15:26:22 +0200

    changed naming, removed uppercase and added underscore

--- meg/analysis_scripts/mous_bfica_pipeline.m
+++ meg/analysis_scripts/mous_bfica_pipeline.m
@@ -7,7 +7,7 @@ doica    = true;
 
 if dodss,
   [comp, avgpre, avgcomp] = mous_bfica_dss(subjectname);
-  mous_db_putdata(subjectname, 'meg_processed_{bfICA_comp}', comp, avgcomp, avgpre);
+  mous_db_putdata(subjectname, 'meg_processed_{_bfica_comp}', comp, avgcomp, avgpre);
 end
 
 %toi = 0.375;
@@ -15,40 +15,40 @@ end
 if dofreq,
   % theta frequency
   %freq   = mous_bfica_freq(subjectname, 5);
-  %mous_db_putdata(subjectname, 'meg_processed_{bfICA_freq5}', freq);
+  %mous_db_putdata(subjectname, 'meg_processed_{_bfica_freq5}', freq);
   
   % beta frequency
   freq   = mous_bfica_freq(subjectname, 20);
-  mous_db_putdata(subjectname, 'meg_processed_{bfICA_freq}', freq);
+  mous_db_putdata(subjectname, 'meg_processed_{_bfica_freq}', freq);
 end
 if dosource,
   % theta frequency
-  %freq   = mous_db_getdata(subjectname, 'meg_processed_{bfICA_freq5}');
+  %freq   = mous_db_getdata(subjectname, 'meg_processed_{_bfica_freq5}');
   %source = mous_bfica_source(subjectname, freq, toi);
-  %mous_db_putdata(subjectname, ['meg_processed_{bfICA_source5_',num2str(round(toi*1000)),'}'], source);
+  %mous_db_putdata(subjectname, ['meg_processed_{_bfica_source5_',num2str(round(toi*1000)),'}'], source);
   
-  freq   = mous_db_getdata(subjectname, 'meg_processed_{bfICA_freq}');
+  freq   = mous_db_getdata(subjectname, 'meg_processed_{_bfica_freq}');
   source = mous_bfica_source(subjectname, freq);
-  %mous_db_putdata(subjectname, ['meg_processed_{bfICA_source',num2str(round(toi*1000)),'}'], source);
-  mous_db_putdata(subjectname, 'meg_processed_{bfICA_source}', source);
+  %mous_db_putdata(subjectname, ['meg_processed_{_bfica_source',num2str(round(toi*1000)),'}'], source);
+  mous_db_putdata(subjectname, 'meg_processed_{_bfica_source}', source);
 end
 if dovox,
   % theta frequency
-  %freq   = mous_db_getdata(subjectname, 'meg_processed_{bfICA_freq5}');
-  %source = mous_db_getdata(subjectname, ['meg_processed_{bfICA_source5_',num2str(round(toi*1000)),'}']);
+  %freq   = mous_db_getdata(subjectname, 'meg_processed_{_bfica_freq5}');
+  %source = mous_db_getdata(subjectname, ['meg_processed_{_bfica_source5_',num2str(round(toi*1000)),'}']);
   %sourcedata = mous_bfica_sourcedata(source, freq, toi);
-  %mous_db_putdata(subjectname, ['meg_processed_{bfICA_sourcedata5_',num2str(round(toi*1000)),'}'], sourcedata);
+  %mous_db_putdata(subjectname, ['meg_processed_{_bfica_sourcedata5_',num2str(round(toi*1000)),'}'], sourcedata);
   
-  freq   = mous_db_getdata(subjectname, 'meg_processed_{bfICA_freq}');
-  source = mous_db_getdata(subjectname, 'meg_processed_{bfICA_source}');
+  freq   = mous_db_getdata(subjectname, 'meg_processed_{_bfica_freq}');
+  source = mous_db_getdata(subjectname, 'meg_processed_{_bfica_source}');
   sourcedata = mous_bfica_sourcedata(source, freq);%, toi);
-  mous_db_putdata(subjectname, 'meg_processed_{bfICA_sourcedata}', sourcedata);
+  mous_db_putdata(subjectname, 'meg_processed_{_bfica_sourcedata}', sourcedata);
 end
 
 if doica,
   
   comp = mous_bfica_ica(subjectname);
-  mous_db_putdata(subjectname, 'meg_processed_{bfICA_ica}', comp);
+  mous_db_putdata(subjectname, 'meg_processed_{_bfica_ica}', comp);
 end
 
 
