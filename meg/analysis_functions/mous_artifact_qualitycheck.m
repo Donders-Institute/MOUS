1cf9d9cffd91def6c0674b23555d4f2d69c2f2a9
Nietzsche Ho Lok Lam <nielam@mentat319.dccn.nl>
2012-11-07 11:13:15 +0100

    updated script for checking artifact detection (but not yet completed)

--- meg/analysis_functions/mous_artifact_qualitycheck.m
+++ meg/analysis_functions/mous_artifact_qualitycheck.m
@@ -1,9 +1,8 @@
-function [h1,h2] = mous_artifact_qualitycheck(subjectname)
-
+function [h1,h2] = mous_artifact_qualitycheck(subjectname, artifact)
 % MOUS_ARTIFACT_QUALITYCHECK does a quality control check on the
-% ouput of the artifact pipeline, mrelying on visual inspection of a
-% number of output figures, generated from the output from the dss artifact
-% identification.
+% ouput of the artifact pipeline, relying on visual inspection of a
+% number of output figures, generated from the output from the dss artifact identification.
+% JM & NL 2012-10-05
 %
 % Use as
 %   mous_artifact_qualitycheck(subjectname)
@@ -13,39 +12,123 @@ function [h1,h2] = mous_artifact_qualitycheck(subjectname)
 %
 % See also MOUS_ARTIFACT_PIPELINE
 
-cfgart = mous_db_getdata(subjectname, 'meg_artifactcfg');
+% trl for trials (sent/seq)
+filename    = mous_db_getfilename(subjectname, 'meg_raw_task'); 
+trlSen      = mous_defineTrial(filename{1}, 0.5, 0.5, 'all','visual_sentence');  % entire sentence, with prestim and poststim = 0.5s 
 
-% the idea is to generate figures summarizing the artifacts
-for k = 1:numel(cfg)
-  % do it for each of the artifact types
-  
-  % read in the MEG-data corresponding to the identified intervals
-  cfg = [];
-  cfg.dataset    = cfgart{k}.dataset;
-  cfg.continuous = 'yes';
-  cfg.trl        = cfgart{k}.artfctdef.zvalue.artifact;
-  cfg.trl(:,1)   = cfg.trl(:,1) - 120;
-  cfg.trl(:,2)   = cfg.trl(:,2) + 120;
-  cfg.trl(:,3)   = cfg.trl(:,1) - mean(cfg.trl,2);
-  cfg.channel    = 'MEG';
-  cfg.demean     = 'yes';
-  data           = ft_preprocessing(cfg);
+%%%  Artifact info  %%%
+cfgart      = mous_db_getdata(subjectname, 'meg_artifactcfg');
 
-  % visualize it in a butterfly plot
-  Nart = numel(data.trial);
-  
-  % come up with a trick to generate more than one figure, if the number of artifacts>100
-  Ncol = 10;
-  Nrow = 10;
-  for m = 1:Nart
-    hpos = mod(m-1,Ncol)*0.1;
-    vpos = 1 - floor((m-1)/10)*0.1;
+for m = 1:numel(cfgart)
+    % trl for artifacts
+    trlArt  = cfgart{m}.artfctdef.zvalue.artifact;
+    artvec  = zeros(1,max(trlSen(:,2)));   % zero vector: each element is a sample within trial data
+
+    % artifact specification for each trial
+    for n = 1:size(trlArt,1)                   % for each artifact (each row in trlArt)
+      artvec(1,trlArt(n,1):trlArt(n,2)) = n;   % mark which trial (number) has the same samples
+    end
+
+    % for each trial, tmpartvec stores location of trial in respect to artifact
+    for k = 1:size(trlSen,1)  
+      tmpartvec                     = artvec(1,trlSen(k,1):trlSen(k,2));          % each element (sample) in artifact vector that overlaps with a trial is marked e.g., [0 0 0 4 4 4 0 0 5 5 ...]
+      % any()  % returns logical 1 if elements are nonzero / logical 1 (true), else returns logical 0.
+      if any(tmpartvec)             % if there is a value in tmpartvec, it says that the artifact belongs to a trial
+        diagnostics(k).okflag       = false;                                        % is an artifact 
+        
+        % get the index relating to the original artifact
+        diagnostics(k).artifactindx = setdiff(unique(tmpartvec),0);                 % returns values in tmpartvec not in 0 vector 
+        uniqueartvec = unique(tmpartvec);                                           % unique values in tmpartvec (there maybe multiples of the same trial number depending on how many samples trial+art extend)
+        
+        % get the number of samples in the artifacts
+        for kk = 1:numel(uniqueartvec)-1
+            diagnostics(k).artifactnsmp(kk) = sum(tmpartvec==uniqueartvec(kk+1));     % sample duration of each artifact (which may span >1 trial) 
+        end
+        
+        % get the begin and endpoint of the artifact, expressed relative to
+        % the trial
+        for kk = 1:numel(uniqueartvec)-1
+            diagnostics(k).artifact(kk,1) = find(diff([0 tmpartvec==uniqueartvec(kk+1)])==1);   
+            diagnostics(k).artifact(kk,2) = find(diff([tmpartvec==uniqueartvec(kk+1) 0])==-1);
+            % first value in uniqueartvec is always 1, there only need n-1 of the values 
+            % tmpartvec gets the current artifact number e.g., 30
+            % diff() calculates difference between consecutive elements where the former is subtracted from the latter 
+            % [0 1] = 1 vs. [1 0] = -1          
+            % find() gets index of first element in tmpartvec that is "1" (onset) or "-1" (offset)
+        end
+        
+      else
+        diagnostics(k).okflag       = true;   % is not an artifact
+        diagnostics(k).artifactindx = [];
+        diagnostics(k).artifactnsmp = [];
+      end
+    end
     
-    ft_plot_vector(data.time{m},data.trial{m},'width',0.09,'height',0.09,'hpos',hpos,'vpos',vpos,'color','k');
+    % load relevant trial data for each type of artifact  
+    cfg             = [];
+    cfg.dataset     = filename{1};
+    cfg.trl         = trlSen;
+    cfg.continuous  = 'yes';
     
-  end
+    if strcmp(cfgart{m}.varname, 'cfgeog1') > 0        % for blinks
+        cfg.demean = 'yes';
+        cfg.channel     = {'EEG058'};           
+    elseif strcmp(cfgart{m}.varname, 'cfgeog2') > 0    % for saccades, maybe informative to include blink channel (sometimes affect saccades, and saccade detection doesn't seem that informative)
+        cfg.demean = 'yes';
+        cfg.channel     = {'EEG057'}; 
+        % filter out blinks: bandstop filter [1 10]? Would this also removing saccades?
+    else                                               % squid jumps or muscles
+        cfg.channel     = {'MEG'};           
+        if strcmp(cfgart{m}.varname, 'cfgmuscle') > 0  
+            cfg.bpfilter = 'no';
+            cfg.hilbert  = 'no';
+            %cfg.rectify  = 'yes';
+            cfg.hpfilter = 'yes';
+            cfg.hpfreq   = 80;
+            %cfg.demean     = 'yes';
+            %cfg.boxcar     = 0.5;
+            cfg.fltpadding = 0;
+            cfg.trlpadding = 0;
+            cfg.artpadding = 0;
+        end 
+    end 
+    dataSen = ft_preprocessing(cfg);
+    dataSen = ft_channelnormalise([], dataSen);                 % z-values
+    
+    vlim = [inf -inf];
+        for k = 1:numel(dataSen.trial)
+            dataSen.trial{k} = mean(dataSen.trial{k},1);        % single vector; average across channels
 
-
-  % we will use ft_plot_vector for efficiency
-
-end
+            vlim = [min(vlim(1),min(dataSen.trial{k})) max(vlim(2),max(dataSen.trial{k}))]; % min and max value 
+            % get smallest and largest value across trials (compare newest min/max value with current min/max value
+        end
+    dataSen.label = {'MEGavg'};
+  
+    
+    % plot trials and superimpose artifact           
+    %for j = 1:size(diagnostics,2)
+     for j = 1:25
+        if mod(j,25) == 1                           % new figure for each set of 10 trials
+            figure;
+            hold on
+            title([subjectname ': Trials ' num2str(j) ' to ' num2str(j+24)])
+            axis on
+        end
+        hpos        = mod(j-1,5)*0.2;
+        vpos        = 0.8 - floor((j-1)/5)*0.2;
+        hlim        = dataSen.time{j}([1 end]);
+        %vlim        = [min(dataSen.trial{j}(:)) max(dataSen.trial{j}(:))];
+        trialFig    = ft_plot_vector(dataSen.time{j},dataSen.trial{j},'height', 0.18 ,'width', 0.18, 'hpos', hpos, 'vpos', vpos ,'color','k','hlim',hlim,'vlim',vlim);  % plot the trial
+        if mod(j,25) == 1 
+            hold on
+            ft_plot_box([-0.08 -0.08 vlim(1) vlim(2)],'height', 0.18 ,'width', 0.18,'hlim',hlim,'vlim',vlim,'color','g');
+        end
+        ft_plot_text(hpos, vpos+0.07, num2str(j));
+        artindx     = diagnostics(j).artifact;
+        for jj = 1:size(artindx,1)
+            ft_plot_vector(dataSen.time{j}(artindx(jj,1):artindx(jj,2)), dataSen.trial{j}(:,artindx(jj,1):artindx(jj,2)),'color','r','hpos',hpos,'vpos',vpos,'height',0.18,'width',0.18,'hlim',hlim,'vlim',vlim);
+        end
+     end
+end 
+saveas(gca,strcat('ArtQC',title.eps),'epsc2'); % convert into pdf
+ % set size of figure to fit paper: http://www.parl.clemson.edu/~ahoover/Matlab-figures.txt
