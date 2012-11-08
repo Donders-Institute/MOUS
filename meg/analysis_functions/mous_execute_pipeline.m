3b9f7af61fd67c487c5386389fb4d7dd6d7797bc
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2012-11-08 12:34:59 +0100

    updated documentation

--- meg/analysis_functions/mous_execute_pipeline.m
+++ meg/analysis_functions/mous_execute_pipeline.m
@@ -1,8 +1,19 @@
 function mous_execute_pipeline(pipelinename, subjectname)
 
-% this function serves the purpose to make a script executable by qsub.
+% MOUS_EXECUTE_PIPELINE serves the purpose to make a script executable by qsub.
 % supply it with the name of the script that has to be run, and the
 % subjectname. the subjectname is assumed to be the only free parameter in
 % the script.
+%
+% example use in combination with qsubcellfun:
+%
+% subjectnames = {'V1001';'V1002';'V1003'};
+% pipelinename = 'mous_bfica_pipeline';
+%
+% qsubcellfun('mous_execute_pipeline', repmat({pipelinename}, [numel(subjectnames) 1]), subjectnames, 'memreq', memreq, 'timreq', timreq);
+% 
+% example use (standalone; not so useful):
+%
+% mous_execute_pipeline('mous_bfica_pipeline', 'V001');
 
 eval(pipelinename);
