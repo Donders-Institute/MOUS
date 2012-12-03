07ff114de26844afe0090f28a9dfd8642a4e69c5
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2012-12-03 12:23:05 +0100

    updated mous_db_putdata to also write provenance info (who created the file, when, and with matlab version + md5 hash key. Change of behavior: input arguments will be strings rather than variables, this requires a change in a lot of code

--- meg/analysis_functions/mous_db_putdata.m
+++ meg/analysis_functions/mous_db_putdata.m
@@ -1,14 +1,14 @@
 function mous_db_putdata(subject, type, varargin)
 
 % MOUS_DB_PUTDATA saves data from specified subject and type to 
-% the database
+% the database.
 %
-% $Id: mous_db_putdata.m 48 2012-05-30 14:21:15Z jansch $
+% See also MOUS_DB_GETFILENAME, MOUS_WRITE_PROVENANCE
 
 if numel(varargin)>1
   % it could be that the root directory is specified as last input argument
   if ischar(varargin{end}) && isdir(varargin{end})
-    rootdir = varargin{end};
+    rootdir  = varargin{end};
     varargin = varargin(1:end-1);
   else
     rootdir = '';
@@ -16,63 +16,73 @@ if numel(varargin)>1
 else
   rootdir = '';
 end
-  
+
+% create the string that specifies the file name and check whether it
+% already exists: FIXME now the default behavior is overwrite, this may
+% need to be changed however
 [filename, st] = mous_db_getfilename(subject, type, 0, rootdir);
+filename       = filename{1};
 if st(1)
-  warning('file %s exists, overwriting existing file', filename{1});
+  warning('file %s exists, overwriting existing file', filename);
 end
 
+% create empty data structure when more than one variable is to be saved,
+% so that the file will be saved as mat-file.
 if numel(varargin)>1
-  % save as a mat file
-  str = ['save(''',filename{1},''','''];
-  for k = 2+(1:numel(varargin))
-    str = [str inputname(k), ''','''];
-    eval([inputname(k),'=varargin{k-2};']);
-  end
-  str = [str(1:end-2),');'];
-  eval(str);
-  [p,n,e] = fileparts(filename{1});
-  if ~isempty(e)
-    cmd = ['chmod g+w ' filename{1}];
-  else
-    cmd = ['chmod g+w ' filename{1}, '.mat'];
-  end
-  system(cmd);
-  return;
+  data = [];
+else
+  data = varargin{1};
 end
 
-data = varargin{1};
-fprintf('putting data to file %s\n', filename{1});
+fprintf('putting data to file %s\n', filename);
 if ft_datatype(data, 'volume')
   % save volumetric data as nifti
-  cfg = [];
+  cfg           = [];
   cfg.filetype  = 'nifti';
   cfg.parameter = 'anatomy';
-  cfg.filename  = filename{1};
+  cfg.filename  = filename;
   ft_volumewrite(cfg, data);
+  mous_write_provenance(filename);
+  
 elseif ishandle(data)
- if exist(filename{1},'file'), system(['rm -rf ',filename{1}]); end
- print(data, '-dpng', filename{1}, '-r500');
-elseif ~isempty(strfind(type, 'headmodel'))
-  % save headmodel data as mat-file and name variable 'vol'
-  vol = data;
-  save(filename{1}, 'vol');
-elseif ~isempty(strfind(type, 'sourcemodel'))
-  sourcemodel = data;
-  save(filename{1}, 'sourcemodel');
+ if exist(filename,'file'), system(['rm -rf ',filename]); end
+ print(data, '-dpng', filename, '-r500');
 else
-%elseif ~isempty(strfind(type, 'processed'))
   % save as a mat-file
-  str = ['save(''',filename{1},''','''];
-  str = [str inputname(3), ''','''];
-  eval([inputname(3),'=varargin{1};']);
-  str = [str(1:end-2),');'];
-  eval(str);
-  [p,n,e] = fileparts(filename{1});
+  
+  % this function uses some local variables with a cryptical name to prevent
+  % variable name collisions with the local copy of the input variables.
+  filename_c9e61b166b = filename; clear filename
+  
+  [p, f, x] = fileparts(filename_c9e61b166b);
+  if ~strcmp(x, '.mat')
+    warning('appending the extension .mat');
+    filename_c9e61b166b = fullfile(p, [f '.mat']);
+  end
+  clear p f x
+  
+  % get the anonymous input variables into the local workspace
+  for index_c9e61b166b=1:length(varargin)
+    setvariable(varargin{index_c9e61b166b}, evalin('caller', varargin{index_c9e61b166b}));
+  end
+  clear index_c9e61b166b
+  
+  save(filename_c9e61b166b, varargin{:});
+  
+  % write the corresponding provenance information
+  mous_write_provenance(filename_c9e61b166b);
+
+  [p,n,e] = fileparts(filename_c9e61b166b);
   if ~isempty(e)
-    cmd = ['chmod g+w ' filename{1}];
+    cmd = ['chmod g+w ' filename_c9e61b166b];
   else
-    cmd = ['chmod g+w ' filename{1}, '.mat'];
+    cmd = ['chmod g+w ' filename_c9e61b166b, '.mat'];
   end
   system(cmd);
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION to prevent an eval(sprintf(...))
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function setvariable(name, val)
+assignin('caller', name, val)
