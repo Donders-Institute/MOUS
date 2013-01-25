be48b953807d23ac300d47b0f23238e59d5e9d2c
Nietzsche Ho Lok Lam <nielam@mentat320.dccn.nl>
2013-01-25 16:31:54 +0100

    start of trial == onset of first word

--- meg/trialfun/trialfun_auditory_sentence.m
+++ meg/trialfun/trialfun_auditory_sentence.m
@@ -1,18 +1,22 @@
 function [trl] = trialfun_auditory_sentence(cfg)
 
 % [trl] = trialfun_visual_sentence(cfg) creates the trl-matrix for the 
-% whole sentence
+% whole sentence: notes onset of trial and target word. For speech onset of
+% first word (and other words) see trialfun_auditory_word
 % 
 % the cfg needs to contain the following option:
 %   cfg.dataset = string, name of the dataset
 %
+% timepoint zero is the speech onset of the first word!
+% 
 % the trl-matrix has 6 columns:
-%   column 1: begin sample, fix onset trigger
-%   column 2: end sample, offset of last word + 600 ms, or next fix onset
-%             trigger, whichever occurs first.
+%   column 1: begin sample: speech onset of first word 
+%   column 2: end sample, offset of audiofile 
+%             Can change to: (1) offset of last speech word, (2) onset of
+%             fixation cross
 %   column 3: offset of first sample with respect to time point 0
 %   column 4: trial number
-%   column 5: condition (i.e. trigger corresponding to target word
+%   column 5: trigger corresponding to target word
 %   column 6: sample number of critical word onset, relative to time point 0
 %
 % $Id: trialfun_visual_sentence.m 39 2012-05-08 11:12:46Z jansch $
@@ -42,12 +46,24 @@ for k = 1:numel(selfix)-1
   
   % FIXATION CROSS - keep track of the '20' trigger
   fixsmp = smp(selfix(k));
-  
+    
   sel = selfix(k):selfix(k+1);
     
   % create a sequence of triggers within the trial
   tmpval = val(sel);
   tmpsmp = smp(sel);
+  
+  % get FIRST WORD onset  
+  for kk = 1:numel(tmpval)
+    trg1 = tmpval(kk);
+    % trg2 = tmpval(kk+1);  no offset
+    if trg1 == 1 || trg1 == 3 || trg1 == 5 || trg1 == 7  % && trg2==15
+      offset = min(1200,tmpsmp(kk)-fixsmp);
+      strtsmp = tmpsmp(kk);  % first word speech onset == time point 0
+      bslsmp  = tmpsmp(kk)-1200; % 1s before speech onset of first word = onset of baseline
+      break;    
+    end    
+  end
   
   % get TARGET WORD onset (offset not possible because not marked by triggers)
   condition = [];
@@ -66,16 +82,6 @@ for k = 1:numel(selfix)-1
     continue;
   end
   
-  % get FIRST WORD onset 
-  for kk = 1:numel(tmpval)
-    trg1 = tmpval(kk);
-    % trg2 = tmpval(kk+1);  no offset
-    if trg1 == 1 || trg1 == 3 || trg1 == 5 || trg1 == 7  % && trg2==15
-      offset = min(1200,tmpsmp(kk)-fixsmp);
-      break;    
-    end    
-  end
-  
   % get AUDIOFILE OFFSET
   for kk = numel(tmpval):-1:1
     trg1 = tmpval(kk);
@@ -88,7 +94,8 @@ for k = 1:numel(selfix)-1
   
   
   %%
-  tmp = [fixsmp endsmp -offset k condition critsmp-offset-fixsmp];
+  %tmp = [fixsmp endsmp -offset k condition critsmp-offset-fixsmp]; % visual stimuli
+  tmp = [strtsmp endsmp -offset k condition critsmp-offset-strtsmp];
   trl = cat(1,trl,tmp);
 
 end
