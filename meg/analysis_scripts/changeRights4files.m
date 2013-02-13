3e17cbec1105de0c66795fe8678446c769ff58a9
Nietzsche Ho Lok Lam <nielam@mentat320.dccn.nl>
2013-02-13 13:19:20 +0100

    updated to reflect new folder structure and includes participants from 1110 to 11116

--- meg/analysis_scripts/changeRights4files.m
+++ meg/analysis_scripts/changeRights4files.m
@@ -1,16 +1,25 @@
 
+% V1001 : V1009
 
 for n= 1:9
-  str = sprintf('cd /home/language/annhul/MOUS/Processed/V100%d/', n);
+  str = sprintf('cd /home/language/annhul/MOUS/meg/V100%d/tfr', n);
   eval(str);
   cmd = ['chmod -R g+w *'];
   system(cmd);
 end
 
+% V1010 : V1099
 for n= 10:99
-  str = sprintf('cd /home/language/annhul/MOUS/Processed/V10%d/', n);
+  str = sprintf('cd /home/language/annhul/MOUS/meg/V10%d/tfr', n);
   eval(str);
   cmd = ['chmod -R g+w *'];
   system(cmd);
 end
 
+% V1110 : V1116
+for n = 10:16
+  str = sprintf('cd /home/language/annhul/MOUS/meg/V11%d/tfr', n);
+  eval(str);
+  cmd = ['chmod -R g+w *'];
+  system(cmd);  
+end 
