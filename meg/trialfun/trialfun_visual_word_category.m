ccc79bd06a9001c7056e2e0fdb62e7ae1b742e65
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2013-03-15 18:06:05 +0100

    code has been cleaned up and tested for all 6 version of the MEG experiment
    Former-commit-id: 28d7eaf52aaf72dcafd544c4bc5716d4d5149d6d

--- meg/trialfun/trialfun_visual_word_category.m
+++ meg/trialfun/trialfun_visual_word_category.m
@@ -1,6 +1,12 @@
+function [outtrl] = trialfun_visual_word_category(subjectname)
 % trialfun_visual_word_category
-% indicates the word category of each word in the sentence
-% there are 9 categories
+% This function codes the category of each word from each
+% trial(sentence/sequence). It does not need the trl (trlA) from
+% trialfun_visual_word as an argument as trlA is called from within
+% this function. % trlA has 8 columns (see trialfun_visual_word for details), and this
+% function makes a 9th column indicate the categories.
+% 
+% There are 9 categories:
 % 1 adjective
 % 2 adverb
 % 3 noun
@@ -10,27 +16,32 @@
 % 13 preposition
 % 14 pronouns
 % 15 telword
-% (1) read in logfile to get words for each trial (sent/seq) = A
-% (2) read in excel file where each word's category has been coded = B
-% (3) match words in A to those in B to get the appropriate categories
-% ** as A (i.e. logfile) doesn't indicate the number for each sent/seq,
-% that means we need to match stimuli between A and B (ARGG.. silly logfile)
 
-%% read in logfile of stimuli and matfile of sent/seq length
-% N.B. logfile is iterated twice, but we only read the first iteration (that's good!)
-% load senlen  % C1 = sentence number, C2 = sentence length 
+% Details of the code:
+% The logfile specific to each participant is read in, the excess
+% information is removed, and only the words from each trial are preserved,
+% concatenated within each trial, and then matched to the sentences/sequences in catdata.
+%    N.B. logfile is iterated twice and we use the 1st interation.
+% catdata is the matrix representation of the excel file containing the words and their
+% respective categories (coded by hand)
 
 %% Read in logfile
-intxt  = '/home/language/annhul/MOUS/meg/V1010/RAW/V1010-4-MEG-MOUS-Vis.log';
-fid = fopen(intxt);
+filename = mous_db_getfilename(subjectname,'meg_raw_log');
+intxt   =  filename{1};
+fid     = fopen(intxt);
 fseek(fid,0,'eof');  % at eof to get number of elements in text
-numelm = ftell(fid);
+numelm  = ftell(fid);
 fseek(fid,0,'bof');  % at bof to start reading
-alltxt = fread(fid,numelm,'uint8=>char');  % whole logfile 
+alltxt  = fread(fid,numelm,'uint8=>char');  % whole logfile 
 fclose(fid); 
 
 %% remove excessive information i.e. lines without words (preceded by a trigger)
-idx = strfind(alltxt(:)','V1010'); % skip over the line of logfile that codes the extra 'empty word' 
+% need to use regexp as there is not a consistent format in the logfile
+% finaltext{1} = X xx
+% finaltext{101} = Xxx
+% finaltext{360} = X xx.  (end of sentence)
+
+idx = strfind(alltxt(:)', subjectname); % skip over the line of logfile that codes the extra 'empty word' 
 add = idx(end)+80;  
 idx = [idx add];
 alltxt = alltxt';
@@ -53,9 +64,6 @@ finaltext = newtext(idxword);  % all words in logfile, in chronological order
 
 %% Concatenate words of the same sentence
 expwords = cell(240,1);
-% finaltext{1} = X xx
-% finaltext{101} = Xxx
-% finaltext{360} = X xx.  (end of sentence)
 cnt = 0;
 for q = 1:size(finaltext,1)
     wordbeg = regexp(finaltext{q},'Picture\s\d\s?\w','end');  % end = return index of last character specified i.e. '\w'
@@ -68,68 +76,85 @@ for q = 1:size(finaltext,1)
     end
     expwords{cnt,1} = lower(cat(2,expwords{cnt,1},word));
 end 
+% remove empty cells (some participants did not complete all 240 trials)
+% code assumes that no trials are missing in between, but only consecutive
+% trials, counting backwards from 240, e.g., participant didn't do last
+% block, so last 5 trials are empty
+expwords = expwords(~cellfun('isempty',expwords));
+%% match trials (usually 240) to excel file with all stimuli (800)
+[catnum,~,catdata] = xlsread('home/language/nielam/MOUS_AnalysisNotes/Mous_categories.xls','','','basic'); 
 
+% % Clean cat: shift 1st column down by one, then remove Nans and first row
+tmp = catnum(:,1);
+tmp = [false;tmp(1:end-1)];
+catnum(:,1) = tmp;
+catnum(1,:) = [];
+catnum(all(isnan(catnum),2),:) = [];
 
-%% match trials (240) to excel file (800)
-% 1. read in excel file with range: A1 - Q1599
-% this excel file removes all the <'s> from all words in order to match the words in catwords  (strcat remove 's in words)
-[cat,text,catdata] = xlsread('home/language/nielam/MOUS_AnalysisNotes/Mous_categories.xls','','A1:Q1599','basic'); 
-
-% put categorisation into a separate matrix, removing first NaN column
-% each row is one sentence/sequence
-% FIXME: how to remove trailing NaNs of each row without automatic reshaping matrix
-% into vector 
-catnum = cat(2:2:end,:);
-catnum = catnum(:,2:end);
-
-% turn trials into concatenated words
-% use lower case for everything to get a perfect match (since logfiles have both "india"  and "India").
+% concatenate all words in a trial, remove empty spaces
+% use lower case to get a perfect match (because logfiles across participants are inconsistent e.g., india" and "India" both exist).
 catdata = catdata(1:2:end,:);      % remove every other line holding category numbers
 catwords = cell(size(catdata,1)/2, 1);
 for qq = 1:size(catdata,1)
-    catwords{qq} = lower(strcat(2,catdata{qq,2:end})); %%FIXME:there's a funny square box before the sent/seq   
+    %FIXME:(1) suppress warning from using strcat about truncating values
+    %      (2) how to remove square box appearing before sentence (eventho it doesn't affect results?)
+    catwords{qq} = lower(strcat(2,catdata{qq,2:end})); 
 end 
 
-% 2. match sentences/sequences
-% N.B. check if matlab will flipout for ptp's with <240 trials
-
+% 2. match the set of experiment trials to entire set of trials
 % idx4expword is the index of the sent/seq in catwords, not the (audiofile) name of
 % the actual sentence/sequence
-idx4expword = [];  % prefer this than assigning space because some participants have <240 trials
-%idx4expword = zeros(240,1)
+idx4expword = [];  % prefer this than zeros(240,1) since some participants have <240 trials
 for qq = 1:size(expwords,1)
-    % catwords is first arguement cuz it is a cellstr (the one to check thru)
-    itmp     = strfind(catwords,expwords{qq}); % going for 100% match, but there possessive s, and capitals causing issues
+    itmp     = strfind(catwords,expwords{qq});
     idxsent  = find(not(cellfun('isempty',itmp)));
     if isempty(idxsent)
         warning('%d has no index',qq);  % indices of sentences in expword that don't have a match in catdata
     end 
     idx4expword = [idx4expword; idxsent];  
-    %idx4expword(qq,1) = idxsent;          % this one stops after expword{17}, why?
 end
 
 %% load trialfun_visual_word and add to it's trialinfo
 filename    = mous_db_getfilename(subjectname, 'meg_ds_task');
 prestim     = 0.5; 
-poststim = 3.0;
+poststim    = 3.0;
 wordType    = 'all';
 trialfun    = 'visual_word';
-[trl] = mous_defineTrial(filename{1}, prestim, poststim, wordType, trialfun); 
+[trl]       = mous_defineTrial(filename{1}, prestim, poststim, wordType, trialfun); 
 
-% Clean cat: shift 1st column down by one, and remove NaNs 
-tmp = cat(:,1);
-tmp = [false;tmp(1:end-1)];
-cat(:,1) = tmp;
-cat(1,:) = [];
-cat(all(isnan(cat),2),:) = [];
+fileid = catnum(idx4expword);
 
-fileid = cat(idx4expword);
-% CHANGE:  don't need to assign category values for each sentence one by
+% length of each sentence/sequence based on trl matrix
+triallen = [];
+dumtrl = [trl; ones(size(trl,2),1)'];
+for kk = 2:size(dumtrl,1)
+    if dumtrl(kk,8) == 1;
+        tmplen = dumtrl(kk-1,8);
+        triallen = [triallen; tmplen];
+    end
+end
+
+catvec = [];
+for mm = 1:size(idx4expword,1)
+    row = find(catnum(:,1) == fileid(mm));
+    assignvec = catnum(row,2:end)';
+    assignvec(all(isnan(assignvec),2),:) = [];
+    assignvec = assignvec(1:triallen(mm));
+    catvec = [catvec; assignvec];
+end 
+outtrl = [trl, catvec];  % catvec is the 9th column in the trl matrix
+
+
+%% if trialfun is fixed to
+% 1) include the last word from the sequence
+% 2) made note on which participants the sentences got cut off (Because
+% stimuli was incomplete)
+% then we can use the code below:
+
+% Here, the code doesn't need to assign category values for each sentence one by
 % one, just create the ENTIRE column of category values and then attach
 % them as one whole column
-% NOPE, can´t do that cuz stimuli in presentation (hence logfile) is wrong (one word short) in
-% some sentences. 
-
+%
 % catvec = [];
 % checker = [];
 % for mm = 1:size(idx4expword,1)
@@ -150,7 +175,8 @@ fileid = cat(idx4expword);
 %        checker = [checker; checkvec];
 %     end    
 % end
-% 
+%
+%% useful when checking accuracy of trl and cat.
 % % mark start of sentences based on file category values
 % for k = 1:size(trl,1)
 %     if trl(k,10) == 1 && trl(k,8) ~= 1
@@ -158,52 +184,4 @@ fileid = cat(idx4expword);
 %     end
 % end
 
-% length of each sentence/sequence based on trl matrix
-triallen = [];
-dumtrl = [trl; ones(size(trl,2),1)'];
-for kk = 2:size(dumtrl,1)
-    if dumtrl(kk,8) == 1;
-        tmplen = dumtrl(kk-1,8);
-        triallen = [triallen; tmplen];
-    end
-end
-
-catvec = [];
-for mm = 1:size(idx4expword,1)
-    row = find(cat(:,1) == fileid(mm));
-    assignvec = cat(row,2:end)';
-    assignvec(all(isnan(assignvec),2),:) = [];
-    assignvec = assignvec(1:triallen(mm));
-    catvec = [catvec; assignvec];
-end 
-
-
-% catvec = 2631 values.  extra 5...
-% trl = 2625 values
-% catvec = [catvec(1:end-6)];
-% checker = [checker(1:end-6)];
-%trl = [trl, catvec, checker];
-trl = [trl, catvec];
-
-
-%% doesn't work
-% 
-% dlmread(intxt, '\t', 6, 1);
-% 
-% fid = fopen(intxt);
-% A = fscanf(fid, '%s');
-% fclose(fid);
-
-% [input,pos] = textscan(fid,'%*s %*d    %*s   %s   %*d %*d %*d %*d %*d %*s %*s %*s %*s',...
-%                  'Delimiter','\t', 'HeaderLines',4);  
-% try restarting textscan after skipping "empty word" 
-% y = textscan(fid(pos:end),'%*s %*d    %*s   %s   %*d %*d %*d %*d %*d %*s %*s %*s %*s',...
-%                  'Delimiter','\t','HeaderLines',4);  
-% input2 = y{:};
-
-%% don't need
-
-% fid = fopen([subjectname,'_vislog.txt'],'w');
-% fwrite(fid, nectxt, 'uint8');
-% fclose(fid);
 
