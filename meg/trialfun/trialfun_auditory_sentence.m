26910f942eda652719dd75f060b86b2871c08769
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2013-04-10 12:24:07 +0200

    updated trialfun to reflect offset properly
    Former-commit-id: 797a4d2a257cf2b262e9293cd46a1061e8f83f05

--- meg/trialfun/trialfun_auditory_sentence.m
+++ meg/trialfun/trialfun_auditory_sentence.m
@@ -10,10 +10,10 @@ function [trl] = trialfun_auditory_sentence(cfg)
 % timepoint zero is the speech onset of the first word!
 % 
 % the trl-matrix has 6 columns:
-%   column 1: begin sample: speech onset of first word - offset of 500ms
+%   column 1: begin sample: speech onset of first word - offset of 1s
 %             this is the auditory version of the begsmp in the visual
 %             version
-%   Offset allows for pre-sent/seq baseline
+%             Offset allows for pre-sent/seq baseline
 %   column 2: end sample, offset of audiofile 
 %             Can change to: (1) offset of last speech word, (2) onset of
 %             fixation cross
@@ -22,7 +22,7 @@ function [trl] = trialfun_auditory_sentence(cfg)
 %   column 5: trigger corresponding to target word
 %   column 6: sample number of critical word onset, relative to time point 0
 %
-% $Id: trialfun_visual_sentence.m 39 2012-05-08 11:12:46Z jansch $
+% $Id: trialfun_auditory_sentence.m  | NL 2013
 
 % read in event information
 hdr   = ft_read_header(cfg.dataset);  % ***remove {1} when done testing within function ***
@@ -59,17 +59,18 @@ for k = 1:numel(selfix)-1
   % get FIRST WORD onset  
   for kk = 1:numel(tmpval)
     trg1 = tmpval(kk);
-    % trg2 = tmpval(kk+1);  no offset
-    if trg1 == 1 || trg1 == 3 || trg1 == 5 || trg1 == 7  % && trg2==15
-      offset = min(600,tmpsmp(kk)-fixsmp);
+    if trg1 == 1 || trg1 == 3 || trg1 == 5 || trg1 == 7  % Don't need to a second cdtn to check because first word's trigger don't overlap with target's trigger
+      offset = 1200;
       begsmp = tmpsmp(kk) - offset;  % first word speech onset == time point 0  
+      %endsmp = tmpsmp(kk);
+      fstwrd = trg1;
       break;    
     end    
   end
   
   % get TARGET WORD onset (offset not possible because not marked by triggers)
-  condition = [];
-  critsmp   = [];
+%   condition = [];
+%   critsmp   = [];
   for kk = 1:numel(tmpval)-1  % need -1 otherwise it counts the following fixation-cross' trigger
     trg1 = tmpval(kk);
     if trg1<=8 && mod(trg1,2)==0 
@@ -83,11 +84,11 @@ for k = 1:numel(selfix)-1
     continue;
   end
   
-  % get AUDIOFILE OFFSET
+%   % get AUDIOFILE OFFSET
   for kk = numel(tmpval):-1:1
     trg1 = tmpval(kk);
     if trg1==15 
-      lastwordindx = kk-1;
+      %lastwordindx = kk-1;
       endsmp = tmpsmp(kk);
       break;
     end
@@ -96,7 +97,8 @@ for k = 1:numel(selfix)-1
   
   %%
   %tmp = [fixsmp endsmp -offset k condition critsmp-offset-fixsmp]; % visual stimuli
-  tmp = [begsmp endsmp -offset k condition critsmp-offset-begsmp];
+  %begsmp = first onset - 1s.
+   tmp = [begsmp endsmp -offset k fstwrd condition critsmp-offset];
   trl = cat(1,trl,tmp);
 
 end
