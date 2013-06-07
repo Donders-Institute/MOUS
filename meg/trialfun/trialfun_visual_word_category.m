6c9dbb419a3c0ee6bbd13dc4dca2a255a36632e4
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-06-07 14:02:06 +0200

    updated. Some subjects do not have exact match because of typos in visual logfile (these are skipped, but we could also aim for a match of at least half the sentence/sequence)

--- meg/trialfun/trialfun_visual_word_category.m
+++ meg/trialfun/trialfun_visual_word_category.m
@@ -3,9 +3,20 @@ function [outtrl] = trialfun_visual_word_category(subjectname)
 % This function codes the category of each word from each
 % trial(sentence/sequence). It does not need the trl (trlA) from
 % trialfun_visual_word as an argument as trlA is called from within
-% this function. % trlA has 8 columns (see trialfun_visual_word for details), and this
-% function makes a 9th column indicate the categories.
-% 
+% this function. 
+
+% output trl has 9 columns
+%   column 1: begin sample: word onset trigger - 500 ms.
+%   column 2: end sample, onset of word + 800 ms, or next word onset
+%             trigger, whichever occurs first.
+%   column 3: offset of first sample with respect to time point 0
+%   column 4: trial number (X out of 240; 120 sentences, 120 sequences)
+%   column 5: trigger corresponding to the word
+%   column 6: sample number relative to the onset of the first word
+%   column 7: number of samples between word on and offset
+%   column 8: ordinal number of word position
+%   column 9: word category 
+
 % There are 9 categories:
 % 1 adjective
 % 2 adverb
@@ -15,7 +26,7 @@ function [outtrl] = trialfun_visual_word_category(subjectname)
 % 12 determiners
 % 13 preposition
 % 14 pronouns
-% 15 telword
+% 15 numbers (one, two, three...)
 
 % Details of the code:
 % The logfile specific to each participant is read in, the excess
@@ -25,106 +36,135 @@ function [outtrl] = trialfun_visual_word_category(subjectname)
 % catdata is the matrix representation of the excel file containing the words and their
 % respective categories (coded by hand)
 
-%% Read in logfile
-filename = mous_db_getfilename(subjectname,'meg_raw_log');
-intxt   =  filename{1};
-fid     = fopen(intxt);
-fseek(fid,0,'eof');  % at eof to get number of elements in text
-numelm  = ftell(fid);
-fseek(fid,0,'bof');  % at bof to start reading
-alltxt  = fread(fid,numelm,'uint8=>char');  % whole logfile 
-fclose(fid); 
 
-%% remove excessive information i.e. lines without words (preceded by a trigger)
-% need to use regexp as there is not a consistent format in the logfile
-% finaltext{1} = X xx
-% finaltext{101} = Xxx
-% finaltext{360} = X xx.  (end of sentence)
+    %% Read in logfile
+    filename = mous_db_getfilename(subjectname,'meg_raw_log');
+    
+    intxt   =  filename{1};
+    fid     = fopen(intxt);
+    fseek(fid,0,'eof');  % at eof to get number of elements in text
+    numelm  = ftell(fid);
+    fseek(fid,0,'bof');  % at bof to start reading
+    alltxt  = fread(fid,numelm,'uint8=>char');  % whole logfile 
+    fclose(fid); 
 
-idx = strfind(alltxt(:)', subjectname); % skip over the line of logfile that codes the extra 'empty word' 
-add = idx(end)+80;  
-idx = [idx add];
-alltxt = alltxt';
-newtext = cell(numel(idx)-1,1);
-for k = 1:numel(idx)-1
-    newtext{k} = alltxt(idx(k):idx(k+1)-1);  % easier to find relevant entry lines in txtfile
-end
- 
-wordpresent = zeros(size(newtext));
-for m = 1:size(newtext,1)
-    check   = regexp(newtext{m},'Picture\s\d');       % trigger WITH word
-    fix     = regexp(newtext{m},'Picture\s\F');       % fixation cross marks start of trial
-    space   = regexp(newtext{m},'Picture\s\d\s\s\s'); % trigger WITHOUT word
-    if ~isempty(check) && isempty(space) || ~isempty(fix)
-        wordpresent(m) = 1;
-    end        
-end 
-idxword = find(wordpresent == 1);
-finaltext = newtext(idxword);  % all words in logfile, in chronological order
 
-%% Concatenate words of the same sentence
-expwords = cell(240,1);
-cnt = 0;
-for q = 1:size(finaltext,1)
-    wordbeg = regexp(finaltext{q},'Picture\s\d\s?\w','end');  % end = return index of last character specified i.e. '\w'
-    wordend = regexp(finaltext{q},'Picture\s\d\s?\w*','end');
-    word    = finaltext{q}(wordbeg:wordend);
-    if isempty(word)
-        cnt = cnt+1;
-        expwords{cnt,1} = '';
-        continue;
-    end
-    expwords{cnt,1} = lower(cat(2,expwords{cnt,1},word));
-end 
-% remove empty cells (some participants did not complete all 240 trials)
-% code assumes that no trials are missing in between, but only consecutive
-% trials, counting backwards from 240, e.g., participant didn't do last
-% block, so last 5 trials are empty
-expwords = expwords(~cellfun('isempty',expwords));
-%% match trials (usually 240) to excel file with all stimuli (800)
-[catnum,~,catdata] = xlsread('home/language/nielam/MOUS_AnalysisNotes/Mous_categories.xls','','','basic'); 
+    %% remove excessive information i.e. lines without words (preceded by a trigger)
+    % need to use regexp as there is not a consistent format in the logfile
+    % finaltext{1} = X xx
+    % finaltext{101} = Xxx
+    % finaltext{360} = X xx.  (end of sentence)
 
-% % Clean cat: shift 1st column down by one, then remove Nans and first row
-tmp = catnum(:,1);
-tmp = [false;tmp(1:end-1)];
-catnum(:,1) = tmp;
-catnum(1,:) = [];
-catnum(all(isnan(catnum),2),:) = [];
-
-% concatenate all words in a trial, remove empty spaces
-% use lower case to get a perfect match (because logfiles across participants are inconsistent e.g., india" and "India" both exist).
-catdata = catdata(1:2:end,:);      % remove every other line holding category numbers
-catwords = cell(size(catdata,1)/2, 1);
-for qq = 1:size(catdata,1)
-    %FIXME:(1) suppress warning from using strcat about truncating values
-    %      (2) how to remove square box appearing before sentence (eventho it doesn't affect results?)
-    catwords{qq} = lower(strcat(2,catdata{qq,2:end})); 
-end 
-
-% 2. match the set of experiment trials to entire set of trials
-% idx4expword is the index of the sent/seq in catwords, not the (audiofile) name of
-% the actual sentence/sequence
-idx4expword = [];  % prefer this than zeros(240,1) since some participants have <240 trials
-for qq = 1:size(expwords,1)
-    itmp     = strfind(catwords,expwords{qq});
-    idxsent  = find(not(cellfun('isempty',itmp)));
-    if isempty(idxsent)
-        warning('%d has no index',qq);  % indices of sentences in expword that don't have a match in catdata
+    idx = strfind(alltxt(:)', subjectname); % skip over the line of logfile that codes the extra 'empty word' 
+    if isempty(idx)
+       subjectnameL = lower(subjectname);  % for some subjects, the logfile has 'v1XXX' instead of 'V1XXX'
+       idx = strfind(alltxt(:)', subjectnameL);
     end 
-    idx4expword = [idx4expword; idxsent];  
-end
+    add = idx(end)+80;  
+    idx = [idx add];
+    alltxt = alltxt';
+    newtext = cell(numel(idx)-1,1);
+    for k = 1:numel(idx)-1
+        newtext{k} = alltxt(idx(k):idx(k+1)-1);  % easier to find relevant entry lines in txtfile
+    end
+
+    % log lines from logfile that contain a word that is actual stimuli
+    wordpresent = zeros(size(newtext));
+    for m = 1:size(newtext,1)
+        check   = regexp(newtext{m},'Picture\s\d');       % trigger WITH word
+        fix     = regexp(newtext{m},'Picture\s\F');       % fixation cross marks start of trial
+        space   = regexp(newtext{m},'Picture\s\d\s\s\s'); % trigger WITHOUT word
+        if ~isempty(check) && isempty(space) || ~isempty(fix)
+            wordpresent(m) = 1;
+        end        
+    end 
+    idxword = find(wordpresent == 1);
+    finaltext = newtext(idxword);  % all words in logfile, in chronological order
+
+    %% Concatenate words of the same sentence
+    expwords = cell(240,1);  % from the experimental logfile (expwords) concatenate words
+    cnt = 0;
+    for q = 1:size(finaltext,1)
+        wordbeg = regexp(finaltext{q},'Picture\s\d\s?\w','end');  % end = return index of last character specified i.e. '\w'
+        wordend = regexp(finaltext{q},'Picture\s\d\s?\w*','end');
+        word    = finaltext{q}(wordbeg:wordend);
+        if isempty(word)
+            cnt = cnt+1;
+            expwords{cnt,1} = '';
+            continue;
+        end
+        expwords{cnt,1} = lower(cat(2,expwords{cnt,1},word));
+    end 
+    % remove empty cells (some participants did not complete all 240 trials)
+    % code assumes that no trials are missing in between, but only consecutive
+    % trials, counting backwards from 240, e.g., participant didn't do last
+    % block, so last 5 trials are empty
+    expwords = expwords(~cellfun('isempty',expwords));
+    
+    %% match trials (usually 240) to excel file with all stimuli (800)
+    [catnum,~,catdata] = xlsread('home/language/nielam/MOUS_AnalysisNotes/GrammDist/Mous_categories.xls','','','basic'); 
+
+    % % Clean cat: shift 1st column down by one, then remove Nans and first row
+    tmp = catnum(:,1);
+    tmp = [false;tmp(1:end-1)];
+    catnum(:,1) = tmp;
+    catnum(1,:) = [];
+    catnum(all(isnan(catnum),2),:) = [];
+
+    % concatenate all words in a trial, remove empty spaces
+    % use lower case to get a perfect match (because logfiles across participants are inconsistent e.g., india" and "India" both exist).
+    catdata = catdata(1:2:end,:);      % remove every other line holding category numbers
+    catwords = cell(size(catdata,1)/2, 1);
+    for qq = 1:size(catdata,1)
+        %FIXME:(1) suppress warning from using strcat about truncating values
+        %      (2) how to remove square box appearing before sentence (eventho it doesn't affect results?)
+        catwords{qq} = lower(strcat(2,catdata{qq,2:end})); 
+    end 
+
+    % 2. match the set of experiment trials to entire set of trials
+    % idx4expword is the index of the sent/seq found in catwords, not the (audiofile) name of
+    % the actual sentence/sequence
+    idx4expword = [];  % prefer this than zeros(240,1) since some participants have <240 trials
+    removeidx   = [];
+    for qq = 1:size(expwords,1)
+        itmp     = strfind(catwords,expwords{qq});
+        idxsent  = find(not(cellfun('isempty',itmp)));
+        % if no match i.e. stimuli in logfile is wrong due to typo, don't add it to trialfun
+        % so far this has been for audfile ID 58, where last word "lachte" is spelled as "achte" 
+        if isempty(idxsent)    %log the trial(sent/seq) without a match
+            if isempty(removeidx)
+                removeidx = qq;
+            else 
+                removeidx = [removeidx; qq];
+            end
+        else 
+            idx4expword = [idx4expword; idxsent]; 
+        end 
+    end
+
 
 %% load trialfun_visual_word and add to it's trialinfo
-filename    = mous_db_getfilename(subjectname, 'meg_ds_task');
+fname       = mous_db_getfilename(subjectname, 'meg_ds_task');
 prestim     = 0.5; 
 poststim    = 3.0;
 wordType    = 'all';
 trialfun    = 'visual_word';
-[trl]       = mous_defineTrial(filename{1}, prestim, poststim, wordType, trialfun); 
+[trl]       = mous_defineTrial(fname{1}, prestim, poststim, wordType, trialfun); 
+
+% remove trial (sent/seq) from experiment(logfile) that doesn't have a
+% match with excel file. Otherwise, when concatenating between
+% trialfun_visual_word and word categories, it will not match up.
+
+if ~isempty(removeidx)
+    for nn = 1:numel(removeidx)
+        ridx = find(trl(:,4) == removeidx(nn));
+        trl(ridx,:) = [];
+    end 
+end 
 
 fileid = catnum(idx4expword);
 
-% length of each sentence/sequence based on trl matrix
+% length of each sentence/sequence based on trl matrix (prior to artifact detection)
 triallen = [];
 dumtrl = [trl; ones(size(trl,2),1)'];
 for kk = 2:size(dumtrl,1)
@@ -135,7 +175,7 @@ for kk = 2:size(dumtrl,1)
 end
 
 catvec = [];
-for mm = 1:size(idx4expword,1)
+for mm = 1:size(triallen,1)   % number of elements in triallen, not idx4expwords, because some ptps don't have all 240trials
     row = find(catnum(:,1) == fileid(mm));
     assignvec = catnum(row,2:end)';
     assignvec(all(isnan(assignvec),2),:) = [];
