2faad4e7a66a253b4c02060b315985b34c684a52
Annika Hulten <annika.hulten@mpi.nl>
2013-07-17 12:53:27 +0200

    removed wordType as a redundant variable
    Former-commit-id: de7b6b8abb40bfe9e7fa5f8b19050da1af2ac9e7

--- meg/analysis_scripts/mous_preprocessing_pipeline.m
+++ meg/analysis_scripts/mous_preprocessing_pipeline.m
@@ -1,16 +1,13 @@
 function mous_preprocessing_pipeline(subjectname)
+% Revamped from the old version by AH 12.7 2013 to be prettier
+% functionality remains unchanged.
 
 prestim = 0.2;  % default is 0.5;
 poststim = 1.0;
-downsampel2 = 300; 
+downsample2 = 300; 
 analysisType = 'ERF'; 
 % other options 'TFR':
 
-
-wordType = 'firstWord' ;   
-% other options:
-% 'all' 'target'; 'tarplusOne'; 'tarplusTwo';
-% to be implemented: 'nouns'; 'verbs' ; firstWord etc
 
 trialfun =  'auditory_word';   
 %            'auditory_sentence' 
@@ -29,7 +26,7 @@ filename    = mous_db_getfilename(subjectname, 'meg_ds_task');
 tmp = mous_db_getdata(subjectname, 'meg_artifact_cfg');
 
 
-[trl] = mous_defineTrial(filename{1}, prestim, poststim, wordType, trialfun);
+%[trl] = mous_defineTrial(filename{1}, prestim, poststim, wordType, trialfun);
 
 [trl] = mous_artifact_remove(trl, filename{1}, tmp);
 %dataStats = mous_samplestats(trl); %FIXME
@@ -38,4 +35,4 @@ data = mous_preprocessing(filename{1}, trl, downsample2, analysisType, prestim);
 
 length = ['0' num2str(prestim*10) '-' num2str(poststim)];
 
-mous_db_putdata(subjectname, ['meg_processed_{_preProcERF' trialfun '_' wordType length 'ds}'], 'data');
+mous_db_putdata(subjectname, ['meg_processed_{_preProcERF' trialfun '_' length 'ds}'], 'data');
