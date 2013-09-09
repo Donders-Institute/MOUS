410a60353a6b07af6ab5e04a9dd28a2a82594db5
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-09-09 22:01:12 +0200

    added function
    Former-commit-id: 1a7cbc0a72f4bc19eb0673b0a28e2697ca67f1be

---
+++ meg/analysis_functions/mous_mne_groupanalysis_parcellated.m
@@ -0,0 +1,51 @@
+function [stat,sent,seq,datsent,datseq] = mous_mne_groupanalysis_parcellated(subj, suffix, rootdir)
+
+if nargin==2
+  rootdir = '';
+end
+
+cfg              = [];
+cfg.parameter    = {'dspm';'pow'};
+cfg.parcellation = 'parcellation2';
+
+[p,f,e] = fileparts('mous_mne_groupanalysis_parcellated');
+sel     = strfind(p, '/');
+fname   = fullfile(p(1:sel),'templates','atlas_conte69_8196reg_LR');
+load(fname);
+
+% load in the data
+for k = 1:numel(subj)
+  mous_db_getdata(subj{k}, ['meg_mne_',suffix,'_Sent'], rootdir);
+  sourcep          = ft_sourceparcellate(cfg, source, atlas);
+  sent{k}          = sourcep;
+  
+  mous_db_getdata(subj{k}, ['meg_mne_',suffix,'_Seq'], rootdir);
+  sourcep          = ft_sourceparcellate(cfg, source, atlas);
+  seq{k}          = sourcep;
+end
+
+for k = 1:numel(subj)
+  sent{k}.dimord = sent{k}.powdimord;
+  seq{k}.dimord  = seq{k}.powdimord;
+end
+
+Nsubj = numel(sent);
+
+cfg = [];
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.design = [ones(1,Nsubj) ones(1,Nsubj)*2;1:Nsubj 1:Nsubj];
+cfg.ivar   = 1;
+cfg.uvar   = 2;
+cfg.numrandomization = 1000;
+cfg.parameter = 'dspm';
+stat = ft_timelockstatistics(cfg,sent{:},seq{:});
+
+datsent = zeros(size(stat.stat));
+datseq  = zeros(size(stat.stat));
+for k = 1:Nsubj
+  datsent = datsent+sent{k}.dspm;
+  datseq  = datseq+seq{k}.dspm;
+end
+datsent = datsent./Nsubj;
+datseq  = datseq./Nsubj;
