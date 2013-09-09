49525fbba570dd33685d22f720796a49ea8bd0d2
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-09-09 17:33:36 +0200

    updated
    Former-commit-id: 410769f97456a5bf20caf7e256910edb3ee6e6d9

--- meg/analysis_scripts/mous_corrmnebf_grpstat_roi2whole.m
+++ meg/analysis_scripts/mous_corrmnebf_grpstat_roi2whole.m
@@ -1,22 +1,43 @@
-function mous_corrmnebf_grpstat_roi2whole(param)
+function mous_corrmnebf_grpstat_roi2whole(subjectnames)
 
 % This function performs group-level statistics on the mne-bf matrix 
 % The function is written to interpolate only once (this is because interpolation is extremely time consuming).
 % Then for defined ROI,  the necessary dataset for statistical analyses are create (i.e. select ROI from interpolated matrix) 
 % Finally, montecarlo permutation is used for statistics to control for multiple comparisons (but not multiple tests, i.e. multiple ROIs)
 
-% corrmat is vertices by voxels!
+% non-interp'd correlation matrix = vox*vert
+% interpolate correlation matrix = vert*vox
+
+dostats = true;
+dodesc  = false;
 
 % define parameters for analyses
+  param.foi         = 16;        
+  param.toie        = [0.35 0.45];  % toi for ERFs
+  param.selfq       = [-0.12 -0.08];
+  param.suff        = num2str(param.foi);
+  param.savebf      = regexprep(num2str(mean(param.selfq(:))),'[.]','');
+  param.savemne     = regexprep([num2str(param.toie(1)) num2str(param.toie(2))],'[.]','');  
+  param.cdtn        = 'sen';  
+  param.cdtn2       = 'seq'; 
 
-% param.foi         = 16;        
-% param.toie        = [0.35 0.45];  % toi for ERFs
-% param.selfq       = [-0.12 -0.08];
-% param.suff        = num2str(foi);
-% param.savebf      = regexprep(num2str(mean(selfq(:))),'[.]','');
-% param.savemne     = regexprep([num2str(toie(1)) num2str(toie(2))],'[.]','');  
-% param.cdtn        = 'sen';
+  % files to use in interpolation
+  cfginterp = [];
+  cfginterp.cor = ['meg_corrmnebf_corVoxvert8mm_sdregwordord_jack_fisher_bf',param.savebf,'mne',param.savemne,'_',param.suff,'Hz_',param.cdtn];
+  cfginterp.erf = 'meg_anatomy_sourcemodel2D';
+  cfginterp.tfr = ['meg_corrmnebf_bfsourcesingletrial8mm_fisher_bf',param.savebf,'mne',param.savemne,'_',param.suff,'Hz_',param.cdtn];
 
+if isfield(param,'cdtn2')
+  cfginterp2 = cfginterp;
+  cfginterp2.cor = ['meg_corrmnebf_corVoxvert8mm_sdregwordord_jack_fisher_bf',param.savebf,'mne',param.savemne,'_',param.suff,'Hz_',param.cdtn2];
+  cfginterp2.tfr  = ['meg_corrmnebf_bfsourcesingletrial8mm_fisher_bf',param.savebf,'mne',param.savemne,'_',param.suff,'Hz_',param.cdtn2];
+end 
+
+%% randomly pick half the subjects for discovery  (other half for replication)
+% ninsubj = numel(subjectnames);
+% tmp = randperm(ninsubj);
+% tmp = tmp(1:floor(ninsubj/2));
+% subjectnames = subjectnames(tmp);
 
 if dostats
     %% define ROIs
@@ -34,12 +55,12 @@ if dostats
     roi(2).cdtn = param.cdtn;
     roi(2).area = 'Rparietal2';
 
-    roi(3).x = 5; 
-    roi(3).y = 17;
-    roi(3).z = 17;
+    roi(3).x = 7; 
+    roi(3).y = 20;
+    roi(3).z = 16;
     roi(3).seed = 'TFRseed';
     roi(3).cdtn = param.cdtn;
-    roi(3).area = 'Rfrontal2';
+    roi(3).area = 'Lfrontal2';
 
     roi(4).x = 5;
     roi(4).y = 17;
@@ -55,115 +76,114 @@ if dostats
     roi(5).cdtn = param.cdtn;
     roi(5).area = 'LIFG2';
     
-    for q = 1:numel(roi)
-        allsdata(q).cdtnone = cell(numel(subjectnames),1);
-        allsdata(q).cdtntwo = cell(numel(subjectnames),1);
+    for rcnt = 1:numel(roi)
+        allsdata(rcnt).cdtnone = cell(numel(subjectnames),1);  % sen
+        allsdata(rcnt).cdtntwo = cell(numel(subjectnames),1);  % seq
     end 
-
-    %% subjects
-    subjectnames = {'V1001' 'V1002' 'V1003' 'V1004' 'V1005' 'V1007' 'V1008' ...
-                     'V1010' 'V1011' 'V1012' 'V1013' 'V1015' 'V1016' 'V1017' 'V1019'...
-                     'V1020' 'V1021' 'V1022' 'V1023' 'V1024' 'V1025' 'V1026' 'V1027' 'V1028'...
-                     'V1030' 'V1031' 'V1032' 'V1036' 'V1037' ...
-                     'V1040' 'V1044' 'V1045' 'V1049'...
-                     'V1050' 'V1052' 'V1053' 'V1054' 'V1055' 'V1057' 'V1058' 'V1059'...
-                     'V1061' 'V1062' 'V1064' 'V1065' 'V1066' 'V1068' 'V1069'... 
-                     'V1071' 'V1073' 'V1074' 'V1076' 'V1077' 'V1079'...
-                     'V1080' 'V1081' 'V1083' 'V1084' 'V1085' 'V1087' 'V1088' 'V1089'...
-                     'V1090' 'V1092'  'V1095' 'V1099'...
-                     'V1100' 'V1102' 'V1103' 'V1104' 'V1106' 'V1107'};   
-
-
+    
      %% get sourcemodel (grid)        
     fname = '/home/language/nielam/MOUS/meg/templates/sourcemodel/standard_sourcemodel3d8mm';
     load(fname);
-    if ~isempty(foi)  
-       mous_db_getdata('V1036',['meg_corrmnebf_bfsourcesingletrial8mm_bf',savebf,'mne',savemne,'_',suff,'Hz_',cdtn]);
-    else
-       mous_db_getdata('V1036',['meg_corrmnebf_bfsourcesingletrial8mm_',savebf,'_',cdtn]);
-    end
+    mous_db_getdata('V1036',['meg_corrmnebf_bfsourcesingletrial8mm_fisher_bf',param.savebf,'mne',param.savemne,'_',param.suff,'Hz_',param.cdtn]);
 
     % adjust sourcemodel size from [1 x 5798] to [1 x 5782], then limit the inside sources
-    sourcemodel.insideold = sourcemodel.inside;
+    % sourcemodel.insideold = sourcemodel.inside;
     sourcemodel.inside    = source.inside;
     sourcemodel.outside   = setdiff(1:size(sourcemodel.pos,1), source.inside);
     if isfield(sourcemodel,'cfg')
         sourcemodel = rmfield(sourcemodel,'cfg');
     end
-    
-    %% load data for each individual subject and select ROI from corrmat
-    cfginterp = [];
-    cfginterp.cor = ['meg_corrmnebf_corVoxvert8mm_sdregwordord_jack_bf',savebf,'mne',savemne,'_',suff,'Hz_',cdtn];
-    cfginterp.erf = ['meg_corrmnebf_mnesingletrial_jack_bf',savebf,'mne',savemne,'_',suff,'Hz_',cdtn];
-    cfginterp.tfr = ['meg_corrmnebf_bfsourcesingletrial8mm_bf',savebf,'mne',savemne,'_',suff,'Hz_',cdtn];
+    rmf = {'xgrid','ygrid','zgrid','unit'};
+    sourcemodel = rmfield(sourcemodel,rmf);
 
-    % k = subj count
-    % q = roi count 
-    
-    for k = 1:numel(subjectnames)
-        % interpolate
-        % [source3d, singlegrid] = mous_corrmnebf_interpolate(subjectnames{k},cfginterp);  % output is verts X voxels
-        source3d = mous_corrmnebf_interpolate(subjectnames{k},cfginterp);  % output is verts X voxels
-        if isfield(source3d, 'cfg')
-            source3d = rmfield(source3d,'cfg');
+    %% load data for each individual subject and select ROI from corrmat
+    Nsubj = numel(subjectnames);
+    for scnt = 1:Nsubj
+        
+        [source3dsenori] = mous_corrmnebf_interpolate(subjectnames{scnt},cfginterp);  % output is verts X voxels
+        if isfield(source3dsen, 'cfg')
+            source3dsen = rmfield(source3dsen,'cfg');
+        end
+        
+        [source3dseq] = mous_corrmnebf_interpolate(subjectnames{scnt},cfginterp2);  % output is verts X voxels
+        if isfield(source3dseq, 'cfg')
+            source3dseq = rmfield(source3dseq,'cfg');
         end
 
         load /home/language/nielam/MOUS_AnalysisNotes/corrmnebf/corrmnebf_wholeheadcoord.mat
+        load /home/language/nielam/MOUS_AnalysisNotes/corrmnebf/corrmnebf_dof_101Subj.mat
         
-        for q = 1:numel(roi)
+        for rcnt = 1:numel(roi)
         % select necessary data for stats for each ROI
                      
             dum=zeros(stat.dim);
-            sub2ind(stat.dim,roi(q).x,roi(q).y,roi(q).z);
-            dum(roi(q).x-1:roi(q).x+1,roi(q).y-1:roi(q).y+1,roi(q).z-1:roi(q).z+1)=1;
+            sub2ind(stat.dim,roi(rcnt).x,roi(rcnt).y,roi(rcnt).z);
+            dum(roi(rcnt).x-1:roi(rcnt).x+1,roi(rcnt).y-1:roi(rcnt).y+1,roi(rcnt).z-1:roi(rcnt).z+1)=1;
             sel = find(dum);
             idxROI = find(ismember(sourcemodel.inside,sel));
 
-            if strcmp(roi(q).seed,'TFRseed')   %% Osc as source
+            if strcmp(roi(rcnt).seed,'TFRseed')   
+               tmp  = source3dsen.corrmat(:,idxROI); % select TFR seed and average across ERF voxel (rows))
+               tmp  = nanmean(tmp,2);            
+
+               tmp2 = source3dseq.corrmat(:,idxROI);
+               tmp2 = nanmean(tmp2,2);
                 
-                tmp = source3d.corrmat(:,idxROI);
-                tmp = nanmean(tmp,2);                 % average across vertices; sometimes, a certain column = NaNs
+               allsdata(rcnt).cdtnone{scnt} = sourcemodel;
+               allsdata(rcnt).cdtnone{scnt}.avg.pow = zeros(1,11000);
+               allsdata(rcnt).cdtnone{scnt}.avg.pow(sourcemodel.inside) = tmp;
+               allsdata(rcnt).cdtnone{scnt}.inside(dof(:,1) < Nsubj) = [];  % .inside determines which voxels are tested for stats
+               allsdata(rcnt).cdtnone{scnt}.outside = setdiff(1:11000,allsdata(rcnt).cdtnone{scnt}.inside);
                 
-%                 sdata{k} = sourcemodel;
-%                 sdata{k}.avg.pow = zeros(1,11000);
-%                 sdata{k}.avg.pow(sourcemodel.inside) = tmp;
-%                 
-%                 sdata2{k} = sdata{k};
-%                 sdata2{k}.avg.pow(:) = 0;
-                
-               allsdata(q).cdtnone{k} = sourcemodel;
-               allsdata(q).cdtnone{k}.avg.pow = zeros(1,11000);
-               allsdata(q).cdtnone{k}.avg.pow(sourcemodel.inside) = tmp;
-                
-               allsdata(q).cdtntwo{k} = allsdata(q).cdtnone{k}; 
-               allsdata(q).cdtntwo{k}.avg.pow(:) = 0;
+               allsdata(rcnt).cdtntwo{scnt} = allsdata(rcnt).cdtnone{scnt}; 
+               allsdata(rcnt).cdtntwo{scnt}.avg.pow(sourcemodel.inside) = tmp2;
+               % dof info already copied from cdtnone, no need to assign again
                 
                 
-            elseif strcmp(seed,'ERFseed')      %% erfs as source
-               tmp = source3d.corrmat(idxROI,:);       % specify vertices of interest
-               tmp = nanmean(tmp,1);                      % average across voxels
+            elseif strcmp(roi(rcnt).seed,'ERFseed')      
+               tmp = source3dsen.corrmat(idxROI,:);     % specify ERF seed and average across TFR voxel (columns))
+               tmp = nanmean(tmp,1);          
                
-               allsdata(q).cdtnone{k} = sourcemodel;
-               allsdata(q).cdtnone{k}.avg.pow = zeros(1,11000);
-               allsdata(q).cdtnone{k}.avg.pow(sourcemodel.inside) = tmp;
-                
-               allsdata(q).cdtntwo{k} = allsdata(q).cdtnone{k}; 
-               allsdata(q).cdtntwo{k}.avg.pow(:) = 0;
+               tmp2 = source3dseq.corrmat(idxROI,:);     % specify ERF seed and average across TFR voxel (columns))
+               tmp2 = nanmean(tmp2,1);     
                
+               allsdata(rcnt).cdtnone{scnt} = sourcemodel;
+               allsdata(rcnt).cdtnone{scnt}.avg.pow = zeros(1,11000);
+               allsdata(rcnt).cdtnone{scnt}.avg.pow(sourcemodel.inside) = tmp;
+               allsdata(rcnt).cdtnone{scnt}.inside(dof(:,1) < Nsubj) = [];
+               allsdata(rcnt).cdtnone{scnt}.outside = setdiff(1:11000,allsdata(rcnt).cdtnone{scnt}.inside); 
+               
+               allsdata(rcnt).cdtntwo{scnt} = allsdata(rcnt).cdtnone{scnt}; 
+               allsdata(rcnt).cdtntwo{scnt}.avg.pow(sourcemodel.inside) = tmp2;
+               % dof info copied from cdtnone, no need to assign again
             end
         end 
     end 
     
+    dumdata = allsdata(1).cdtnone(:);  % create one dummy variable to compare against sen or seq.
+    for k = 1:numel(allsdata(1).cdtnone)
+      dumdata{k}.avg.pow(:) = 0;
+    end 
+    
       %% montecarlo permutation statistics
       
-    for qq = 1:numel(roi)
+    for qq = 1:numel(roi)*3  % 3 comparisons (svs, sen v. 0, seq v. 0) for each ROI
         
-        % assign data structure for analysis
-        sdat1 = allsdata(qq).cdtnone;
-        sdat2 = allsdata(qq).cdtntwo;
-        
+        if qq < 6                 % svs comparison 
+          sdat1 = allsdata(qq).cdtnone;
+          sdat2 = allsdata(qq).cdtntwo;
+          statsavename = ['meg_corrmnebf_groupStats_',roi(qq).area,'_',roi(qq).seed,'_svs_',num2str(Nsubj)];
+        elseif qq > 5 && qq < 11  % sent vs. 0
+          sdat1 = allsdata(qq-5).cdtnone;
+          sdat2 = dumdata;
+          statsavename = ['meg_corrmnebf_groupStats_',roi(qq-5).area,'_',roi(qq-5).seed,'_sen_',num2str(Nsubj)];
+        elseif qq > 10            % seq vs. 0
+          sdat1 = allsdata(qq-10).cdtntwo;
+          sdat2 = dumdata;
+          statsavename = ['meg_corrmnebf_groupStats_',roi(qq-10).area,'_',roi(qq-10).seed,'_seq_',num2str(Nsubj)];
+        end       
+                
         % do stats       
-        Nsubj = numel(subjectnames);
         cfg = [];
         cfg.method = 'montecarlo';
         cfg.statistic = 'depsamplesT';
@@ -178,7 +198,7 @@ if dostats
         cfg.clusterthreshold = 'nonparametric_individual'; % is "nonpar_common" determined across all subjects?
         stat2 = ft_sourcestatistics(cfg, sdat1{:}, sdat2{:}); 
 
-        mous_db_putdata('groupresults',['meg_corrmnebf_groupStats_',ROI,'_',seed,'_',cdtn,'_72'],'stat2');
+        mous_db_putdata('groupresults_N',statsavename,'stat2');
     end 
  
 end % end dostats
@@ -189,7 +209,7 @@ if dodesc
     seed = 'ERFseed';  % 'ERFseed'
     cdtn = 'sent'; 
     
-    mous_db_getdata('groupresults',['meg_corrmnebf_groupStats_',ROI,'_',seed,'_',cdtn,'_72']);
+    mous_db_getdata('groupresults',statsavename);
     
     if ndims(stat2.stat)>2 %i.e. being a 3d matrix, rather than space x something else
       stat2.stat=stat2.stat(:);
@@ -205,25 +225,25 @@ if dodesc
     cfg.method      = 'slice';  %ortho
     cfg.funparameter = 'pow';
     cfg.funcolorlim = [-5 5];
+    cfg.title       = 'test';
     % superimpose ROIs onto afni brain atlas
-    cfg.title = 'hi';
-    cfg.atlas='/home/common/matlab/fieldtrip/template/atlas/afni/TTatlas+tlrc.BRIK';
+    % cfg.atlas='/home/common/matlab/fieldtrip/template/atlas/afni/TTatlas+tlrc.BRIK';
     ft_sourceplot(cfg,i1);
     
     row = max(length(stat2.posclusters),length(stat2.negclusters));
     statval = zeros(row,4);
     for k = 1:numel(stat2.posclusters)
         if k > length(stat2.negclusters)
-            statvalpos(k,3) = NaN;        % negative p-value
-            statvalpos(k,4) = NaN; % t-value
+            statval(k,3) = NaN;        % negative p-value
+            statval(k,4) = NaN; % t-value
         elseif k > length(stat2.negclusters) 
-            statvalpos(k,1) = NaN;        % positive cluster p-value
-            statvalpos(k,2) = Nan; 
+            statval(k,1) = NaN;        % positive cluster p-value  
+            statval(k,2) = Nan; 
         else        
-            statvalpos(k,1) = stat2.posclusters(k).prob;        % positive cluster p-value
-            statvalpos(k,2) = stat2.posclusters(k).clusterstat; % t-value
-            statvalpos(k,3) = stat2.negclusters(k).prob;        % negative p-value
-            statvalpos(k,4) = stat2.negclusters(k).clusterstat; % t-value
+            statval(k,1) = stat2.posclusters(k).prob;        % positive cluster p-value
+            statval(k,2) = stat2.posclusters(k).clusterstat; % t-value
+            statval(k,3) = stat2.negclusters(k).prob;        % negative p-value
+            statval(k,4) = stat2.negclusters(k).clusterstat; % t-value
         end 
     end 
     
@@ -236,12 +256,12 @@ if dodesc
         nclust = 1;
         value  = 'neg'; % 'neg'
         %title  = [ROI,' ',seed,' ', cdtn,' ', value,' clust 1'];
-        mous_corrmnebf_selstatclus(nclust,stat2,value,[])
+        mous_corrmnebf_selstatclus(nclust,stat2,value,[]);
         
         nclust = 1;
         value  = 'pos'; % 'neg'
         %title  = [ROI,' ',seed,' ', cdtn,' ', value,' clust 1'];
-        mous_corrmnebf_selstatclus(nclust,stat2,value,[])
+        mous_corrmnebf_selstatclus(nclust,stat2,value,[]);
 
 
     % summary of t- and p-values of all clusters 
