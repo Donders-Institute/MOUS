aa7c81c93e35e67fe6030cb3bc2004dffd7385ca
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-09-11 16:24:07 +0200

    updated to check for number of questions given'

--- meg/trialfun/mous_visual_logfileanswers.m
+++ meg/trialfun/mous_visual_logfileanswers.m
@@ -1,32 +1,54 @@
 function [output] = mous_visual_logfileanswers(subjectname)
 
 %% Read in logfile
+% In the logfile:
+% Response 1 &  Response 2 are responses from the subject
+% Response 3 -  is the experimenter pressing the space bar
 [oritxt] = read_logfile_visual(subjectname);
 
-% get indices of logfile that contains 'Response' which codes the
-% response to question of the current trial (sent/seq)
+%% get relevant lines of logfile
+% retrieve indices of logfile that logs whether subjects response is
+% correct ('hit') or incorrect ('incorrect').
 wordpresent = zeros(size(oritxt));
 for m = 1:size(oritxt,1)
-    check = regexp(oritxt{m},'Question\d\d');  % check if text of interest exists
+    check = regexp(oritxt{m},'QUESTION \d\d');  % check if text of interest exists
     if ~isempty(check)
         wordpresent(m) = 1;
     end 
 end 
-idxq = find(wordpresent == 1);
-idxr = idq + 1;
-onlyresp = oritxt(idxq); % only parts of logfile that contain responses;
 
-% In the logfile:
-% Response 1 &  Response 2 - these are fine
-% Response  3 - do not code this because it is the experimenter pressing
-% the space bar, nothing to do with the subject
+idxq  = find(wordpresent == 1);% index of logfile with "QUESTION... hit/incorrect" 
+%idxr = idq + 1;               % index of responses
+resp  = oritxt(idxq);          % lines in logfile that only contain "QUESTION..." 
+
+% checks for a complete number of questions
+% if <48 find out why e.g., subject was only presented 23 blocks / dsq
+% crash...etc
+if numel(resp) < 48
+    warning('there are less than %d questions in the this logfile',numel(resp));
+
+cor     = zeros(24,1);
+incor   = zerps(24,1);
+for m = 1:size(oritxt,1)
+    checkcor = regexp(oritxt{m},'hit');
+    checkincor = regexp(oritxt{m},'incorrect');
+    if ~isempty(checkcor)
+        checkcor(m) = 1;
+    end
+    if ~isempty(checkincor)
+        checkincor(m) = 1;
+    end
+end
+    
+
+
+%% organise information
+% number & percentage correct
+
+
+% number & percentage incorrect
+
 
 
 end 
 
-
-%  the following was found for the first question in V1106; see if the num
-%  spaces is regular  across Questions in the logfile.
-%% Response <spaces> X 
-% X = response on button box (1 or 2)
-% number of spaces = 19  
