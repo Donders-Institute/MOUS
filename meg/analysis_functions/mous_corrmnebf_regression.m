7d5732716823ae285fef955e22079310a8495dc4
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-10-16 13:47:24 +0200

    previous version didn't work properly, changes made, and order of input arguments changed
    Former-commit-id: 49dc3dec7a5b0370fa6b7db6914afac11b1d166d

--- meg/analysis_functions/mous_corrmnebf_regression.m
+++ meg/analysis_functions/mous_corrmnebf_regression.m
@@ -1,31 +1,38 @@
-function [cleanVoxM, cleanVertM] = mous_corrmnebf_regression(voxM, vertM,trialinfo,numbin)
+function [cleanVoxM, cleanVertM] = mous_corrmnebf_regression(trialinfo,voxM,vertM,numbin)
 %% configure design matrix FOR ONE VERTEX
     % r1 = intercept
     % r2 = trialinfo (word order)
     % ** leave out: observations for vertex y  **
-  
-if nargin == 3 % regress out individual (standardised) word order for each trial(word)
 
+% voxM, vertM, individual trials         OR  voxM/VertM, individual trials
+if nargin == 3  && size(trialinfo,2) > 1 || nargin == 2 && size(trialinfo,2) > 1
     linword     = trialinfo(:,5)';    
     quadword    = trialinfo(:,5).^2';
-    DM = ones(3,size(trialinfo,1)); 
+    DM = ones(3,size(trialinfo,1));
 
+% % voxM, vertM, 
+% elseif nargin == 3 && size(trialinfo,2) == 1
+%     linword     = trialinfo;   
+%     quadword    = trialinfo.^2';
+%     DM = ones(3,size(trialinfo,1));
+     
+% voxM, vertM, binning trials
 elseif nargin == 4 % regress out average word order for each bin  
     linword     = nanmean(trialinfo);   % average word order of each bin
     quadword    = nanmean(trialinfo.^2);
     DM = ones(3,numbin);
 end
 
-    linmean     = nanmean(linword);
-    linsd       = nanstd(linword);
-    linword     = (linword-linmean)./linsd;
+linmean     = nanmean(linword);
+linsd       = nanstd(linword);
+linword     = (linword-linmean)./linsd;
 
-    quadmean    = nanmean(quadword);
-    quadsd      = nanstd(quadword);
-    quadword    = (quadword-quadmean)./quadsd;  
-    
-    DM(2,:) = linword;      % linear
-    DM(3,:) = quadword;   % quadratic 
+quadmean    = nanmean(quadword);
+quadsd      = nanstd(quadword);
+quadword    = (quadword-quadmean)./quadsd;  
+
+DM(2,:) = linword;      % linear
+DM(3,:) = quadword;   % quadratic 
 
 
 %% Get Beta Weights
@@ -40,10 +47,17 @@ end
 % Zclean = Z - betaZ * X
 % y = b*x, therefore, b = y/x
 
-betaVox = voxM/DM;     
-betaVert = vertM/DM;  
+if nargin == 3 || 4
+    betaVox = voxM/DM;     
+    betaVert = vertM/DM;  
 
-cleanVoxM = voxM - betaVox*DM;
-cleanVertM = vertM - betaVert*DM; 
+    cleanVoxM = voxM - betaVox*DM;
+    cleanVertM = vertM - betaVert*DM; 
+
+elseif nargin == 1  % written as voxM, but if first inarg is given as vertM, then this is fine since the same computation is done for voxM and vertM
+    betaVox = voxM/DM;     
+    cleanVoxM = voxM - betaVox*DM;
+end 
+
 
 
