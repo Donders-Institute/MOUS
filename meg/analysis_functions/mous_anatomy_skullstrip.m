1ef7a6e90413d70a848390eaccd7c970c7eed7d6
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-10-23 15:19:43 +0200

    updated code to accommodate rootdir, and updated fsl version to use
    Former-commit-id: c12b181befbdef5409d3e3ab313d2e54a5cc73aa

--- meg/analysis_functions/mous_anatomy_skullstrip.m
+++ meg/analysis_functions/mous_anatomy_skullstrip.m
@@ -1,4 +1,4 @@
-function [seg, mask] = mous_anatomy_skullstrip(subjectname, threshold, center)
+function [seg, mask] = mous_anatomy_skullstrip(subjectname, threshold, center, rootdir)
 
 % [SEG, MASK] = MOUS_ANATOMY_SKULLSTRIP computes the skullstripped MRI based on
 % the subject's anatomical MRI (coregMNI)
@@ -11,10 +11,10 @@ function [seg, mask] = mous_anatomy_skullstrip(subjectname, threshold, center)
 %cfg.smooth = 2;
 %seg = ft_volumesegment(cfg, mri);
 
-d   = '/home/language/annhul/MOUS/meg/';
-str = ['/opt/fsl/bin/bet ',d,subjectname,'/anatomy/',subjectname,'coregMNI.nii ',d,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip '];
+d   = rootdir;
+str = ['/opt/fsl_5.0.4/bin/bet ',d,filesep,subjectname,'/anatomy/',subjectname,'coregMNI.nii ',d,filesep,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip '];
 str = [str,'-R -f ',num2str(threshold),' -c ', num2str(center),' -g 0 -m -v'];
 
 system(str);
-seg  = ft_read_mri([d,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip.nii.gz']);
-mask = ft_read_mri([d,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip_mask.nii.gz']);
+seg  = ft_read_mri(fullfile(d,subjectname,'anatomy',[subjectname,'coregMNIskullstrip.nii.gz']));
+mask = ft_read_mri(fullfile(d,subjectname,'anatomy',[subjectname,'coregMNIskullstrip_mask.nii.gz']));
