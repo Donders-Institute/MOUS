025266f573887296cc55b71bc3148710e2ca520a
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-11-04 10:12:10 +0100

    fixed typo

--- meg/analysis_functions/mous_mne_groupanalysis_parcellated.m
+++ meg/analysis_functions/mous_mne_groupanalysis_parcellated.m
@@ -115,9 +115,9 @@ cfg.channel = sent{1}.label(setdiff(1:numel(sent{1}.label),[1 2 44 45])); % remo
 stat = ft_timelockstatistics(cfg,sent{:},seq{:});
 
 datsent = zeros(size(sent{1}.(param)));
-cntsent = zeros(1,size(datsent,2));
+cntsent = zeros(size(datsent,2),1);
 datseq  = zeros(size(sent{1}.(param)));
-cntseq  = zeros(1,size(datseq,2));
+cntseq  = zeros(size(datseq,2),1);
 for k = 1:Nsubj
   tmpsent = sent{k}.(param);
   tmpseq  = seq{k}.(param);
