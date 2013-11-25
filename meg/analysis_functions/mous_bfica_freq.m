cabf41101c3181f1536aec40d48ecabebd828f3a
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-11-25 13:06:11 +0100

    reorganised: build in flexibility for subjects with more than one dataset;  old code removed, inarg options moved downstream (right before freqanalysis code);

--- meg/analysis_functions/mous_bfica_freq.m
+++ meg/analysis_functions/mous_bfica_freq.m
@@ -13,46 +13,46 @@ if nargin<4
   options.t_ftimwin  = 0.25;
   options.resamplefs = 200;
 else
+  % don't define anything but rely on how it's dealt with below
 end
-if isfield(options, 'tapsmofrq')
-  tapsmofrq = options.tapsmofrq;
-else
-  tapsmofrq = 8;
-end
-if isfield(options, 't_ftimwin')
-  t_ftimwin = options.t_ftimwin;
-else
-  t_ftimwin = 0.250;
-end
-if isfield(options, 'resamplefs')
-  resamplefs = options.resamplefs;
-else
-  resamplefs = 200;
-end
-if isfield(options, 'taper')
-  taper = options.taper;
-else
-  taper = 'dpss';
-end
-% get data and apply artifact rejection
+
 dataset   = mous_db_getfilename(subjectname, 'meg_raw_task');
-artfctcfg = mous_db_getdata(subjectname, 'meg_artifact_cfg');
-comp      = mous_db_getdata(subjectname, 'meg_bfica_comp', rootdir);
+if numel(dataset)>1
+  for k = 1:numel(dataset)
+    tmpdataset   = dataset{k};
+    tmpartfctcfg = mous_db_getdata(subjectname, ['meg_artifact_cfg_pt',num2str(k)]);
+    tmpcomp      = mous_db_getdata(subjectname, 'meg_bfica_comp', rootdir);   
+    tmpdata      = compute_data(tmpdataset, tmpartfctcfg, tmpcomp);
+    if k==1,
+      data = tmpdata;
+    else
+      data = ft_appenddata([], data, tmpdata);
+    end
+  end
+else
+  artfctcfg = mous_db_getdata(subjectname, 'meg_artifact_cfg');
+  comp      = mous_db_getdata(subjectname, 'meg_bfica_comp', rootdir);
+  data      = compute_data(dataset{1}, artfctcfg, comp);
+end
+freq = compute_freq(data, options);
+
+function data = compute_data(dataset, artfctcfg, comp)
 
 avgcomp   = comp{1};
 avgpre    = comp{2};
 comp      = comp{3};
 
+% get data and apply artifact rejection
 cfg          = [];
-cfg.dataset  = dataset{1};
+cfg.dataset  = dataset;
 %cfg.trialfun = 'trialfun_visual_sentence';
 cfg.trialfun = 'trialfun_visual_word';
 cfg.trialdef.prestim  = 0.3;
 cfg.trialdef.poststim = 'nextword';
 cfg          = ft_definetrial(cfg);
 trl          = cfg.trl;
-%trl          = mous_artifact_remove(trl, dataset{1}, artfctcfg([1 3 4]), 'partial', 1); % don't do the horizontal EOG
-trl          = mous_artifact_remove(trl, dataset{1}, artfctcfg([1 2 3 4]), 'partial', 1); % don't do the horizontal EOG
+%trl          = mous_artifact_remove(trl, dataset, artfctcfg([1 3 4]), 'partial', 1); % don't do the horizontal EOG
+trl          = mous_artifact_remove(trl, dataset, artfctcfg([1 2 3 4]), 'partial', 1); % don't do the horizontal EOG
 dataStats    = mous_samplestats(trl);
 
 % trl > 2 second does not make sense, sanity check: FIXME
@@ -60,7 +60,7 @@ nsmp = trl(:,2)-trl(:,1);
 trl  = trl(nsmp<2400,:);
 
 cfg            = [];
-cfg.dataset    = dataset{1};
+cfg.dataset    = dataset;
 cfg.trl        = trl;
 cfg.continuous = 'yes';
 cfg.demean     = 'yes';
@@ -89,45 +89,13 @@ cfg           = [];
 cfg.component = find(v>0.1);
 data          = ft_rejectcomponent(cfg, comp, data);
 
-% if frequency>=20 && frequency < 30
-%   % spectral analysis
-%   cfg = [];
-%   cfg.method = 'mtmconvol';
-%   cfg.output = 'fourier';
-%   %cfg.toi    = 0.125:0.025:0.625;
-%   cfg.toi    = -0.2:0.05:0.8;
-%   cfg.foi    = frequency;
-%   cfg.t_ftimwin = 0.250;
-%   %cfg.tapsmofrq = 8;
-%   cfg.taper = 'hanning';
-%   cfg.pad    = 10;
-%   freq       = ft_freqanalysis(cfg, data);
-% elseif frequency>=30
-%   % spectral analysis
-%   cfg = [];
-%   cfg.method = 'mtmconvol';
-%   cfg.output = 'fourier';
-%   %cfg.toi    = 0.125:0.025:0.625;
-%   cfg.toi    = -0.2:0.05:0.8;
-%   cfg.foi    = frequency;
-%   cfg.t_ftimwin = t_ftimwin;
-%   cfg.tapsmofrq = tapsmofrq;
-%   cfg.pad    = 4;
-%   freq       = ft_freqanalysis(cfg, data);
-% else
-%   % spectral analysis
-%   cfg = [];
-%   cfg.method = 'mtmconvol';
-%   cfg.output = 'fourier';
-%   cfg.toi    = 0.2:0.025:0.6;
-%   cfg.foi    = frequency;
-%   cfg.t_ftimwin = 0.40;
-%   %cfg.tapsmofrq = 8;
-%   cfg.taper  = 'hanning';
-%   cfg.pad    = 1;
-%   freq       = ft_freqanalysis(cfg, data);
-% end
-% spectral analysis
+function freq = compute_freq(data, options)
+
+tapsmofrq  = ft_getopt(options, 'tapsmofrq', 8);
+t_ftimwin  = ft_getopt(options, 't_ftimwin', 0.250);
+resamplefs = ft_getopt(options, 'resamplefs', 200);
+taper      = ft_getopt(options, 'taper', 'dpss');
+
 cfg        = [];
 cfg.method = 'mtmconvol';
 cfg.output = 'fourier';
