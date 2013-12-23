d4cded796cf1ea0b4f641377778e863da2859c78
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-12-23 12:57:28 +0100

    function that hardcode filenames of subjects with >1 dataset, and one dataset is too small to be considered as task data within mous_db_getfilename.m
    Former-commit-id: afba04dbc725df830ed2ee012d115dd6b2e89de3

---
+++ meg/analysis_functions/private/datasets_exceptions.m
@@ -0,0 +1,15 @@
+function filename = datasets_exceptions(subject,templatepath)
+% datasets_exceptions hardcodes filenames for subjects with more than one
+% task dataset, in which one dataset is much smaller than the other.
+
+% taking inarg from mous_db_getfilename so that if rootdir changes again
+% we don't need to change this function
+[p,n,e] = fileparts(templatepath); 
+switch subject
+    case 'A2062'
+        filename(1).name = [n(1:end-1),'2',e];
+        filename(2).name = [n(1:end-1),'3',e];
+    case 'A2063'
+        filename(1).name = [n(1:end-1),'2',e];
+        filename(2).name = [n(1:end-1),'3',e];
+end
