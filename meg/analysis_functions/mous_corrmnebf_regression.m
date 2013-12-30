e8da194ee9139aa945911b9ad9e33b48d5545bcb
Nietzsche Ho Lok Lam <nielam@mentat003.dccn.nl>
2013-12-30 19:36:39 +0100

    updated to be suitable for corrbfbf and corrmnebf. improved variable names
    Former-commit-id: ba3085345f76e20dc83a95f8f9ed442ce78c9ee7

--- meg/analysis_functions/mous_corrmnebf_regression.m
+++ meg/analysis_functions/mous_corrmnebf_regression.m
@@ -1,22 +1,18 @@
-function [cleanVoxM, cleanVertM] = mous_corrmnebf_regression(trialinfo,voxM,vertM,numbin)
+function [cleanMat1, cleanMat2] = mous_corrmnebf_regression(trialinfo,mat1,mat2,numbin)
 %% configure design matrix FOR ONE VERTEX
     % r1 = intercept
     % r2 = trialinfo (word order)
     % ** leave out: observations for vertex y  **
 
-% voxM, vertM, individual trials         OR  voxM/VertM, individual trials
+% 2 matrices, remove word order from individual trials         
+%              OR 
+% 1 matrix, remove word order from individual trials
 if nargin == 3  && size(trialinfo,2) > 1 || nargin == 2 && size(trialinfo,2) > 1
     linword     = trialinfo(:,5)';    
     quadword    = trialinfo(:,5).^2';
     DM = ones(3,size(trialinfo,1));
-
-% % voxM, vertM, 
-% elseif nargin == 3 && size(trialinfo,2) == 1
-%     linword     = trialinfo;   
-%     quadword    = trialinfo.^2';
-%     DM = ones(3,size(trialinfo,1));
-     
-% voxM, vertM, binning trials
+    
+% 2 matrices, removed averaged word order for each set of binned trials
 elseif nargin == 4 % regress out average word order for each bin  
     linword     = nanmean(trialinfo);   % average word order of each bin
     quadword    = nanmean(trialinfo.^2);
@@ -47,17 +43,36 @@ DM(3,:) = quadword;   % quadratic
 % Zclean = Z - betaZ * X
 % y = b*x, therefore, b = y/x
 
-if nargin == 3 || 4
-    betaVox = voxM/DM;     
-    betaVert = vertM/DM;  
+if nargin == 3 || nargin == 4
+    beta1 = mat1/DM;     
+    beta2 = mat2/DM;  
 
-    cleanVoxM = voxM - betaVox*DM;
-    cleanVertM = vertM - betaVert*DM; 
+    cleanMat1 = mat1 - beta1*DM;
+    cleanMat2 = mat2 - beta2*DM; 
 
-elseif nargin == 1  % written as voxM, but if first inarg is given as vertM, then this is fine since the same computation is done for voxM and vertM
-    betaVox = voxM/DM;     
-    cleanVoxM = voxM - betaVox*DM;
+elseif nargin == 2  
+    beta1 = mat1/DM;     
+    cleanMat1 = mat1 - beta1*DM;
 end 
 
 
+%%% Backup:
+
+% % voxM, vertM, 
+% elseif nargin == 3 && size(trialinfo,2) == 1
+%     linword     = trialinfo;   
+%     quadword    = trialinfo.^2';
+%     DM = ones(3,size(trialinfo,1));
+
+% if nargin == 3 || 4
+%     betaVox = voxM/DM;     
+%     betaVert = vertM/DM;  
+% 
+%     cleanVoxM = voxM - betaVox*DM;
+%     cleanVertM = vertM - betaVert*DM; 
+% 
+% elseif nargin == 1  % written as voxM, but if first inarg is given as vertM, then this is fine since the same computation is done for voxM and vertM
+%     betaVox = voxM/DM;     
+%     cleanVoxM = voxM - betaVox*DM;
+% end 
 
