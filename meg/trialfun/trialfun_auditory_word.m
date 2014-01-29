255d4adf3b08350f6e94b5798bd8cafeba18488d
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-01-29 19:00:32 +0100

    trialfun now includes true word position of target i.e. based on logfile not triggers.  More updates required to deal with exception cases

--- meg/trialfun/trialfun_auditory_word.m
+++ meg/trialfun/trialfun_auditory_word.m
@@ -21,27 +21,35 @@ function [trl] = trialfun_auditory_word(cfg)
 %   column 5: trigger corresponding to the word
 %   column 6: Number of samples between the trigger and the  onset of the first word
 %   column 7: number of samples between word on and offset
-%   column 8: wordcount 
-%
+%   column 8: wordcount based on triggers
+%   column 9: wordcount based on word position across sentence/sequence
 %   2012 | NL
 % 
- prestim  = ft_getopt(cfg.trialdef, 'prestim', 0.5);
- poststim = ft_getopt(cfg.trialdef, 'poststim', 0.8-1./1200); 
+
+%% load target location info (word position of target for each sentence/sequence)
+idx = regexp(cfg.dataset,'A2');
+subjectname = cfg.dataset(idx:idx+4);
+tarloc = mous_audio_gettarloc(subjectname);
+
+%% create trl matrix
+prestim  = ft_getopt(cfg.trialdef, 'prestim', 0.5);
+poststim = ft_getopt(cfg.trialdef, 'poststim', 0.8-1./1200); 
  
  
 % read in event information
- hdr   = ft_read_header(cfg.dataset);   % if running code locally, change to "cfg.dataset{1}"
+hdr   = ft_read_header(cfg.dataset);   % if running code locally, change to "cfg.dataset{1}"
 % event = ft_read_event(cfg.dataset);
 
 event = mous_read_event_audio(cfg.dataset);  % this line is necessary to fix the trigger problems we have (which applies only after subject A2029, except for A2014).
 
 % select the UPPT001 events
 type = {event.type};
-fp   = strcmp('UPPT001', type);
+fp   = strcmp('UPPT001', type) | ~cellfun('isempty', strfind(type, 'wav'));
 
 % create a vector with the event values and their respective sample numbers
 val  = [event(fp).value];
 smp  = [event(fp).sample];
+type = type(fp);
 
 val = [val 20]; % add a 20 to the val to avoid problems with the last sentence (able to identify boundaries for last sentence)
 smp = [smp smp(end)];
@@ -54,7 +62,7 @@ if selfix(end)<numel(val)
   selfix(end+1) = numel(val);  %  
 end                            
 
-trl    = zeros(0,8);
+trl    = zeros(0,9);
 for k = 1:numel(selfix)-1      % (1)for EACH CONSTITUENT TRIAL: sentence/sequence; % -1 because last trigger is a dummy
   
   sel = selfix(k):selfix(k+1); % (2) start and end of a trial defined by two consecutive 20 triggers.
@@ -64,7 +72,7 @@ for k = 1:numel(selfix)-1      % (1)for EACH CONSTITUENT TRIAL: sentence/sequenc
   
   % get the each word's on-,offset, trigger and sample info
   firstword = [];
-  tmptrl    = zeros(0,8);
+  tmptrl    = zeros(0,9);
   for kk = 1:numel(tmpval)-1     % triggers for audio on- and offset, 1st and target word onset
     trg1 = tmpval(kk);         % loop through the triggers
     trg2 = tmpval(kk+1);
@@ -80,14 +88,29 @@ for k = 1:numel(selfix)-1      % (1)for EACH CONSTITUENT TRIAL: sentence/sequenc
       else
         endsample = min(tmpsmp(kk) + round(hdr.Fs*poststim), inf); % offset of word: word's onset sample + poststim (3s);  
       end
-
+      
+      wavfileid = str2double(type{selfix(k)+1}(1:3)); % get soundfile filename of current trial
+     
       wordcount = wordcount + 1;
-
-      %         1         2         3       4 5          6                   7                       8
-      tmp    = [begsample endsample -offset k tmpval(kk) begsample-firstword tmpsmp(kk+1)-tmpsmp(kk) wordcount];
+      
+      % get target position (based on logfile, not triggers)
+      if wordcount ~= 1  
+      % if trial = sequence, find matching sentence (that shares same
+      % target location) because tarloc only holds sentence filenames
+        if wavfileid > 409
+            wavfileid2 = wavfileid - 500;  % create second wavfileid2 so as not to overwrite the real one
+        end 
+          idxwav = find(tarloc(:,1) == wavfileid2);
+          wordcount = tarloc(idxwav,2);          
+      end 
+      
+      %         1         2         3       4 5          6                    7                      8         9
+      tmp    = [begsample endsample -offset k tmpval(kk) begsample-firstword tmpsmp(kk+1)-tmpsmp(kk) wordcount wavfileid];
+      
       tmptrl = cat(1,tmptrl,tmp);
-    end
+    end  
   end
+
   
 %   % for each sentence/sequence truncate the epochs' length of words 
 %   % so that they do not go beyond the end of a
@@ -101,3 +124,18 @@ for k = 1:numel(selfix)-1      % (1)for EACH CONSTITUENT TRIAL: sentence/sequenc
 %   
    trl = cat(1, trl, tmptrl);
 end
+
+%% exception cases in trl (due to trigger issues)
+% FIX ME
+% if strcmp(subjectname,'A2013')
+%     trl(459,:) = [];
+% end
+% 
+% % account for not having data for the first 20 trials for A2002
+% if strcmp(subjectname,'A2002')
+%     diff = 240-(size(selfix,2)-1);
+%     tarloc = tarloc(diff+1:end,:);
+%     firstloc = ones(numel(selfix)-1,1);
+% end
+
+
