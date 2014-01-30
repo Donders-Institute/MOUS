6b58486a2e7b6bba8a4a38c2bc6cfaf4907def48
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2014-01-30 09:26:17 +0100

    change to write intermediate results to a temporary file to avoid permission issues when re-computing

--- meg/analysis_functions/mous_anatomy_skullstrip.m
+++ meg/analysis_functions/mous_anatomy_skullstrip.m
@@ -12,9 +12,13 @@ function [seg, mask] = mous_anatomy_skullstrip(subjectname, threshold, center, r
 %seg = ft_volumesegment(cfg, mri);
 
 d   = rootdir;
-str = ['/opt/fsl_5.0.4/bin/bet ',d,filesep,subjectname,'/anatomy/',subjectname,'coregMNI.nii ',d,filesep,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip '];
+t   = tempname;
+str = ['/opt/fsl_5.0.4/bin/bet ',d,filesep,subjectname,'/anatomy/',subjectname,'coregMNI.nii ',t];%,d,filesep,subjectname,'/anatomy/',subjectname,'coregMNIskullstrip '];
 str = [str,'-R -f ',num2str(threshold),' -c ', num2str(center),' -g 0 -m -v'];
+delete(t);
 
 system(str);
-seg  = ft_read_mri(fullfile(d,subjectname,'anatomy',[subjectname,'coregMNIskullstrip.nii.gz']));
-mask = ft_read_mri(fullfile(d,subjectname,'anatomy',[subjectname,'coregMNIskullstrip_mask.nii.gz']));
+seg  = ft_read_mri([t,'-R.nii.gz']);
+mask = ft_read_mri([t,'-R_mask.nii.gz']);
+delete([t,'-R.nii.gz']);
+delete([t,'-R_mask.nii.gz']);
