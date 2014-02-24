d299d989a13baea27e97a19ba84eed236c913981
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-02-24 15:16:44 +0100

    same msg as in mous_bfica_sourcestatistics.m (to remove interpolation for source_freq_time data)

--- meg/analysis_functions/mous_bfica_sourcestatistics_seqsentpar.m
+++ meg/analysis_functions/mous_bfica_sourcestatistics_seqsentpar.m
@@ -1,4 +1,4 @@
-function [stat, i1, Nsubj] = mous_bfica_sourcestatistics_seqsentpar(subj, suffix, baselineflag, cfg)
+function [stat, Nsubj] = mous_bfica_sourcestatistics_seqsentpar(subj, suffix, baselineflag, cfg, rootdir)
 
 if nargin<3
   baselineflag = 0;
@@ -16,10 +16,16 @@ if strcmp(cfg.correctm, 'cluster')
 end
 
 Nsubj   = numel(subj);
-rootdir = '/home/language/jansch/public/mous';
+if nargin<4
+    rootdir = '/project/3011020.09/jansch/';
+end
 
-load('/home/language/jansch/projects/mous/meg/templates/sourcemodel/standard_sourcemodel3d8mm');
+[p,n,e] = fileparts(which('standard_sourcemodel3d8mm.mat'));
+load([p,'/',n,e]);
 sourcemodeltemplate = sourcemodel;
+
+% load('/home/language/jansch/projects/mous/meg/templates/sourcemodel/standard_sourcemodel3d8mm');
+% sourcemodeltemplate = sourcemodel;
 
 for k = 1:Nsubj
   clear tlcksentpar statsentpar stat2sentpar
@@ -31,64 +37,92 @@ for k = 1:Nsubj
       sourcemodel = rmfield(sourcemodel, 'cfg');
     end
   end
-  
   sourcemodel.time = tlcksentpar.time;
+  if isfield(tlcksentpar, 'freq')
+    sourcemodel.freq  = tlcksentpar.freq;
+    sourcemodel.dimord = 'pos_freq_time';
+  else
+    sourcemodel.dimord = 'pos_time';
+  end
   
-  % no log transform
+% no log transform
   sourcemodel.avg.pow = statsentpar.stat;% ./ repmat(Bseq, [1 numel(tlckseq.time)]);
-  tmp                 = zeros(prod(sourcemodel.dim), numel(sourcemodel.time));
-  tmp(newinside,:)    = sourcemodel.avg.pow;
-  sourcemodel.avg.pow = tmp;
-  
+  %tmp                 = zeros(prod(sourcemodel.dim), numel(sourcemodel.time));
+  if isfield(statsentpar,'freq')
+    tmp                 = zeros(prod(sourcemodel.dim),size(sourcemodel.avg.pow,2),numel(sourcemodel.time));
+  else 
+    tmp                 = zeros(prod(sourcemodel.dim),numel(sourcemodel.time));
+  end 
+  tmp(newinside,:,:)    = sourcemodel.avg.pow;
+  sourcemodel.avg.pow = tmp; 
   dat{k}         = sourcemodel;
   dat{k}.pos     = sourcemodeltemplate.pos;
   
-  clear tlcksqtpar statseqpar stat2seqpar
+  
+  clear tlckseqpar statseqpar stat2seqpar
   mous_db_getdata(subj{k}, ['meg_bfica_',suffix{2}], rootdir);
-  
-  sourcemodel.time = statseqpar.time;
-  
+  sourcemodel.time = tlckseqpar.time;
+
   % no log transform
   sourcemodel.avg.pow = statseqpar.stat;% ./ repmat(Bseq, [1 numel(tlckseq.time)]);
-  tmp                 = zeros(prod(sourcemodel.dim), numel(sourcemodel.time));
-  tmp(newinside,:)    = sourcemodel.avg.pow;
+  %tmp                 = zeros(prod(sourcemodel.dim), numel(sourcemodel.time));
+  if isfield(statseqpar,'freq')
+    tmp                 = zeros(prod(sourcemodel.dim),size(sourcemodel.avg.pow,2),numel(sourcemodel.time));
+  else 
+    tmp                 = zeros(prod(sourcemodel.dim),numel(sourcemodel.time));
+  end 
+  tmp(newinside,:,:)    = sourcemodel.avg.pow;
   sourcemodel.avg.pow = tmp;
-  
   dat2{k}         = sourcemodel;
   dat2{k}.pos     = sourcemodeltemplate.pos;
-  
 end
 
 % do a baseline subtraction
 if baselineflag
-  ix = find(dat{k}.time<=0.1);
-  for k = 1:numel(dat)
-    tmp = dat{k}.avg.pow;
-    dat{k}.avg.pow = tmp - nanmean(tmp(:,ix),2)*ones(1,size(tmp,2));
-    tmp = dat2{k}.avg.pow;
-    dat2{k}.avg.pow = tmp - nanmean(tmp(:,ix),2)*ones(1,size(tmp,2));
+  ix = find(dat{k}.time<=-0.1);  % define toi for baseline
+  if isfield(tlckseqpar,'freq')  % if 3D matrix
+    for k = 1:numel(dat)
+      tmp = dat{k}.avg.pow;
+      
+      bsl = nanmean(tmp(:,:,ix),3);
+      dat{k}.avg.pow = tmp - repmat(bsl,[1,1,size(tmp,3)]); % subtract baseline (repmat)
+
+      tmp = dat2{k}.avg.pow;
+      bsl = nanmean(tmp(:,:,ix),3);
+      dat2{k}.avg.pow = tmp - repmat(bsl,[1,1,size(tmp,3)]); % subtract baseline (repmat)
+    end
+  else 
+    for k = 1:numel(dat)
+      tmp = dat{k}.avg.pow;
+      dat{k}.avg.pow = tmp - nanmean(tmp(:,ix),2)*ones(1,size(tmp,2));
+
+      tmp = dat2{k}.avg.pow;
+      dat2{k}.avg.pow = tmp - nanmean(tmp(:,ix),2)*ones(1,size(tmp,2));
+    end
   end
 end
 
-%cfg = [];
+% cfg = []; % keep cfg from inarg
 cfg.method = 'montecarlo';
 cfg.statistic = 'depsamplesT';
 cfg.design = [ones(1,Nsubj) ones(1,Nsubj)*2;1:Nsubj 1:Nsubj];
 cfg.ivar = 1;
 cfg.uvar = 2;
-%cfg.numrandomization = 1000;
 cfg.parameter = 'avg.pow';
-stat = ft_sourcestatistics(cfg, dat{:}, dat2{:});
-if ndims(stat.stat)>2 %i.e. being a 3d matrix, rather than space x something else
-  stat.stat=stat.stat(:);
-  stat.prob=stat.prob(:);
-  stat.mask=stat.mask(:);
-end
-i1    = mous_bfica_sourceinterpolate(stat, 'stat', stat.inside);
-iprob = mous_bfica_sourceinterpolate(stat, 'prob', stat.inside);
-imask = mous_bfica_sourceinterpolate(stat, 'mask', stat.inside);
-for k = 1:numel(i1)
-  i1(k).coordsys = 'spm';
-  i1(k).mask = imask(k).avg.pow;
-  i1(k).prob = iprob(k).avg.pow;
-end
+stat = ft_sourcestatistics(cfg, dat{:}, dat2{:});  % sent = dat;  seq = dat2;
+
+% Commented out because this functions wasn't written to take into account
+% source_freq_time (e.g., 11000 x 16 x 13)
+% if ndims(stat.stat)>2 %i.e. being a 3d matrix, rather than space x something else
+%   stat.stat=stat.stat(:);
+%   stat.prob=stat.prob(:);
+%   stat.mask=stat.mask(:);
+% end
+% i1    = mous_bfica_sourceinterpolate(stat, 'stat', stat.inside);
+% iprob = mous_bfica_sourceinterpolate(stat, 'prob', stat.inside);
+% imask = mous_bfica_sourceinterpolate(stat, 'mask', stat.inside);
+% for k = 1:numel(i1)
+%   i1(k).coordsys = 'spm';
+%   i1(k).mask = imask(k).avg.pow;
+%   i1(k).prob = iprob(k).avg.pow;
+% end
