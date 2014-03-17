74791fd73d6124b6c8b184d7722ca537793df965
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-03-17 16:56:30 +0100

    revamped the script to now be able to calculate any combination of wordtype (allwords, first words only, target words only) X condition (mix,rc,all) X modality (visual, auditory). Note that the directories may need to be updated at some point if files get moved again.

--- meg/analysis_scripts/mous_erf_pipeline.m
+++ meg/analysis_scripts/mous_erf_pipeline.m
@@ -1,84 +1,85 @@
-% Use Annika's list if subjectname is not defined in the workspace, else
-% use the defined subjectname: this allows for use with qsub
+% mous_erf_pipeline
+%  NL - major changes on 17 March 2014
+% For this script, specify the following:
+% 1. rootdir
+% 2. doerf_main = 1?
+% 3. doerf_parametric = 1?
+% 4. wordtype = 'tar', 'first', 'all
+% 5. condition = 'mix','rc'; OR don't specify anything (that's also okay)
 
-if ~exist('subjectname', 'var')
-  load MOUS/meg/subjects_OK_20130613.mat
-elseif ~iscell(subjectname)
-  subj = {subjectname};
-else
-  subj = subjectname;
-end
 
-if ~exist('rootdir', 'var')
-  rootdir = '/project/3011020.09/MEG';
-end
+% NOTE 
+% VISUAL SUBJECTS: preprocessed data is in /project/3011020.09/MEG/'
+%                  ERF data is in /project/3011020.09/annhul/'
+% AUDITORY SUBJS:  both are both in /project/3011020.09/annhul/'
 
-if ~exist('doerf_main', 'var')
-  doerf_main = 0;
-end
+if ~exist('rootdir', 'var')           rootdir = '/project/3011020.09/MEG/';  end
+if ~exist('doerf_main', 'var')        doerf_main = 0;                       end
+if ~exist('doerf_parametric', 'var')  doerf_parametric = 0;                 end
+if ~exist('length','var')             length = '02-10';                     end
 
-if ~exist('doerf_parametric', 'var')
-  doerf_parametric = 0;
-end
-
+% define parameters used for both doerf_main and doerf_parametric
+% N.B.Neither wordtype / trialfunreflected in inputname therefore removed
+  
+inputdata  = ['meg_erf_allwords_' length];
+axial = '-ag';
+planar = '-pg';
+  
 if doerf_main
-  % FIXME this is still a hybrid Annika/JM style
-  if ~exist('inputdata', 'var')
-    length   = '02-1'; %means -0.2 to 1 sec
-    wordType = 'all'; 
-    trialfun = 'visual_word';
-    inputdata  = ['meg_processed_{_preProcERF' trialfun '_' wordType '_' length 'ds}'];
-    outputdata = ['meg_processed_{_erf_' trialfun '_' wordType '_' length 'ds'];
-    outname1   = strcat(outputdata, '-ag}');
-    outname2   = strcat(outputdata, '-pg}');
-  end
-
-  if  ~exist('outputdata', 'var')
-    error('you need to specify the name of the file that will contain the output data');
+  % files for input/output are divided between MEG and annhul
+  if strcmp(subjectname(1),'V')
+    mous_db_getdata(subjectname,inputdata,'/project/3011020.09/MEG/'); 
+  elseif strcmp(subjectname(1),'A')
+    mous_db_getdata(subjectname,inputdata,rootdir);
   end
   
-  if ~exist('outname1', 'var')
-    outname1 = strcat(outputdata, '-ag');
-    outname2 = strcat(outputdata, '-pg');
-  end
-
-  for k = 1:numel(subj)
-    subjectname = subj{k};
-    
-    % get the preprocessed data from the database
-    %   tmp  = mous_db_getdata(subjectname, inputdata);
-    %   if iscell(tmp)
-    %     data = tmp{1};
-    %   else
-    %     data = tmp;
-    %   end
-    %   clear tmp;
-    load(fullfile(rootdir,subjectname,'erf',[subjectname,inputdata(4:end)]));
-    
-    
-    % auditory data is apparently used, select whether to use the first
-    % words, or the targets: first words are odd numbered, targets are even
-    % numbered
-    if ~isempty(strfind(inputdata, 'auditory'))
-      % use the first words here
-      %sel = find(mod(data.trialinfo(:,2),2)==1);
-      sel = find(mod(data.trialinfo(:,2),2)==0);
-      cfg.trials = sel;
-      data = ft_preprocessing(cfg, data);
+  if exist('condition','var')
+    cfg = [];
+    switch condition
+      case 'mix'
+        sel = find(ismember(data.trialinfo(:,2),[1 2 3 4]));
+        cfg.trials = sel;
+      case 'rc'
+        sel = find(ismember(data.trialinfo(:,2),[5 6 7 8]));
+        cfg.trials = sel;
     end
-    [senWord_AG, seqWord_AG, senWord_PG, seqWord_PG, senWord_CPG, seqWord_CPG, stdev] = mous_erf_compute(subjectname, data);
-    
-    mous_db_putdata(subjectname, outname1, 'senWord_AG', 'seqWord_AG',rootdir);
-    mous_db_putdata(subjectname, outname2, 'senWord_PG', 'seqWord_PG', 'senWord_CPG', 'seqWord_CPG', 'stdev',rootdir);
+    data = ft_selectdata(cfg,data);
   end
+  
 
-end
+  cfg2 = [];
+  switch wordtype
+    case 'first'
+      sel = find(mod(data.trialinfo(:,2),2)==1);        % 1st words=odd numbered (For auditory mainly)
+      cfg2.trials  = sel;
+      outname1 = strcat(inputdata,'-firstword',axial);
+      outname2 = strcat(inputdata,'-firstword',planar);
+    case 'tar'
+      sel = find(mod(data.trialinfo(:,2),2)==0);        % Target word = even numbered (for auditory mainly)
+      cfg2.trials  = sel;
+      outname1 = strcat(inputdata,'-target',axial);
+      outname2 = strcat(inputdata,'-target',planar);
+    otherwise 'all'
+      outname1 = strcat(inputdata,'-allwords',axial);  % erf_allwords_02-10-allwords-pg
+      outname1 = strcat(inputdata,'-allwords',planar); % 1st allwords = word for preprocessing, 2nd = word for tlck.
+  end
+  data = ft_preprocessing(cfg2, data);
+
+  [senWord_AG, seqWord_AG, senWord_PG, seqWord_PG, senWord_CPG, seqWord_CPG, stdev] = mous_erf_compute(subjectname, data);
+  
+  if exist('condition','var') % update outputdata filename
+    outname1 = strcat(outname1(1:end-2),condition,axial);
+    outname2 = strcat(outname2(1:end-2),condition,planar);
+  end 
+    
+  mous_db_putdata(subjectname, outname1, 'senWord_AG', 'seqWord_AG',rootdir);
+  mous_db_putdata(subjectname, outname2, 'senWord_PG', 'seqWord_PG', 'senWord_CPG', 'seqWord_CPG', 'stdev',rootdir);
+end  % end doerf_main
 
 if doerf_parametric
-  if ~exist('suffix', 'var')
-    error('you need to specify the file suffix for the preprocessed data');
-  end
-  mous_db_getdata(subjectname, suffix, rootdir);
+  % FIXME: currently, parametric is done for all words (e.g., not only for
+  % target words)
+  mous_db_getdata(subjectname, inputdata, rootdir);
   
   % move around the columns in the trialinfo field so that the condition
   % trigger ends up in the third column and the word ordinal indicator in
@@ -90,10 +91,10 @@ if doerf_parametric
   
   tlck = tlck_sent;
   stat = stat_sent;
-  mous_db_putdata(subjectname, [suffix,'_wordsent_parametric_blc'], 'tlck', 'stat', rootdir);
+  mous_db_putdata(subjectname, [inputdata,'_wordsent_parametric_blc'], 'tlck', 'stat', rootdir);
   tlck = tlck_seq;
   stat = stat_seq;
-  mous_db_putdata(subjectname, [suffix,'_wordseq_parametric_blc'], 'tlck', 'stat', rootdir);
+  mous_db_putdata(subjectname, [inputdata,'_wordseq_parametric_blc'], 'tlck', 'stat', rootdir);
  
   % convert to planar
   cfg = [];
@@ -108,9 +109,8 @@ if doerf_parametric
   
   tlck = tlckp_sent;
   stat = statp_sent;
-  mous_db_putdata(subjectname, [suffix,'_wordsent_parametric_blc_planar'], 'tlck', 'stat', rootdir);
+  mous_db_putdata(subjectname, [inputdata,'_wordsent_parametric_blc_planar'], 'tlck', 'stat', rootdir);
   tlck = tlckp_seq;
   stat = statp_seq;
-  mous_db_putdata(subjectname, [suffix,'_wordseq_parametric_blc_planar'], 'tlck', 'stat', rootdir);
-
+  mous_db_putdata(subjectname, [inputdata,'_wordseq_parametric_blc_planar'], 'tlck', 'stat', rootdir);
 end
