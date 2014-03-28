4cc618f885366e7a5a044d794ec1e2ade694379b
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2014-03-28 10:14:57 +0100

    added functionality for meg fmri comparison
    Former-commit-id: f7895b8c62bfe9b58baae89a6f1885283009490c

---
+++ meg/analysis_scripts/mous_megmri_pipeline.m
@@ -0,0 +1,37 @@
+% MOUS_MEGMRI_PIPELINE
+
+if ~exist('subjectname', 'var'), error('subjectname should be provided'); end
+if ~exist('rootdir',     'var'), rootdir  = '/project/3011020.09/MEG';    end
+if ~exist('dointerp',    'var'), dointerp = 0;                            end
+
+% HARD CODED, remove!
+figpath  = '/project/3011020.09/jansch/results/20140328';  
+  
+opengl software
+if dointerp
+  close all;
+  mripath  = '/home/language/juludd/MOUS/ffxstats_VIS_102';
+  fname    = 'beta_0001.img'; % it would be good to have a lookup table in the script that specifies the meaning of each contrast.
+  filename = fullfile(mripath,[subjectname,'-ffxStats'],fname);
+  mri      = ft_read_mri(filename);
+  [mri,~,h] = mous_fmri_3dto2d(subjectname, mri);
+  
+  mous_db_makesubjdir(subjectname, rootdir, {'megmri'});
+  mous_db_putdata(subjectname, 'meg_megmri_beta0001', 'mri', rootdir);
+  
+  figname = fullfile(figpath, [subjectname,'_megmri_qc1']);
+  p = get(h(1), 'position');
+  x = getframe(h(1), [1 1 p(3:4)-1]);
+  savepng(x.cdata, figname);
+  
+  %print(h(1), '-dpng', figname);
+  
+  figname = fullfile(figpath, [subjectname,'_megmri_qc2']);
+  p = get(h(2), 'position');
+  x = getframe(h(2), [1 1 p(3:4)-1]);
+  savepng(x.cdata, figname);
+ 
+  %print(h(2), '-dpng', figname);
+  
+
+end
