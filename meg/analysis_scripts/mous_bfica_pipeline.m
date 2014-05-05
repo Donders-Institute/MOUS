d624c0c039d8e0977f09124062c59571e41d2c92
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-05-05 17:55:11 +0200

    doparcel_source2sens. compute parcellations of source-level activity and convert data structure to be suitable for ft_freqanalysis
    Former-commit-id: 330f6dfe57a470935ba3a00870baac88e8765544

--- meg/analysis_scripts/mous_bfica_pipeline.m
+++ meg/analysis_scripts/mous_bfica_pipeline.m
@@ -108,9 +108,10 @@ if dosource,
 end
 if doleadfield8mm,
   mous_db_getdata(subjectname, ['meg_bfica_freq',suff], rootdir);
-  [sourcemodel, newinside, oldinside] = mous_bfica_leadfield(subjectname, ft_struct2double(freq), toi, 8);
+  [sourcemodel, newinside, oldinside] = mous_bfica_leadfield(subjectname, ft_struct2double(freq), toi, 8, gridtype);
   mous_db_putdata(subjectname, 'meg_bfica_leadfield8mm', 'sourcemodel', 'newinside', 'oldinside', rootdir, 0);
 end
+
 if dosource8mmparcellate,
   mous_db_getdata(subjectname, 'meg_anatomy_sourcemodel3D_nonlin8mm');
   sourcemodel = mous_anatomy_sourcemodelparcellate(subjectname, sourcemodel);
@@ -237,7 +238,7 @@ if dosource_contrasts,
     %tois = -0.2:0.05:0.8;
 
     tmpfreq = ft_selectdata(freq, 'foilim', frequency*[1 1]+[-0.1 0.1]);
-    [source, trialinfo] = mous_bfica_source(subjectname, tmpfreq, toi(toilop), 8,rootdir);  % default directory is jansch in order to get leadfield
+    [source, trialinfo] = mous_bfica_source(subjectname, tmpfreq, toi(toilop), 8, leadfieldtype, rootdir);  % default directory is jansch in order to get leadfield
     sourcedataorig      = mous_bfica_sourcedata(source, tmpfreq, toi(toilop));
     
     sourcedataorig.trialinfo(:,end+1:7) = 1; % add dummy columns, they don't mean anything
@@ -255,10 +256,10 @@ if dosource_contrasts,
     sourcedata = sourcedataorig;
     
     %% dosentvsseq   
-%     [tlcksent(toilop), tlckseq(toilop),tstat(:,toilop)] = mous_makecontrast(sourcedata,'sent-seq');
+    [tlcksent(toilop), tlckseq(toilop),tstat(:,toilop)] = mous_makecontrast(sourcedata,'sent-seq');
 
-    %  domixvsrelativeclause
-      [tlcksentMX(toilop), tlcksentRC(toilop), tstat(:,toilop)] = mous_makecontrast(sourcedata,'sentMX-sentRC');
+%      domixvsrelativeclause
+     [tlcksentMX(toilop), tlcksentRC(toilop), tstat(:,toilop)] = mous_makecontrast(sourcedata,'sentMX-sentRC');
 
     % dowordsentpar2
 %     if strcmp(subjectname(1),'V')     
@@ -272,28 +273,28 @@ if dosource_contrasts,
 %     % dofirstword - use <0s for baseline computation
 %     [tlcksentfirst(toilop),tlckseqfirst(toilop),tstatfirst(:,toilop)] = mous_makecontrast(sourcedata,'sent-seqFirstword');   
 % 
-%     [tlcksenttar(toilop), tlckseqtar(toilop),tstattar(:,toilop)] = mous_makecontrast(sourcedata,'sent-seqTarget');
+    [tlcksenttar(toilop), tlckseqtar(toilop),tstattar(:,toilop)] = mous_makecontrast(sourcedata,'sent-seqTarget');
 % 
 %     % FIXME: Auditory parametric needs logfile+excel file info - not completed for all subjects (there are exceptions - 26 Feb 2014)
 %      % target word parametric (sent)
-%     [tlcksentpartar(toilop),statsentpartar(toilop),stat2sentpartar(toilop)] = mous_makecontrast(sourcedata,'wordsenttar_parametric'); % _blc option available
+    [tlcksentpartar(toilop),statsentpartar(toilop),stat2sentpartar(toilop)] = mous_makecontrast(sourcedata,'wordsenttar_parametric'); % _blc option available
 % 
 %     % target word parametric (seq)
-%     [tlckseqpartar(toilop),statseqpartar(toilop),stat2seqpartar(toilop)] = mous_makecontrast(sourcedata,'wordseqtar_parametric'); % _blc option available
+    [tlckseqpartar(toilop),statseqpartar(toilop),stat2seqpartar(toilop)] = mous_makecontrast(sourcedata,'wordseqtar_parametric'); % _blc option available
   end
   
-  % concatenate 
-%   tlcksent(1).avg = cat(2,tlcksent(:).avg);                % tlcksent
-%   tlcksent(1).var = cat(2,tlcksent(:).var);
-%   tlcksent(1).dof = cat(2,tlcksent(:).dof);
-%   tlcksent(1).time = cat(2,tlcksent(:).time);
-%   tlcksent         = tlcksent(1);
-% 
-%   tlckseq(1).avg = cat(2,tlckseq(:).avg);                   % tlckseq
-%   tlckseq(1).var = cat(2,tlckseq(:).var);
-%   tlckseq(1).dof = cat(2,tlckseq(:).dof);
-%   tlckseq(1).time = cat(2,tlckseq(:).time);
-%   tlckseq         = tlckseq(1);
+  concatenate 
+  tlcksent(1).avg = cat(2,tlcksent(:).avg);                % tlcksent
+  tlcksent(1).var = cat(2,tlcksent(:).var);
+  tlcksent(1).dof = cat(2,tlcksent(:).dof);
+  tlcksent(1).time = cat(2,tlcksent(:).time);
+  tlcksent         = tlcksent(1);
+
+  tlckseq(1).avg = cat(2,tlckseq(:).avg);                   % tlckseq
+  tlckseq(1).var = cat(2,tlckseq(:).var);
+  tlckseq(1).dof = cat(2,tlckseq(:).dof);
+  tlckseq(1).time = cat(2,tlckseq(:).time);
+  tlckseq         = tlckseq(1);
 
 
   tlcksentMX(1).avg = cat(2,tlcksentMX(:).avg);                % tlcksentMIX
@@ -379,68 +380,77 @@ if dosource_contrasts,
 %     stat2seqpar            = stat2seqpar(1);
 %   end
 % 
-%   tlcksentpartar(1).avg = cat(2,tlcksentpartar(:).avg);     % tlcksentpartar
-%   tlcksentpartar(1).var = cat(2,tlcksentpartar(:).var);
-%   tlcksentpartar(1).dof = cat(2,tlcksentpartar(:).dof);
-%   tlcksentpartar(1).time = cat(2,tlcksentpartar(:).time);
-%   tlcksentpartar(1).trial = cat(3,tlcksentpartar(:).trial);
-%   tlcksentpartar(1).trial2 = cat(3,tlcksentpartar(:).trial2);
-%   tlcksentpartar           = tlcksentpartar(1);
-% 
-%   tlckseqpartar(1).avg = cat(2,tlckseqpartar(:).avg);       %  tlckseqpartar
-%   tlckseqpartar(1).var = cat(2,tlckseqpartar(:).var);
-%   tlckseqpartar(1).dof = cat(2,tlckseqpartar(:).dof);
-%   tlckseqpartar(1).time = cat(2,tlckseqpartar(:).time);
-%   tlckseqpartar(1).trial = cat(3,tlckseqpartar(:).trial);
-%   tlckseqpartar(1).trial2 = cat(3,tlckseqpartar(:).trial2);
-%   tlckseqpartar           = tlckseqpartar(1);
-% 
-%   statsentpartar(1).stat = cat(2,statsentpartar(:).stat);    % statsentpartar
-%   statsentpartar(1).prob = cat(2,statsentpartar(:).prob);
-%   statsentpartar(1).mask = cat(2,statsentpartar(:).mask);
-%   statsentpartar(1).time = cat(2,statsentpartar(:).time);
-%   statsentpartar(1).cirange = cat(2,statsentpartar(:).cirange);
-%   statsentpartar            = statsentpartar(1);
-% 
-%   stat2sentpartar(1).stat = cat(2,stat2sentpartar(:).stat);  % stat2sentpartar
-%   stat2sentpartar(1).prob = cat(2,stat2sentpartar(:).prob);
-%   stat2sentpartar(1).mask = cat(2,stat2sentpartar(:).mask);
-%   stat2sentpartar(1).time = cat(2,stat2sentpartar(:).time);
-%   stat2sentpartar(1).cirange = cat(2,stat2sentpartar(:).cirange);
-%   stat2sentpartar            = stat2sentpartar(1);
-% 
-%   statseqpartar(1).stat = cat(2,statseqpartar(:).stat);      % statseqpartar
-%   statseqpartar(1).prob = cat(2,statseqpartar(:).prob);
-%   statseqpartar(1).mask = cat(2,statseqpartar(:).mask);
-%   statseqpartar(1).time = cat(2,statseqpartar(:).time);
-%   statseqpartar(1).cirange = cat(2,statseqpartar(:).cirange);
-%   statseqpartar            = statseqpartar(1);
-% 
-%   stat2seqpartar(1).stat = cat(2,stat2seqpartar(:).stat);    % stat2seqpartar
-%   stat2seqpartar(1).prob = cat(2,stat2seqpartar(:).prob);
-%   stat2seqpartar(1).mask = cat(2,stat2seqpartar(:).mask);
-%   stat2seqpartar(1).time = cat(2,stat2seqpartar(:).time);
-%   stat2seqpartar(1).cirange = cat(2,stat2seqpartar(:).cirange);
-%   stat2seqpartar            = stat2seqpartar(1);
+  tlcksentpartar(1).avg = cat(2,tlcksentpartar(:).avg);     % tlcksentpartar
+  tlcksentpartar(1).var = cat(2,tlcksentpartar(:).var);
+  tlcksentpartar(1).dof = cat(2,tlcksentpartar(:).dof);
+  tlcksentpartar(1).time = cat(2,tlcksentpartar(:).time);
+  tlcksentpartar(1).trial = cat(3,tlcksentpartar(:).trial);
+  tlcksentpartar(1).trial2 = cat(3,tlcksentpartar(:).trial2);
+  tlcksentpartar           = tlcksentpartar(1);
+
+  tlckseqpartar(1).avg = cat(2,tlckseqpartar(:).avg);       %  tlckseqpartar
+  tlckseqpartar(1).var = cat(2,tlckseqpartar(:).var);
+  tlckseqpartar(1).dof = cat(2,tlckseqpartar(:).dof);
+  tlckseqpartar(1).time = cat(2,tlckseqpartar(:).time);
+  tlckseqpartar(1).trial = cat(3,tlckseqpartar(:).trial);
+  tlckseqpartar(1).trial2 = cat(3,tlckseqpartar(:).trial2);
+  tlckseqpartar           = tlckseqpartar(1);
+
+  statsentpartar(1).stat = cat(2,statsentpartar(:).stat);    % statsentpartar
+  statsentpartar(1).prob = cat(2,statsentpartar(:).prob);
+  statsentpartar(1).mask = cat(2,statsentpartar(:).mask);
+  statsentpartar(1).time = cat(2,statsentpartar(:).time);
+  statsentpartar(1).cirange = cat(2,statsentpartar(:).cirange);
+  statsentpartar            = statsentpartar(1);
+
+  stat2sentpartar(1).stat = cat(2,stat2sentpartar(:).stat);  % stat2sentpartar
+  stat2sentpartar(1).prob = cat(2,stat2sentpartar(:).prob);
+  stat2sentpartar(1).mask = cat(2,stat2sentpartar(:).mask);
+  stat2sentpartar(1).time = cat(2,stat2sentpartar(:).time);
+  stat2sentpartar(1).cirange = cat(2,stat2sentpartar(:).cirange);
+  stat2sentpartar            = stat2sentpartar(1);
+
+  statseqpartar(1).stat = cat(2,statseqpartar(:).stat);      % statseqpartar
+  statseqpartar(1).prob = cat(2,statseqpartar(:).prob);
+  statseqpartar(1).mask = cat(2,statseqpartar(:).mask);
+  statseqpartar(1).time = cat(2,statseqpartar(:).time);
+  statseqpartar(1).cirange = cat(2,statseqpartar(:).cirange);
+  statseqpartar            = statseqpartar(1);
+
+  stat2seqpartar(1).stat = cat(2,stat2seqpartar(:).stat);    % stat2seqpartar
+  stat2seqpartar(1).prob = cat(2,stat2seqpartar(:).prob);
+  stat2seqpartar(1).mask = cat(2,stat2seqpartar(:).mask);
+  stat2seqpartar(1).time = cat(2,stat2seqpartar(:).time);
+  stat2seqpartar(1).cirange = cat(2,stat2seqpartar(:).cirange);
+  stat2seqpartar            = stat2seqpartar(1);
 
   
   % save the results
   suff2 = num2str(round(frequency*10));
-%   mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseq',suff2], 'tlcksent',    'tlckseq',      'tstat', rootdir, 0);
+
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseq',suff2], 'tlcksent',    'tlckseq',      'tstat', rootdir, 0);
   
-    mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentMXRC',suff2], 'tlcksentMX', 'tlcksentRC',    'tstat', rootdir, 0);
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentMXRC',suff2], 'tlcksentMX', 'tlcksentRC',    'tstat', rootdir, 0);
 %   if strcmp(subjectname(1),'V')
 %     mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentpar',suff2], 'tlcksentpar', 'stat2sentpar', 'statsentpar', rootdir, 0);
 %     mous_db_putdata(subjectname, ['meg_bfica_sourcedataseqpar', suff2], 'tlckseqpar',  'stat2seqpar',  'statseqpar', rootdir, 0);
 %   end
   
-%   mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseq_firstword',suff2], 'tlcksentfirst',    'tlckseqfirst',  'tstatfirst', rootdir, 0);
-%   mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseqtar',suff2], 'tlcksenttar',    'tlckseqtar',      'tstattar', rootdir, 0);
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseq_firstword',suff2], 'tlcksentfirst',    'tlckseqfirst',  'tstatfirst', rootdir, 0);
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentseqtar',suff2], 'tlcksenttar',    'tlckseqtar',      'tstattar', rootdir, 0);
 %   
-%   mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentpartar',suff2], 'tlcksentpartar', 'stat2sentpartar', 'statsentpartar', rootdir, 0);
-%   mous_db_putdata(subjectname, ['meg_bfica_sourcedataseqpartar', suff2], 'tlckseqpartar',  'stat2seqpartar',  'statseqpartar', rootdir, 0);
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedatasentpartar',suff2], 'tlcksentpartar', 'stat2sentpartar', 'statsentpartar', rootdir, 0);
+  mous_db_putdata(subjectname, ['meg_bfica_sourcedataseqpartar', suff2], 'tlckseqpartar',  'stat2seqpartar',  'statseqpartar', rootdir, 0);
 end 
 
+if doparcel_source2sens,
+  mous_db_getdata(subjectname, sourcedata, rootdir);
+  mous_db_getdata(subjectname,'meg_bfica_leadfield8mm',rootdir); % only need 'newinside' variable
+  load('/home/language/nielam/MOUS/meg/templates/sourcemodel/standard_sourcemodel3d8mm_parcellated_aal_sub');
+  [tlcksent] = mous_bfica_parcellate(sourcemodel,tlcksent,newinside);
+  [tlckseq]  = mous_bfica_parcellate(sourcemodel,tlckseq,newinside);
+  mous_db_putdata(subjectname,[sourcedata,'_parcelavg'],'tlcksent','tlckseq',rootdir,1);
+end 
   
 if docombinefreq,
   mous_bfica_sourcedata_combinefreq(subjectname, prefix, freqs, savesuffix);
