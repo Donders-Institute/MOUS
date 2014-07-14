1d1ed10e7d6528544d333d34ffc555088f1da5d9
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2014-07-14 21:36:46 +0200

    do a force copy into the subject specific freesurfer dir
    Former-commit-id: 7b5d1edc945a409ff73093837fe7c605ee00eff4

--- meg/analysis_functions/freesurferscript1.sh
+++ meg/analysis_functions/freesurferscript1.sh
@@ -8,8 +8,8 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 mksubjdirs $SUBJECTS_DIR/$2
 
 cd $SUBJECTS_DIR
-cp $2coregMNI.nii $SUBJECTS_DIR/$2/mri/
-cp $2coregMNIskullstrip.nii $SUBJECTS_DIR/$2/mri/
+cp -f $2coregMNI.nii $SUBJECTS_DIR/$2/mri/
+cp -f $2coregMNIskullstrip.nii $SUBJECTS_DIR/$2/mri/
 
 cd $SUBJECTS_DIR/$2/mri
 mri_convert -c -oc 0 0 0 $2coregMNI.nii orig.mgz
