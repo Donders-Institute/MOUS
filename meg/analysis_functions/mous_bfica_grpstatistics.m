c52004e4b48be0b3dec6e65c81c52f992c4f3b2d
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-07-15 10:47:53 +0200

    updated suffend

--- meg/analysis_functions/mous_bfica_grpstatistics.m
+++ meg/analysis_functions/mous_bfica_grpstatistics.m
@@ -3,11 +3,11 @@ function mous_bfica_grpstatistics(subjectnames,suffix,bslflag)
 % suffix.wordtype: 
 % suffix.parcel: 
 % suffix.oscband:  can reflect a oscillatory band: low/medium/high, or just one
-% specific frequency e.g., 25.
-% low: '25' '50' '75' '100' '150'};
-% medium: '120' '160' '200' '240' '280' '320'};
-% high = 400  440  480  520  560   600  640   680   720   760  800  840
-% 880  920  960  1000;
+% specific frequency e.g., 25 for 2.5Hz
+% suffix.selfreq: [min max] 
+% if selfreq is specified, then specify if need averaging
+% suffix.avg = 'yes' or 'no';
+
   
 rootdir = '/project/3011020.09/MEG/';
 savedir = '/project/3011020.09/nielam/groupresults/bfica/';
@@ -39,36 +39,38 @@ else
 end
 
 % determine frequency choice, and place in savename
-  if isfield(suffix,'selfreq')
-    if suffix.selfreq(1) == suffix.selfreq(2)
-      suffixfreq = [num2str(suffix.selfreq(1)),'Hz'];
-    elseif suffix.selfreq(1) ~= suffix.selfreq(2)
-      suffixfreq = [num2str(suffix.selfreq(1)),'to',num2str(suffix.selfreq(2)),'Hz'];
-        if strcmp(suffix.avg,'yes')
-          suffixfreq = [suffixfreq,'avg'];
-        end
-    end
+if isfield(suffix,'selfreq')
+  if suffix.selfreq(1) == suffix.selfreq(2)
+    suffixfreq = [num2str(suffix.selfreq(1)),'Hz'];
+  elseif suffix.selfreq(1) ~= suffix.selfreq(2)
+    suffixfreq = [num2str(suffix.selfreq(1)),'to',num2str(suffix.selfreq(2)),'Hz'];
+      if strcmp(suffix.avg,'yes')
+        suffixfreq = [suffixfreq,'avg'];
+      end
   end
-  if isfield(suffix,'toi')
-    a = num2str(suffix.toi(1)); a = [a(1) a(3)];
-    b = num2str(suffix.toi(2)); b = [b(1) b(3)];
-    suffend = [a,b,'s'];
-  end
+end
+if isfield(suffix,'toi')
+  a = num2str(suffix.toi(1)); a = [a(1) a(3)];
+  b = num2str(suffix.toi(2)); b = [b(1) b(3)];
+  suffend = [a,b,'s'];
+else
+  suffend = '';
+end
   
 % earlylate
 if ~isempty(strfind(suffix.sourcedata,'sourcedataearlylate'))
   tmp = suffix.sourcedata;
   if regexp(tmp,'low')
-    suffix.wordtype = {'sourcedataearlylateRC_low_bslabsolute','sourcedataearlylateMX_low_bslabsolute'};
+    suffix.wordtype = {'sourcedataearlylateRC_matched_low','sourcedataearlylateMX_matched_low'};
     
   elseif regexp(tmp,'medium')
-    suffix.wordtype = {'sourcedataearlylateRC_medium_bslabsolute','sourcedataearlylateMX_medium_bslabsolute'};
+    suffix.wordtype = {'sourcedataearlylateRC_matched_medium','sourcedataearlylateMX_matched_medium'};
     
   elseif regexp(tmp,'high')
-    suffix.wordtype = {'sourcedataearlylateRC_high_bslabsolute','sourcedataearlylateMX_high_bslabsolute'};
+    suffix.wordtype = {'sourcedataearlylateRC_matched_high','sourcedataearlylateMX_matched_high'};
   end
   
-  [stattime statcomplex stattimecomp avgearlyRC avglateRC avgearlyMX avglateMX semearlyRC semlateRC semearlyMX semlateMX] = mous_bfica_sourcestatistics_earlylate(subjectnames, suffix, bslflag, cfg, rootdir);
+  [stattime statcomplex stattimecomp avgearlyRC avglateRC avgearlyMX avglateMX semearlyRC semlateRC semearlyMX semlateMX] = mous_bfica_sourcestatistics_timecomplexity(subjectnames, suffix, bslflag, cfg, rootdir);
   save([savedir,mod,suffix.sourcedata,'_',savebsl,'_',suffixfreq,'_',suffend,'_',num2str(Nsubj),'subj'],'stattime','statcomplex','stattimecomp', 'avgearlyRC', 'avglateRC', 'avgearlyMX', 'avglateMX', 'semearlyRC', 'semlateRC', 'semearlyMX', 'semlateMX','-v7.3');
 
 % sentseq / sentseqtar
