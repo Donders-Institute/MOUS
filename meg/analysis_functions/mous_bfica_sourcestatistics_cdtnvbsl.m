ecdf6a670db0467e683ff7ebb07b72f578259020
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-07-16 18:44:03 +0200

    updated to deal with (1) toi selection  (2) compare active condition (sent/seq) against pre-word baseline, pre-sent bsl, or zero
    Former-commit-id: 8d7a1180cf071f28fa4ef2dd4edb9ddc8428455b

--- meg/analysis_functions/mous_bfica_sourcestatistics_cdtnvbsl.m
+++ meg/analysis_functions/mous_bfica_sourcestatistics_cdtnvbsl.m
@@ -1,5 +1,11 @@
- function [stat, Nsubj, avgcdtn, avgbsl, semcdtn, sembsl] = mous_bfica_sourcestatistics_cdtnvbsl(subj, suffix, bslflag, cfg, rootdir)
+ function [stat, Nsubj, avgact, avgbslorzero, semact, sembslorzero] = mous_bfica_sourcestatistics_cdtnvbsl(subj, suffix, bzflag, cfg, rootdir, findx)
 
+suffixstruct = isstruct(suffix);
+
+if nargin<6;
+  findx = [];
+end 
+ 
 if nargin<=3
   cfg = [];
 end
@@ -24,8 +30,6 @@ sourcemodeltemplate = sourcemodel;
 suffix.wordtypesel = 'sourcedatasentseq'; % change in order to load data
 
 for k = 1:Nsubj
-  clear tlcksent tlckseq
-  mous_db_getdata(subj{k}, ['meg_bfica_',suffix.wordtypesel, suffix.oscband], rootdir);
   if k==1
     mous_db_getdata(subj{k}, 'meg_bfica_leadfield8mm', rootdir);
     sourcemodel = rmfield(sourcemodel, 'leadfield');
@@ -33,53 +37,61 @@ for k = 1:Nsubj
       sourcemodel = rmfield(sourcemodel, 'cfg');
     end
   end
+  
+  clear tlcksent tlckseq
+  mous_db_getdata(subj{k}, ['meg_bfica_',suffix.wordtypesel,'_', suffix.oscband], rootdir);
+  
+  % determine active condition to test against bsl/zero
+  switch suffix.sourcedata
+    case {'sourcedataseqvbz'};
+      cdtn = tlckseq;
+    case {'sourcedatasentvbz'}
+      cdtn = tlcksent;
+    otherwise
+      error('unidentified suffix.sourcedata, specify "sourcedatasentvbsl" or "sourcedataseqvbsl"');
+  end
+  
+  % frequency selection (and average)
+  if isfield(suffix,'selfreq') 
+    if strcmp(suffix.avg,'no')
+      cdtn = ft_selectdata(cdtn,'foilim',suffix.selfreq);
+    elseif strcmp(suffix.avg, 'yes'); % average across frequencies
+      cdtn = ft_selectdata(cdtn,'foilim',suffix.selfreq,'avgoverfreq','yes');
+    end 
+  end
+  
+  % baseline (pre-word; select first because cdtn then becomes toi specific)
+  if bzflag == 1
+    bsl = ft_selectdata(cdtn, 'toilim',[-inf -0.09]);
+  end
+  
+  if isfield(suffix,'toi')
+    cdtn = ft_selectdata(cdtn,'toilim',suffix.toi);
+  end
+   
+ 
+  %% update dimensions and dimord
+  if isfield(suffix,'selfreq') && suffix.selfreq(1) == suffix.selfreq(2)
+    cdtn.avg = squeeze(cdtn.avg);
+    cdtn.var = squeeze(cdtn.var);
+   
+    if bzflag == 1
+      bsl.avg = squeeze(bsl.avg);
+    end 
+  end
 
-  %sourcemodel.time = tlckseq.time;  % time separately determine for bsl and cdtn
-  if isfield(tlckseq, 'freq')
-    sourcemodel.freq  = tlckseq.freq;
+  if isfield(cdtn, 'freq')  && ndims(cdtn.avg) == 3 
+    sourcemodel.freq  = cdtn.freq;
     sourcemodel.dimord = 'pos_freq_time';
   else
     sourcemodel.dimord = 'pos_time';
   end
    
-  % no log transform
-  % select necessary timepoints before placing into sourcemodel structure
-  switch suffix.wordtype
-    case {'sourcedataseqvbsl'};
-    cfg2 = [];
-    cfg2.latency = [0 0.5];
-    cdtn = ft_selectdata(cfg2, tlckseq);
-
-    if bslflag == 1
-      if strcmp(suffix.oscband,'_low')
-        cfg2.latency = [-0.1 -0.1];
-      else 
-        cfg2.latency = [-0.15 -0.1];
-      end 
-      bsl = ft_selectdata(cfg2,tlckseq); 
-    end
   
-    case {'sourcedatasentvbsl'}
-    cfg2 = [];
-    cfg2.latency = [0 0.5];
-    cdtn = ft_selectdata(cfg2, tlcksent); 
-
-    if bslflag == 1
-      if strcmp(suffix.oscband,'_low')
-        cfg2.latency = [-0.1 -0.1];
-      else 
-        cfg2.latency = [-0.15 -0.1];
-      end 
-      bsl = ft_selectdata(cfg2,tlcksent); 
-    end 
-    otherwise
-      error('unidentified input, should be "sourcedatasentvbsl" or "sourcedataseqvbsl"');
-  end 
-  
-  % insert selected data into sourcemodel structure
+  %% Set data (sent or seq)
   sourcemodel.avg.pow = cdtn.avg;
   sourcemodel.time    = cdtn.time;
-  if isfield(tlckseq,'freq')
+  if isfield(cdtn,'freq') && ndims(cdtn.avg) == 3 
     tmp                 = zeros(prod(sourcemodel.dim),size(sourcemodel.avg.pow,2),numel(sourcemodel.time));
   else 
     tmp                 = zeros(prod(sourcemodel.dim),numel(sourcemodel.time));
@@ -89,57 +101,67 @@ for k = 1:Nsubj
   act{k}         = sourcemodel;
   act{k}.pos     = sourcemodeltemplate.pos;
   
+  %% set comparison data baseline / zero
   % pre-word baseline structure
-  if bslflag == 1
+  if bzflag == 1
     sourcemodel.avg.pow = bsl.avg;
-    sourcemodel.time    = bsl.time;  %% USE CDTN.TIME? for ft_Freqanalysis need to use same timepoints
+    sourcemodel.time    = cdtn.time;  %% fool ft_sourcestatistics (need same # time points)
     if isfield(tlckseq,'freq')
       tmp = zeros(prod(sourcemodel.dim),size(sourcemodel.avg.pow,2),numel(sourcemodel.time));
     else
       tmp = zeros(prod(sourcemodel.dim),numel(sourcemodel.time));
     end
-    tmp(newinside,:,:) = sourcemodel.avg.pow;
+    tmp(newinside,:,:) = repmat(sourcemodel.avg.pow, [1 1 numel(sourcemodel.time)]);
     sourcemodel.avg.pow = tmp;
-    bslcdtn{k}             = sourcemodel;
-    bslcdtn{k}.pos         = sourcemodeltemplate.pos;  
-    sourcemodel.time   = cdtn.time;  % only change time here to fool statistics, keep time on line 96 so that sourcemodel dimensions are consistent
-  end
-end 
+    bslorzero{k}             = sourcemodel;
+    bslorzero{k}.pos         = sourcemodeltemplate.pos;  
 
-  % pre-sentence baseline structure 
-   
-  if bslflag == 2         % pre-sentence baseline
+  % set pre-sentence baseline 
+  elseif bzflag == 2         % pre-sentence baseline
     [~, ~, bslsenwhole, bslseqwhole] = mous_make_presentencebsl(subj,suffix.oscband,rootdir);
     if strcmp(suffix.wordtype,'sourcedatasentvbsl')
       bsl = bslsenwhole;
     elseif strcmp(suffix.wordtype,'sourcedataseqvbsl')
       bsl = bslseqwhole;
     end
-  end 
- 
-  
-% compute cumulative sum and ssq + determine the inside for all
+
+    % set zero condition
+  elseif bzflag == 3
+    bslorzero{k} = act{k};
+    bslorzero{k}.avg.pow(:) = 0;
+  end  % bzflag 
+
+end  % subject loop
+
+%% compute cumulative sum and ssq + determine the inside for all
 for k = 1:Nsubj
   if k==1
-    sumact      = act{k}.avg.pow;
-    sumbslcdtn  = bslcdtn{k}.avg.pow;
+    sumact      = act{k}.avg.pow;  
     ssqact      = act{k}.avg.pow.^2;
-    ssqbslcdtn  = bslcdtn{k}.avg.pow.^2;
+    if bzflag == 1
+      sumbslorzero  = bslorzero{k}.avg.pow;
+      ssqbslorzero  = bslorzero{k}.avg.pow.^2;
+    end
     allinside   = act{k}.inside;
   else
     sumact      = sumact + act{k}.avg.pow;
-    sumbslcdtn  = sumbslcdtn  + bslcdtn{k}.avg.pow;
     ssqact      = ssqact + act{k}.avg.pow.^2;
-    ssqbslcdtn  = ssqbslcdtn  + bslcdtn{k}.avg.pow.^2;
+    if bzflag == 1
+      sumbslorzero  = sumbslorzero  + bslorzero{k}.avg.pow;  
+      ssqbslorzero  = ssqbslorzero  + bslorzero{k}.avg.pow.^2;
+    end
     allinside = intersect(allinside, act{k}.inside);
   end  
 end
 alloutside = setdiff(1:size(act{1}.pos,1), allinside);
+
 for k = 1:Nsubj
   act{k}.inside = allinside(:)';
   act{k}.outside = alloutside(:)';
-  bslcdtn{k}.inside  = allinside(:)';
-  bslcdtn{k}.outside = alloutside(:)';
+  if bzflag == 1
+    bslorzero{k}.inside  = allinside(:)';
+    bslorzero{k}.outside = alloutside(:)';
+  end
 end
 
 % compute mean per condition and sem
@@ -147,12 +169,14 @@ avgact = sumact./Nsubj;
 varact = (ssqact - sumact.^2./Nsubj)./(Nsubj-1);
 semact = sqrt(varact./Nsubj);
 
-avgbslcdtn  = sumbslcdtn./Nsubj;
-varbslcdtn  = (ssqbslcdtn - sumbslcdtn.^2./Nsubj)./(Nsubj-1);
-sembslcdtn  = sqrt(varbslcdtn./Nsubj);
+if bzflag == 1
+  avgbslorzero  = sumbslorzero./Nsubj;
+  varbslorzero  = (ssqbslorzero - sumbslorzero.^2./Nsubj)./(Nsubj-1);
+  sembslorzero  = sqrt(varbslorzero./Nsubj);
+end
 
 % cfg = [ ];  % keep cfg from inarg
-% FIXME: does data for 'act' and ' bslcdtn'  need to have same time
+% FIXME: does data for 'act' and ' bslorzero'  need to have same time
 % parameters?
 cfg.method = 'montecarlo';
 cfg.statistic = 'depsamplesT';
@@ -160,5 +184,5 @@ cfg.design = [ones(1,Nsubj) ones(1,Nsubj)*2;1:Nsubj 1:Nsubj];
 cfg.ivar = 1;
 cfg.uvar = 2;
 cfg.parameter = 'avg.pow';
-stat  = ft_sourcestatistics(cfg, act{:}, bslcdtn{:});
+stat  = ft_sourcestatistics(cfg, act{:}, bslorzero{:});
 
