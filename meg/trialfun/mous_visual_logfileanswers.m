c7b7b82c3b37b8349586289d31f47e63c0f592c5
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2014-10-13 10:42:55 +0200

    made script more efficient now that all subjects have been tested function now also calculates an average (% correct answers) for each subject and the entire group (mean and SD)

--- meg/trialfun/mous_visual_logfileanswers.m
+++ meg/trialfun/mous_visual_logfileanswers.m
@@ -1,57 +1,100 @@
-function [output] = mous_visual_logfileanswers(subjectname)
+% This script extracts the SubjectName, QuestionNr and responses from
+% all specified logfiles.
+% updated on 22 May 2014 to reflect and updated set of subjectnames and to
+% correct the code NL
 
-%% Read in logfile
-% In the logfile:
-% Response 1 &  Response 2 are responses from the subject
-% Response 3 -  is the experimenter pressing the space bar
-[oritxt] = read_logfile_visual(subjectname);
+%% Get responses for single trials
+subjectname = {'V1001',	'V1002',	'V1003',	'V1004',	'V1005',	'V1006',	'V1007',	'V1008',	'V1009',	'V1010',...
+            'V1011',	'V1012',	'V1013',	'V1015',	'V1016',	'V1017',	'V1019',	'V1020',...
+            'V1022',	'V1024',	'V1025',	'V1026',	'V1027',	'V1028',	'V1029',	'V1030',...
+            'V1031',	'V1032',	'V1033',	'V1034',	'V1035',	'V1036',	'V1037',	'V1038',	'V1039',	'V1040',...
+            'V1042',	'V1044',	'V1045',	'V1046',	'V1048',	'V1049',	'V1050',...
+            'V1052',	'V1053',	'V1054',	'V1055',	'V1057',	'V1058',	'V1059',...
+            'V1061',	'V1062',	'V1063',	'V1064',	'V1065',	'V1066',	'V1068',	'V1069',	'V1070',...
+            'V1071',	'V1072',	'V1073',	'V1074',	'V1075',	'V1076',	'V1077',	'V1078',	'V1079',	'V1080',...
+            'V1081',	'V1083',	'V1084',	'V1085',	'V1086',	'V1087',	'V1088',	'V1089',	'V1090',...
+            'V1092',	'V1093',	'V1094',	'V1095',	'V1097',	'V1098',	'V1099',	'V1100',...
+            'V1101',	'V1102',	'V1103',	'V1104',	'V1105',	'V1106',	'V1107',	'V1108',	'V1109',	'V1110',...
+            'V1111',	'V1113',	'V1114',	'V1115',  'V1116',  'V1117'};
+%%
+% In each iteration of the loop a logfile is opened, data are extracted
+% as mentioned above and added to output array AllDataSingleTrials.
 
-%% get relevant lines of logfile
-% retrieve indices of logfile that logs whether subjects response is
-% correct ('hit') or incorrect ('incorrect').
-wordpresent = zeros(size(oritxt));
-for m = 1:size(oritxt,1)
-    check = regexp(oritxt{m},'QUESTION \d\d');  % check if text of interest exists
-    if ~isempty(check)
-        wordpresent(m) = 1;
-    end 
-end 
+for j = 1:numel(subjectname)
+        %% Get data
+        % Read in logfile using another MOUS specific funtion. This one
+        % again uses mous_db_getfilename in which some "bad subjects" are
+        % hard coded and for those the whole script would stop. Therefore,
+        % try/catch.
+        [oritxt] = read_logfile_visual(subjectname{j});
+        
+        % Check when the questions have been presented in logfile
+        % sum(wordpresent) == total num questions
+        wordpresent = zeros(size(oritxt));
+        for m = 1:size(oritxt,1)
+            check = regexp(oritxt{m},'QUESTION \d\d\d');
+            if ~isempty(check)
+                wordpresent(m) = 1;
+            end
+        end
+        %% Extract data
+        % get entire row of the questions
+        idx  = find(wordpresent == 1);
+        resp  = oritxt(idx);
+        % get QuestionNr, which corresponds to .wavfile name 
+        % Subjnr = subjectname 
+        % (not the running nr!), hardcoded difference between aud and vis
+        % script Axxxx  vs Vxxxx
+        A=[];
+        C={};
+        for k = 1:size(resp,1)
+            qidx = regexp(resp{k},'QUESTION \d\d\d');
+            QueNr = resp{k}(qidx+9:qidx+11); % to only get the number
+            A(3,k) = str2num(QueNr);
+            
+            subnr = str2num(subjectname{j}(2:5));
+        end
+        QuestionNr = transpose(A);
+        
+        % mark the condition (triggers) in column 4
+        for R = 1:length(QuestionNr)
+            QuestionNr(R,1) = subnr;
+            if QuestionNr(R,3) < 204
+                QuestionNr(R,4) = 4;
+            elseif QuestionNr(R,3) > 204 && QuestionNr(R,3) < 409
+                QuestionNr(R,4) = 3;
+            elseif QuestionNr(R,3) > 501 && QuestionNr(R,3) < 704
+                QuestionNr(R,4) = 2;
+            elseif QuestionNr(R,3) > 705
+                QuestionNr(R,4) = 1;
+            end
+        end
+        % Check for ppt's responses 'incorrect'
+        logincor  = zeros(size(resp));
+        for m = 1:size(resp,1)
+            checkincor = regexp(resp{m},'incorrect');
+            if ~isempty(checkincor)
+                QuestionNr(m,2) = 1;
+            end
+        end
+        
+        if j == 1
+          subjavg = zeros(1,numel(subjectname));
+        end
+        wrong = sum(QuestionNr(:,2));
+        allq  = size(QuestionNr,1);
+        subjavg(j) = ((allq-wrong)/allq)*100; % percentage correct per person
 
-idx  = find(wordpresent == 1);% index of logfile with "QUESTION... hit/incorrect" 
-%idxr = idq + 1;               % index of responses
-resp  = oritxt(idx);          % lines in logfile that only contain "QUESTION..." 
-
-% checks for a complete number of questions
-% if <48 find out why e.g., subject was only presented 23 blocks / dsq
-% crash...etc
-if numel(resp) < 48
-    warning('there are less than %d questions in the this logfile',numel(resp));
-
-logcor    = zeros(size(oritxt));
-logincor  = zeros(size(oritxt));
-for m = 1:size(oritxt,1)
-    checkcor = regexp(oritxt{m},'hit');
-    checkincor = regexp(oritxt{m},'incorrect');
-    if ~isempty(checkcor)
-        logcor(m) = 1;
-    end
-    if ~isempty(checkincor)
-        logincor(m) = 1;
-    end
+        %% Save data
+        if j == 1
+            SingleTrialResponse_vis_meg = QuestionNr;
+        else
+            SingleTrialResponse_vis_meg = [SingleTrialResponse_vis_meg;QuestionNr];
+        end
 end
 
-%% go thru and check that these are the correct lines in the logfile
-respcor = oritxt(find(logcor == 1));
-respincor = oritxt(find(logincor == 1));
+subjavg = subjavg';
+grpdesc(1) = mean(subjavg);
+grpdesc(2) = std(subjavg);
 
-
-%% organise information
-% number & percentage correct
-numel(respcor)
-
-% number & percentage incorrect
-numel(respincor)
-
-
-end 
-
+save('QuestionResponses_vis_meg_13Oct2014','SingleTrialResponse_vis_meg','subjavg','grpdesc');
