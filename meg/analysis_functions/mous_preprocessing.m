13a85867dc02060d40c9a170c7b2e23c6b8fcadb
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2014-11-19 14:21:58 +0100

    some clean up
    Former-commit-id: 01e6b485f5058c52a226d7fbae21ec9c6cae6f97

--- meg/analysis_functions/mous_preprocessing.m
+++ meg/analysis_functions/mous_preprocessing.m
@@ -1,35 +1,36 @@
-function [data] = mous_preprocessing(filename, trl, resamplefs, analysisType)
+function [data] = mous_preprocessing(filename, trl, resamplefs, analysisType, inputcfg)
 
 %% Preprocess data
-% Created by AH, NL. 31-5-2012
+% Created by AH, NL. 31-5-2012, and adjusted by JM
 
-%% Do filtering BEFORE down sampling
-% define filter parameters common to TFR and ERFs
-cfg            = [];
-cfg.dataset    = filename;
-cfg.trl        = trl;
-cfg.continuous = 'yes';
-cfg.channel    = {'MEG' 'EEG057' 'EEG058'};
-
-
-if (strcmp(analysisType, 'TFR') > 0)
+if nargin<5,
+  % if no custom cfg is in the input, specify the options for preprocessing
+  % here
+  cfg         = [];
+  cfg.channel = {'MEG' 'EEG057' 'EEG058'};
+  
+  
+  %% Do filtering BEFORE down sampling
+  % define filter parameters common to TFR and ERFs
+  
+  if (strcmp(analysisType, 'TFR') > 0)
     %TFR-specific parameters
     cfg.dftfilter  = 'yes';
     cfg.padding    = 4;
     
-elseif (strcmp(analysisType, 'ERF') > 0)
+  elseif (strcmp(analysisType, 'ERF') > 0)
     % ERF-specific parameters
-%     cfg.lpfilter        = 'yes';   % apply lowpass filter
-%     cfg.lpfreq          = 40;
-%     cfg.lpfiltord       = 100;
-%     cfg.lpfilttype      = 'fir';
-%     
-%     cfg.hpfilter        = 'yes';
-%     cfg.hpfilttype      = 'fir'; 
-%     cfg.hpfiltord       = 1800;  
-%     cfg.hpfreq          = 0.5;
-%     cfg.padding         = 4;    % big padding for hp to work   
-
+    %     cfg.lpfilter        = 'yes';   % apply lowpass filter
+    %     cfg.lpfreq          = 40;
+    %     cfg.lpfiltord       = 100;
+    %     cfg.lpfilttype      = 'fir';
+    %
+    %     cfg.hpfilter        = 'yes';
+    %     cfg.hpfilttype      = 'fir';
+    %     cfg.hpfiltord       = 1800;
+    %     cfg.hpfreq          = 0.5;
+    %     cfg.padding         = 4;    % big padding for hp to work
+    
     cfg.bpfilter   = 'yes';
     cfg.bpfilttype = 'fir';
     cfg.bpfiltord  = 600;
@@ -40,10 +41,16 @@ elseif (strcmp(analysisType, 'ERF') > 0)
     %cfg.custom.funhandle = @ft_preproc_highpass_box;
     %cfg.custom.varargin  = 1200;
     
-else
+  else
     error('unrecognized type requested');
+  end
+else
+  cfg = inputcfg;
 end
 
+cfg.dataset    = filename;
+cfg.trl        = trl;
+cfg.continuous = 'yes';
 data = ft_preprocessing(cfg);
 
 %% Downsample data (to resamplefs = target frequency)
@@ -57,30 +64,30 @@ data = ft_preprocessing(cfg);
 cfg            = [];
 cfg.resamplefs = resamplefs;
 cfg.detrend    = 'no';  % not good for evoked data
-cfg.demean     = 'yes';
+cfg.demean     = 'no';
 cfg.trials     = 'all';
 data = ft_resampledata(cfg, data);
 
 
 %% Do ERF baseline correction AFTER downsampling.
 %% so that different baselines can be applied.
-
-cfg            = [];
-cfg.channel    = {'MEG' 'EEG057' 'EEG058'};
-
-if (strcmp(analysisType, 'TFR') > 0)
+if nargin<5,
+  cfg            = [];
+  cfg.channel    = {'MEG' 'EEG057' 'EEG058'};
+  
+  if (strcmp(analysisType, 'TFR') > 0)
     cfg.detrend    = 'yes';
     
-elseif (strcmp(analysisType, 'ERF') > 0)
+  elseif (strcmp(analysisType, 'ERF') > 0)
     cfg.detrend    = 'no';
     cfg.demean     = 'yes';
     cfg.baselinewindow  = [-inf 0];
-else
+  else
     error('unrecognized type requested');
     
+  end
+  
+  data = ft_preprocessing(cfg, data);
 end
-
-data = ft_preprocessing(cfg, data);
-
 
 
