142702e9970b3921fd73573f8966581b0f5fcc8e
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2014-12-29 10:14:38 +0100

    cleaned up code and annotated it more extensively. No functional changes

--- meg/analysis_scripts/mous_mne_pipeline.m
+++ meg/analysis_scripts/mous_mne_pipeline.m
@@ -1,40 +1,63 @@
-if ~exist('domne_main',       'var'), domne_main       = 0; end
-if ~exist('domne_parametric', 'var'), domne_parametric = 0; end
-if ~exist('domne_parcellate', 'var'), domne_parcellate = 0; end
+% specification of flags that determine which section(s) of the pipeline
+% will be executed
+if ~exist('domne_main',       'var'),  domne_main        = 0; end
+if ~exist('domne_parametric', 'var'),  domne_parametric  = 0; end
+if ~exist('domne_parcellate', 'var'),  domne_parcellate  = 0; end
 if ~exist('domne_parcellate2', 'var'), domne_parcellate2 = 0; end
-if ~exist('domne_denoise', 'var'),    domne_denoise    = 0; end
-if ~exist('dodspm', 'var'),           dodspm           = 0; end     
-if ~exist('domne_earlylate', 'var'), domne_earlylate = 0; end
+if ~exist('domne_denoise',     'var'), domne_denoise     = 0; end
+if ~exist('dodspm',            'var'), dodspm            = 0; end     
+if ~exist('domne_earlylate',   'var'), domne_earlylate   = 0; end
 if ~exist('domne_parametric_rc', 'var'), domne_parametric_rc = 0; end
 
+% specify the directory into which the results will be saved
 if ~exist('rootdir', 'var')
   rootdir = '/project/3011020.09/MEG';
 end
   
 if domne_main,
-  if ~exist('suffix_rawdata', 'var')
-    suffix_rawdata = 'meg_erf_allwords_02-nextword';
-  end
-  if ~exist('suffix_erfdata', 'var')
-    suffix_erfdata = 'meg_erf_allwords_02-nextword-allwords-ag';
-  end
-  if ~exist('suffix_mnedata', 'var')
-    suffix_mnedata = strrep(suffix_rawdata, 'erf', 'mne');
-  end
+  % This section computes the the minimum-norm solution for the two
+  % original main conditions of interest: sentences and sequences,
+  % based on a 'common' spatial filter, i.e. the inverse operator
+  % is computed with a regularisation noise covariance matrix that
+  % has been computed across the conditions of interest. 
+  % 
+  % - the noise covariance is computed based on a specified datafile
+  %   containing a 'raw' data structure, based on suffix_rawdata,
+  %   taking only the pre-zero timewindow of the first word in the sentence/sequence
+  % - the source level data is computed by projecting sensor level
+  %   ERFs through the inverse operator. the datafile for the ERFs
+  %   is based on suffix_erfdata
+  % - the results will be saved in file, based on suffix_mnedata
+  % - the assumption is that the suffix_erfdata file contains the
+  %   ERFs of the respective main conditions
+
+  % specify the suffices if not specified by the user
+  if ~exist('suffix_rawdata', 'var'), suffix_rawdata = 'meg_erf_allwords_02-nextword'; end
+  if ~exist('suffix_erfdata', 'var'), suffix_erfdata = 'meg_erf_allwords_02-nextword-allwords-ag'; end
+  if ~exist('suffix_mnedata', 'var'), suffix_mnedata = strrep(suffix_rawdata, 'erf', 'mne'); end
   
-  % compute covariance matrix of the noise
-  % use an equal amount of sentence and sequence 'baselines' for the cov
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % computation of the noise covariance matrix
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  % get the single trial data
   mous_db_getdata(subjectname, suffix_rawdata, rootdir);
-  data.grad = ft_convert_units(data.grad, 'm'); % this is related to an FT issue with the units
-  % if all is explicitly set to 'm' it should be OK. 
+  data.grad = ft_convert_units(data.grad, 'm');
  
   data     = ft_selectdata(data, 'toilim', [-inf 0.6]);
   database = ft_selectdata(data, 'toilim', [-inf 0]);
+
+  % the following classifies the single words according to main condition and whether
+  % it reflects the first word in the sentence, NOTE: this assumes the condition trigger
+  % to be in the second column of the trialinfo field, and the ordinal word index in the 
+  % fifth row of the trialinfo field
   selsent  = find(ismember(database.trialinfo(:,2),[1 2 5 6]) & database.trialinfo(:,5)==1);
   selseq   = find(ismember(database.trialinfo(:,2),[3 4 7 8]) & database.trialinfo(:,5)==1);
+  
+  % equate the number of trials that enter into the computation across conditions
   n        = min(numel(selsent),numel(selseq));
-  tmp      = randperm(numel(selsent));selsent=sort(selsent(tmp(1:n)));
-  tmp      = randperm(numel(selseq));selseq=sort(selseq(tmp(1:n)));
+  tmp      = randperm(numel(selsent)); selsent = sort(selsent(tmp(1:n)));
+  tmp      = randperm(numel(selseq));  selseq  = sort(selseq(tmp(1:n)));
   
   cfg                  = [];
   cfg.channel          = 'MEG';
@@ -46,37 +69,43 @@ if domne_main,
   cfg.preproc.baselinewindow = [-inf 0];
   cfg.keeptrials       = 'no';
   cfg.trials           = sort([selsent(:);selseq(:)]);
-  tlck = ft_timelockanalysis(cfg, database);
+  tlck                 = ft_timelockanalysis(cfg, database);
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % preparation of the anatomical data
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % load the 2D sourcemodel and deal with the midline
   mous_db_getdata(subjectname,'meg_anatomy_sourcemodel2D_surfreg');
-  if exist('bnd', 'var')
-    sourcemodel = bnd; clear bnd;
+  if exist('bnd', 'var'),
+    sourcemodel = bnd; 
+    clear bnd;
   end
-  if ~isfield(sourcemodel, 'pos') && isfield(sourcemodel, 'pnt')
+  if ~isfield(sourcemodel, 'pos') && isfield(sourcemodel, 'pnt'),
     sourcmodel.pos = sourcemodel.pnt;
     sourcemodel    = rmfield(sourcemodel, 'pnt');
   end
-  sourcemodel = ft_convert_units(sourcemodel, 'm'); % due to an FT issue related to units
-  % explicit conversion to 'm'. This should be OK  
+  sourcemodel = ft_convert_units(sourcemodel, 'm');  
   % NOTE: the areas of the triangles are not updated, I think that this is not a problem
   % because the area weighting is relative, i.e. the scale of the numbers shouldn't matter.
   % Yet, I am not 100% sure. FIXME
  
+  % define the medial wall parcel as outside. NOTE: this assumes
+  % the medial wall te have a value of 2
   load atlas_conte69_8196reg_LR_brodmann_subparc
-  sourcemodel.inside  = find(atlas.parcellation~=2);% & atlas.parcellation2~=1);
-  sourcemodel.outside = find(atlas.parcellation==2);% | atlas.parcellation2==1);
+  sourcemodel.inside  = find(atlas.parcellation~=2);
+  sourcemodel.outside = find(atlas.parcellation==2);
   sourcemodelorig     = sourcemodel;
   
-  % load the volume conductor model of the head
+  % load the volume conduction model of the head
   mous_db_getdata(subjectname, 'meg_anatomy_headmodel');
-  if exist('vol', 'var')
-    headmodel = vol; clear vol;
+  if exist('vol', 'var'),
+    headmodel = vol; 
+    clear vol;
   end
-  %headmodel = ft_convert_units(headmodel, 'cm');
   headmodel = ft_convert_units(headmodel, 'm');
   
-  % Compute the leadfields
+  % pre-compute the leadfields
   cfg          = [];
   cfg.grad     = tlck.grad;
   cfg.vol      = headmodel;
@@ -86,8 +115,9 @@ if domne_main,
   %cfg.normalize = 'yes';
   sourcemodel  = ft_prepare_leadfield(cfg);
   
-  
-   %% Compute MNE for each condition
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % computation of the MNE inverse operator
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % this is a bit clunky, but in order to make the pipeline general purpose
   % (i.e. using uniform variable names), it has to try out the possible
@@ -103,12 +133,18 @@ if domne_main,
   else
     error('don''t know which variable to use');
   end
-  [a,b]     = match_str(data1.label,tlck.label); % added 2014-06-20 to ensure the correct order of channels in covariance and label list!!!!!
+  
+  % the following is to ensure the correct order of channels in the covariance
+  % compared to the data. added 2014-06-20
+  [a,b]     = match_str(data1.label,tlck.label);
   data1.cov = zeros(numel(data1.label));
   data2.cov = zeros(numel(data2.label));
-  data1.cov(a,a) = tlck.cov(b,b); % add the covariance computed from both conditions
-  data2.cov(a,a) = tlck.cov(b,b);
   
+  % add the covariance computed from both conditions
+  data1.cov(a,a) = tlck.cov(b,b);
+  data2.cov(a,a) = tlck.cov(b,b);
+
+  % only select up until 0.6 seconds after word onset  
   data1 = ft_selectdata(data1, 'toilim', [-inf 0.6]);
   data2 = ft_selectdata(data2, 'toilim', [-inf 0.6]);
   
@@ -121,13 +157,11 @@ if domne_main,
     % take hs1
     vertex_area = zeros(size(sourcemodelorig.pos,1),1);
     for k = 1:size(sourcemodelorig.pos,1)
-      sel = find(sum(sourcemodelorig.tri==k,2));
+      sel              = find(sum(sourcemodelorig.tri==k,2));
       vertex_area(k,1) = sum(sourcemodelorig.area(sel))./numel(sel);
     end
-    
     weightlim = 5;
     weightexp = 0.5;
-    
     
     % this part computes the sum of squares of the leadfields, and uses the
     % inverse of it for depth weighting.
@@ -148,7 +182,6 @@ if domne_main,
     % times the 1./leadfield-sum-of-square to the power of weightexp
     % weighting in bst_wmne
     S = spdiags(A(:),0,speye(numel(A)));
-    
   end
   
   cfg                 = [];
@@ -162,30 +195,34 @@ if domne_main,
   cfg.mne.noiselambda = 0.2*trace(data1.cov)./size(data1.cov,1);
   cfg.mne.sourcecov   = S;
   source_sent         = ft_sourceanalysis(cfg, data1);
-  cfg.mne.keepfilter  = 'no'; % the spatial filter will be the same as for source_sent, redundant: added this line 2014-06-19
+  
+  % don't keep the filter for the list condition,
+  % the spatial filter will be the same as for source_sent.
+  % added this line 2014-06-19
+  cfg.mne.keepfilter  = 'no'; 
   source_seq          = ft_sourceanalysis(cfg, data2);
   
-  cfg            = [];
-  cfg.demean     = 'yes';
+  
+  cfg                = [];
+  cfg.demean         = 'yes';
   cfg.baselinewindow = [-0.2 0];
-  cfg.projectmom = 'yes';
-  cfg.zscore     = 'no';
+  cfg.projectmom     = 'yes';
+  cfg.zscore         = 'no';
   
   % we probably don't want the projection to be condition specific, rather
   % compute the orientation on the conditions combined.
-  
   sd_Sent        = ft_sourcedescriptives(cfg, source_sent);
   sd_Seq         = ft_sourcedescriptives(cfg, source_seq);
   
   source         = source_sent;
-  % source.avg.pow = (source_sent.avg.pow+source_seq.avg.pow)./2;
   for k = 1:numel(source.inside)
     mom = (source.avg.mom{source.inside(k)} + source_seq.avg.mom{source.inside(k)})./2;
     source.avg.mom{source.inside(k)} = mom;
   end
-  sd             = ft_sourcedescriptives(cfg, source);
-  sd_Seq.avg.ori = sd.avg.ori;
+  sd              = ft_sourcedescriptives(cfg, source);
+  sd_Seq.avg.ori  = sd.avg.ori;
   sd_Sent.avg.ori = sd.avg.ori;
+  
   % replace the pow with the orientation from the combined data
   for k = 1:numel(sd.inside)
     indx = sd.inside(k);
@@ -206,16 +243,19 @@ if domne_main,
   sd_Sent.avg = rmfield(sd_Sent.avg, 'mom');
   sd_Seq.avg  = rmfield(sd_Seq.avg,  'mom');
   
-  % save the solution
+  % save the output
   source = sd_Seq;
-  mous_db_putdata(subjectname, [suffix_mnedata,'_seq'],  'source', rootdir,1); % removed the currentdensity_weighted on 2014-06-19
+  mous_db_putdata(subjectname, [suffix_mnedata,'_seq'],  'source', rootdir, 1);
   
   source = sd_Sent;
-  mous_db_putdata(subjectname, [suffix_mnedata,'_sent'], 'source', rootdir,1);
+  mous_db_putdata(subjectname, [suffix_mnedata,'_sent'], 'source', rootdir, 1);
 end
 
 if domne_parametric
-  % this part assumes that it can use precomputed MNE filters, and that the
+  % This section computes the source level representation of the 'parametric'
+  % effect, i.e. the slope fitted (for each time point in the ERF) as a function
+  % of ordinal word position. 
+  % This part assumes that it can use precomputed MNE filters, and that the
   % filters have been computed on the same data as the one that will be
   % projected
   
@@ -256,8 +296,7 @@ if domne_parametric
   
   % move around the columns in the trialinfo field so that the condition
   % trigger ends up in the third column and the word ordinal indicator in
-  % the second
-  % FIXME this is hard coded expected based on XXX_erf_allwords_01-10
+  % the second, this is hard coded!
   data.trialinfo = data.trialinfo(:,[1 5 2 3 4]);
   
   [tlck_sent, stat_sent, stat2_sent, mu_sent] = mous_makecontrast(data, 'wordsent_parametric_blc', [], F);
@@ -366,8 +405,6 @@ if domne_parametric_rc
   stat = rmfield(stat, {'prob', 'mask', 'cirange'});
   mu   = single(mu_seq);
   mous_db_putdata(subjectname, [strrep(suffix_mne, 'sent', 'seq'),'_mix_parametric_blc'], 'tlck', 'stat', 'mu', rootdir,1);
-  
-  
 end
 
 if domne_earlylate
