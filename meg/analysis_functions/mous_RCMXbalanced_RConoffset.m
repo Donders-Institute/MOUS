fd918fc8efec4d3a2f04bf12ed639ce29f79d986
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2015-01-20 19:11:11 +0100

    Compute RCearly, RClate, MXearly, MXlate.  Across conditions, trials are matched for word positions (and therefore, there are an equal number of trials for conditions forming one contrast. Also in progress is the selection of trials in the onset and offset of the RC

--- meg/analysis_functions/mous_RCMXbalanced_RConoffset.m
+++ meg/analysis_functions/mous_RCMXbalanced_RConoffset.m
@@ -1,4 +1,5 @@
-function varargout = mous_RCMXbalanced_RConoffset(sourcedata,toilop,contrast)
+function varargout = mous_RCMXbalanced_RConoffset(sourcedata,contrast, rseed)
+% don't need to specify 'toilop' because will use randomseed when calling qsub
 % [RCearly, RClate, MXearly, MXlate] = mous_RCearlylatevsMXearlylate
 % [RConset  RCoffset]                = mous_RcearlylatevsMXearlylate
 
@@ -13,18 +14,15 @@ function varargout = mous_RCMXbalanced_RConoffset(sourcedata,toilop,contrast)
 % and neatness
 
 
-%% extract trialinfo for easier manipulation
-trialinfo = sourcedata.trialinfo;
-
 %% [trialinfo] add column 7 to reflect sentence length after artifact rejection
 % 1          2            3              4                           5                                6          7 
 % sentcount  ordinalpstn  word trigger   #smp. relative to 1st word  #smps btw word onset and offset  .wavfile   total num words
-sentid = unique(trialinfo(:,1));       
+sentid = unique(sourcedata.trialinfo(:,1));       
 nwords = zeros(size(sentid,1),size(sentid,2));
 for kk = 1:numel(sentid)
-  currlen = max(trialinfo(trialinfo(:,1)==sentid(kk),2));  % extract sentence length
-  i = find(trialinfo(:,1) == sentid(kk));                  % get number of words in sentence
-  trialinfo(i(1):i(end),7) = currlen*ones(numel(i),1);     % add senlen to these words
+  currlen = max(sourcedata.trialinfo(sourcedata.trialinfo(:,1)==sentid(kk),2));  % extract sentence length
+  i = find(sourcedata.trialinfo(:,1) == sentid(kk));                  % get number of words in sentence
+  sourcedata.trialinfo(i(1):i(end),7) = currlen*ones(numel(i),1);     % add senlen to these words
 end
 
 %% [trialinfo] - rearrange columns suitable as input to mous_makecontrast{early-late} 
@@ -32,8 +30,11 @@ end
 % 1         2            3             4              5                                6                                7         8
 % sentence, ordinalpstn, word trigger, total # words, smp# relative to 1st word onset, smp# btw word onset-word offset, .wavfile, uniqueID for each
 % word
-trialinfo = trialinfo(:,[1 2 3 7 4 5 6]);
-trialinfo(:,8) = (trialinfo(:,1)*10)+trialinfo(:,2); % uniqueID for each word
+sourcedata.trialinfo = sourcedata.trialinfo(:,[1 2 3 7 4 5 6]);
+sourcedata.trialinfo(:,8) = (sourcedata.trialinfo(:,1)*100)+sourcedata.trialinfo(:,2); % uniqueID for each word
+
+%% extract trialinfo for easier manipulation
+trialinfo = sourcedata.trialinfo;
 
 %% [select data]
 switch contrast
@@ -44,7 +45,8 @@ switch contrast
     sel2 = find(ismember(trialinfo(:,3),[5 6]));  %sent MX [sen: 5 6  seq: 7 8]
     dat2 = ft_selectdata(sourcedata,'rpt',sel2);
 
-  case 'RConoff'
+  case 'RConoff' 
+    %% WORK IN PROGRESS %%
     load('/home/language/nielam/MOUS/meg/trialfun/mous_stimuli.mat');
     % 001 - 204 = RC, but  file 107 and 164 do not exist (in experiment or in
     % this mat file) which leads to error when using square brackets
@@ -112,33 +114,16 @@ end
 % might be better to ignore sentences with <3 positions for onset, and
 % offset
 % balance assumes that word positions are consecutive (no gaps, e.g., 3 5 6)
-tmp = unique(dat1.trialinfo(:,2));  % word positions in RC cdtn
+tmp  = unique(dat1.trialinfo(:,2));  % word positions in RC cdtn
 tmp2 = unique(dat2.trialinfo(:,2)); % word positions in MX cdtn
 matchedpstn = min(numel(tmp),numel(tmp2));  
 
 
-%% remove additional word position from the condition that has more % FIXME: remove - not necessary
-% use same word positions used for n-1, n-2 and n-3
-% if min(numel(tmp) ~= numel(tmp2))
-%    sel = find(sentRC.trialinfo(:,2) ~= matchedpstn+1);
-%    sentRC = ft_selectdata(sentRC,'rpt',sel);  
-% 
-%    sel = find(sentMX.trialinfo(:,2) ~= matchedpstn+1);
-%    sentMX = ft_selectdata(sentMX,'rpt',sel);
-% end
-
 %% Balance the number of trials at each word position
-% the output from here is submitted to mous_makecontrast(X,'early-late')
-% Within a frequency range, retain same trial across frequencies, at each time point
-
-  if toilop == 1  &&  (frequency == 2.5 || frequency == 12 || frequency == 40)
-    x = randomseed([]); % does it work prior to calling mous_makecontrast in mous_bfica_pipeline?
-  end 
-
-  % Each word position and each time point, the same trials are used for
-  % all frequencies in the same range.
-  % This is independent of whether or not number of trials are balanced for
-  % each word position between the conditions (RC/MX; onset/offset)
+% randomseed means more consistency for analyzing across frequencies
+  % For each wordposition-timepoint pair the same trials are used for
+  % all frequencies in the same range (low, medium, high)
+  % Due to matching, there will be an equal number of trials per condition
   for q = 1:matchedpstn
     % equate number of trials for each condition
     sel1 = find(dat1.trialinfo(:,2) == q);  n1 = numel(sel1);
@@ -146,7 +131,7 @@ matchedpstn = min(numel(tmp),numel(tmp2));
     n = min(n1,n2);
     
     % randomise trials (because not selecting all, make a random subselection)
-    randomseed(x);  
+    randomseed(rseed);  % seed determined when calling qsub.
     tmp1 = randperm(n1);
     tmp2 = randperm(n2);
 
@@ -159,22 +144,45 @@ matchedpstn = min(numel(tmp),numel(tmp2));
     if q == 1
       idat1all = idat1;  idat2all = idat2;
     else
-      idat1all = [iRCall; idat1];
-      idat2all = [idat2all; idat2];
+      idat1all = [idat1all; idat1]; % RC
+      idat2all = [idat2all; idat2]; % MX
     end
   end % loop for word position
   
   switch contrast  % assume averaging across trials for each condition (i.e. 3 word positions per cdtn)
     case 'RCMX'
-      % return data (including trialinfo)
-      % data used as input for 'mous_makecontrast(X,early-late')
-      varargout{1} = ft_selectdata(dat1,'rpt',idat1all);
-      varargout{2} = ft_selectdata(dat2,'rpt',idat2all);
+      % dat1 = RC, dat2 = MX
+      RCall = ft_selectdata(dat1,'rpt',idat1all);  
+      MXall = ft_selectdata(dat2,'rpt',idat2all);
+      
+      endpstn = matchedpstn - [1 2 3];
+      i   = find(ismember(RCall.trialinfo(:,2),[2 3 4]));
+      RCe = ft_selectdata(RCall,'rpt',i);
+      
+      i   = find(ismember(RCall.trialinfo(:,2),endpstn));
+      RCl = ft_selectdata(RCall,'rpt',i);
+      
+      i   = find(ismember(MXall.trialinfo(:,2),[2 3 4]));     
+      MXe = ft_selectdata(MXall,'rpt',i);
+      
+      i   = find(ismember(MXall.trialinfo(:,2),endpstn));      
+      MXl = ft_selectdata(MXall,'rpt',i);
+      
+      % index of each trial from each condition in original (full) dataset
+      [c,i1] = intersect(sourcedata.trialinfo(:,8),RCe.trialinfo(:,8));       
+      [c,i2] = intersect(sourcedata.trialinfo(:,8),RCl.trialinfo(:,8));
+      [c,i3] = intersect(sourcedata.trialinfo(:,8),MXe.trialinfo(:,8)); 
+      [c,i4] = intersect(sourcedata.trialinfo(:,8),MXl.trialinfo(:,8)); 
+      
+      varargout{1} = i1;
+      varargout{2} = i2;
+      varargout{3} = i3;
+      varargout{4} = i4;
 
     case 'RConoff'
       % return indices to select trials
       varargout{1} = sel1;  % RConset
       varargout{2} = sel2;  % RCoffset
   %     varargout{3} = sel3;  % MXonset  (or word list?)
-  %     varargout{4} = sel4;  % MXoffset (or wordl list?)
+  %     varargout{4} = sel4;  % MXoffset (or word list?)
   end 
