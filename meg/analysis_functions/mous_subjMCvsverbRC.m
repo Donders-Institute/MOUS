301ba51d5eb54df852ee1d40084f0b356a283b16
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2015-01-22 17:46:59 +0100

    updated to become more efficient; currently still work in progress
    Former-commit-id: 5b0c030efc0087366f261bebc8db33d01b18b5b3

--- meg/analysis_functions/mous_subjMCvsverbRC.m
+++ meg/analysis_functions/mous_subjMCvsverbRC.m
@@ -1,114 +1,156 @@
-function [senRCsubjmc, senRCverbrc, senMXsubjmc, senMXverbrc, seqRCsubjmc, seqRCverbrc] = mous_subjMCvsverbRC(data)
+function [subjMC_RC, verbRC_RC, subjMC_MX, verbRC_MX, subjMC_RCWL, verbRC_RCWL]= mous_subjMCvsverbRC(data, rseed)
          
-
 % In all sentences with a relative clause this function selects
-% i)  subject in the main clause (senRCsubjmc), 
-% ii) verb in the relative clause that refers to the subject (senRCverbrc).
+% i)  subjMC_RC:  subject in the main clause (senRCsubjmc), 
+% ii) verbRC_RC:  verb in the relative clause that refers to the subject (senRCverbrc).
 % Subsequently, words at equivalent word positions to the aforementioned are then selected (perfect/pseudo
 % match) from
-% ~  sentences with relative clauses (aka 'RC-', aka 'MIX' sentences (MX = MIX)): to produce
-%     iii) senMXsubjmc 
-%     iv)  senMXverbrc
-% ~  word lists formed from relative clause sentences (aka 'RC+ word list' (WL = word list))
-%       v) seqRCsubjmc
-%      vi) seqRCverbrc
+% sentences with relative clauses (aka 'MX' for MIX sentences)
+%  iii) subjMC_MX
+%  iv)  verbRC_MX
+% word lists formed from relative clause sentences (aka 'RC+ word list' (WL = word list))
+%  v) subjMC_RCWL
+%  vi) verbRC_RCWL
 
-% These 6 types of trials can then be contrasted in the following manner in
-% mous_makecontrast:
-% senRCsubjmc-senRCverbmc  vs.  senMXsubjmc-senMXverbmc    vs.  seqRCsubjmc-seqRCverbmc
-% senRCsubjmc              vs.  senMXsubjmc                vs.  seqRCsubjmc
-% senRCverbrc              vs.  senMXverbrc                vs.  seqRCverbrc  
+% sentences are retained if they have a subjMC and verbRC after artifact
+% detection, and are not a special sentence (i.e. sentencetype = [0 0 0 0];
+% A perfect match means: same word position and sentence/Wordlist length
+% A psuedo match means:  same word postiion, and closest sentence/wordlist
+% length
 
-% To ensure that the same trials are selected for all frequencies at the
-% same timepoint, randomseed() is made use when calling the analysis at the
-% level of mous_bfica_pipeline, using qsub.
+% If a perfect match (in the MX and RCWL cannot be found, a pseudomatch is
+% used in order to retain as many trials as possible (across all conditions).
+% a pseudo match is reasonable because subjects do know #sent/wordlist length
 
-% nielam 10 November 2014
+% These 6 types of trials can then be contrasted as follows:
+% subjMC_RC - verbRC_RC  vs.  subjMC_RCWL - verbRC_RCWL  vs.  subjMC_MX - verbRC_MX
+% NL; 10 November 2014
 
-% get trialinfo and create unique ID for each trial
-trialinfo = data.trialinfo;
-trialinfo(:,7) = trialinfo(:,6)*100 + trialinfo(:,2);
+% get stimuli
+load('/home/language/nielam/MOUS/meg/trialfun/mous_stimuli');
 
-% stimuli details:
-load('/home/language/nielam/MOUS/meg/trialfun/mous_stimuli.mat');
-s = [107 164];
-  for k = 1:2 % stupid, but couldn't get multiple fields option to work 
-    stimuli(s(k)) = setfield(stimuli(s(k)),'id',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'string',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'numwords',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'RConsetword',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'MCcontinuationword',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'numadditionalclauses',NaN);
-    stimuli(s(k)) = setfield(stimuli(s(k)),'additionalclauseinfo',NaN);
-  end
-% FIXME: add fields to stimuli which will mark the RCsubjMC word position,
-% and RCverbRC word position, as well as categories additional
-% characteristics of the sentence: setfield()
+%% create unique ID for each trial
+data.trialinfo(:,7) = data.trialinfo(:,6)*100 + data.trialinfo(:,2); 
 
-% stimuli(1:numel(stimuli).subjMC) = ??
-  % adverbial phrase = [startpstn endpstn]; else []
-  % subjORobjRC      = ['s'] or ['o']
-  % rightbranching   = [startpstn endpstn];
-  % To determine whether or not RC is embedded, just see if there if the 
-  % 'MCcontinuation' field has a word position or not.
+%% get original sentence length (from original stimuli, not after artifact rejection)
+%  will need to match RCsentences, MXsentences and RCwordlists to be of
+%  same length
+for k = 1:size(data.trialinfo,1)
+  data.trialinfo(k,8) = stimuli(data.trialinfo(k,6)).numwords;  
+end
 
-% % extract info from stimuli.mat
-% allID   = [stimuli.id];
-% allsubj = [stimuli.subjMC];        
-% allverb = [stimuli.verbRC4subj];
+% get trialinfo 
+trialinfo = data.trialinfo;   
 
-%% select trials
-% RC, MX and WL
-% senRC[1 2], seqRC[3 4], sentMX[5 6], seqMX[7 8]
+%% select trials from current experiment (expRC, expMX, expRCWL)
+% RC, MX and WL (senRC[1 2], seqRC[3 4], sentMX[5 6], seqMX[7 8])
 sel  = find(ismember(trialinfo(:,3),[1 2]));  
-senRCtrl = trialinfo(sel,:);
-
+expRC = trialinfo(sel,:);
 sel  = find(ismember(trialinfo(:,3),[5 6]));  
-senMXtrl = trialinfo(sel,:);
-
+expMX = trialinfo(sel,:);
 sel  = find(ismember(trialinfo(:,3),[3 4]));  
-seqRCtrl = trialinfo(sel,:);
+expRCWL = trialinfo(sel,:);
 
-% number of RC in dataset
-[f,i] = unique(senRCtrl(:,6),'first'); 
-fname = senRCtrl(sort(i),6);
-numrc    = numel((find(ismember(fname,1:204))));
 
-% senRCsubjmc
-for k = 1:numrc
-%   id             = stimuli(fname(k)).id*100 + stimuli(fname(k)).subjMC;    % actual 
-  id             = stimuli(fname(k)).id*100 + stimuli(fname(k)).RConsetword; % test run
-  tmp            = find(senRCtrl(:,7) == id);
-  if ~isempty(tmp)
-    idx(k) = tmp;
-  else 
-    idx(k) = NaN;  % trial removed during artifact rejection
+%% extract subjMC_RC trials
+% select all subjMC_RC: use stimID-subjpstn pair
+uq      = unique(expRC(:,6));
+for k = 1:numel(uq)
+  sent_subjpstn(k) = stimuli(uq(k)).id*100 + stimuli(uq(k)).subjMC;
+end
+idx    = find(ismember(expRC(:,7),sent_subjpstn)); % some of allsubjRC may not be in expRC due to artifact rejection
+subjMC_RC = expRC(idx,:);
+
+%% extract verbRC_RC trials
+uq      = unique(expRC(:,6));
+for k = 1:numel(uq)
+  sent_subjpstn(k) = stimuli(uq(k)).id*100 + stimuli(uq(k)).verb2subjMC;
+end
+idx    = find(ismember(expRC(:,7),sent_subjpstn)); % some of allsubjRC may not be in expRC due to artifact rejection
+verb2subjMC_RC = expRC(idx,:);
+
+%% remove RC sentences that do not have a subj-verb pair
+[~,iverb, isubj] = intersect(verb2subjMC_RC(:,6),subjMC_RC(:,6));
+verb2subjMC_RC = verb2subjMC_RC(iverb,:);
+subjMC_RC      = subjMC_RC(isubj,:);
+
+%% remove RC sentences that ~senttype = [0 0 0 0] 
+
+senttype = [stimuli(subjMC_RC(:,6)).sentencetype];   % get senttype for remaining RC sentences
+senttype = reshape(senttype,[4,size(subjMC_RC,1)])'; 
+i = any(senttype,2);                                 % identify special senttype
+verb2subjMC_RC = verb2subjMC_RC(~i,:);               % remove special senttype
+subjMC_RC      = subjMC_RC(~i,:);
+ 
+
+%% Find MX matches
+% create subjMC-verb2subjMC pairs for RC
+RCpair      = zeros(size(subjMC_RC,1),2);
+RCpair(:,1) =      subjMC_RC(:,8)*100 +      subjMC_RC(:,2);  % sentencelength-wordpstn pair
+RCpair(:,2) = verb2subjMC_RC(:,8)*100 + verb2subjMC_RC(:,2);
+RCpair(:,3) = false  % flag as true when match has been found
+
+% if multiple match options; permute and select first one
+% sort RCpair in order of sentencelength-wordposition
+
+% FIXME: how to ensure that 1105-1108 are chosen from same MXsentence?
+
+% 1 create sentencelength-wordpstn pairs in MX
+expMX(:,9) = expMX(:,8)*100 + expMX(:,2);
+
+% 2 pairup subjMC_MX and verb2subjMC_MX from the same sentence
+% 3 sort, so that shorter sentences are used first.
+
+
+
+% perfect match
+for k = 1:size(subjMC_RC,1)
+  % get trial with matching senlength-wordpstn pair
+  if ~isempty(find(subjMC_RC(:,X) == subjMC_MX(:,X)));
+    if k == 1
+      chosen = zeros(0,size(senRCsubjmc,2));
+      senadd = 0;
+    end
+    [chosen, senRCsubjmc, ]  = getmatch(senRCsubjmc,senMXsubjmc,k,senadd,chosen,rseed);
   end
 end
-idx1 = idx;
-senRCsubjmc = trialinfo(idx,:);
+% pseudo match
 
-% senRCverbrc
-for k = 1:numrc
-%   id             = stimuli(fname(k)).id*100 + stimuli(fname(k)).verbRC;    % actual 
-  id             = stimuli(fname(k)).id*100 + stimuli(fname(k)).RConsetword; % test run
-  tmp            = find(senRCtrl(:,7) == id);
-  if ~isempty(tmp)
-    idx(k) = tmp;
-  else 
-    idx(k) = NaN;  % trial removed during artifact rejection
-  end
+
+%% Find RCWL matches
+% perfect match
+% pseudo match
+
+%% get indices that match up to original (full) dataset
+  % index of each trial from each condition in original (full) dataset
+  [c,i1] = intersect(data.trialinfo(:,8),       subjMC_RC(:,8));       
+  [c,i2] = intersect(data.trialinfo(:,8),  verb2subjMC_RC(:,8));
+  [c,i3] = intersect(data.trialinfo(:,8),       subjMC_MX(:,8)); 
+  [c,i4] = intersect(data.trialinfo(:,8),  verb2subjMC_MX(:,8)); 
+  [c,i5] = intersect(data.trialinfo(:,8),     subjMC_RCWL(:,8)); 
+  [c,i6] = intersect(data.trialinfo(:,8),verb2subjMC_RCWL(:,8)); 
+
+  [c,subjMC_RC]        = intersect(data.trialinfo(:,8),       subjMC_RC(:,8));       
+  [c,verb2subjMC_RC]   = intersect(data.trialinfo(:,8),  verb2subjMC_RC(:,8));
+  [c,subjMC_MX]        = intersect(data.trialinfo(:,8),       subjMC_MX(:,8)); 
+  [c,verb2subjMC_MX]   = intersect(data.trialinfo(:,8),  verb2subjMC_MX(:,8)); 
+  [c,subjMC_RCWL]      = intersect(data.trialinfo(:,8),     subjMC_RCWL(:,8)); 
+  [c,verb2subjMC_RCWL] = intersect(data.trialinfo(:,8),verb2subjMC_RCWL(:,8)); 
+  
 end
-idx2 = idx;
-senRCverbrc= trialinfo(idx,:);
-
-% remove trials that don't have a subjMC or verbRC counterpart
-% crucial that i1 and i2 reflect original order of trialinfo
-i1 = find(isnan(idx1));
-i2 = find(isnan(idx2));
-
-idx1 = 
-
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% subfunction for matching %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [chosenMX, mx, rc] = getmatch(mx,rc,cnt,addlen,chosenMX,rseed)
+    randomseed(rseed);
+    tmp2 = find(mx(:,8) == rc(cnt,8)+addlen);  % get options
+    tmp2 = tmp2(randperm(size(tmp2,1)),:);     % randomly select one of possible options
+    if cnt == 1 && isempty(chosenMX)                                    
+      chosenMX  = [mx(tmp2(1),:) rc(cnt,11)]; % select MXtrial
+    else
+      chosenMX  = [chosenMX; mx(tmp2(1),:) rc(cnt,11)];  
+    end 
+    irm       = find(mx(:,10) == chosenMX(end,10));  % find used MX trial
+    mx(irm,:) = [];                                  % toss out  MX trial         
+    rc(cnt,9)  = true;                               % mark RC trial as having found match
+end 
