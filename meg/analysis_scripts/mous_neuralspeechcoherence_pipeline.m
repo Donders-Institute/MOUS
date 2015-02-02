fb816f440038a65e064ddc0880d7cb0fb7629e18
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2015-02-02 17:53:10 +0100

    updated dosensor and doaverage
    Former-commit-id: e02b7de31664c047f7b3d20725b3f904d67b912c

--- meg/analysis_scripts/mous_neuralspeechcoherence_pipeline.m
+++ meg/analysis_scripts/mous_neuralspeechcoherence_pipeline.m
@@ -11,18 +11,24 @@ suff = [num2str(tmp{1}(1)),'_',num2str(tmp{1}(2)),'Hz'];
 %% do sensor
 %  Determine which frequencies have coherence
 if dosens
-  [sentcoh, wlcoh] = mous_neuralspeechcoherence(subjectname, foi);
-  mous_db_putdata(subjectname, ['meg_coh_sensor',suff],'sentcoh','wlcoh',rootdir);
+  [sentcohAX, wlcohAX, sentcohPL, wlcohPL] = mous_neuralspeechcoherence(subjectname, foi);
+  
+  sentcoh = sencohAX;  wlcoh = whcohAX;
+  mous_db_putdata(subjectname, ['meg_coh_sensor',suff,'_axial'],'sentcoh','wlcoh',rootdir);
+  
+  sentcoh = sencohPL;  wlcoh = whcohPL;
+  mous_db_putdata(subjectname, ['meg_coh_sensor',suff,'_planar'],'sentcoh','wlcoh',rootdir);
 
 end
 
 %% do average at sensor level
 if dosensavg
   nsubj     = num2str(numel(subjectnames));
-  suff      = [suff,'_',nsubj,'subjs'];
+  filename  = ['meg_coh_sensor',suff,'_planar'];
+  cohgrpavg = mous_neuralspeechcoherence_grpavg(subjectnames, filename, rootdir);
   
-  cohgrpavg = mous_cohsensoravg(subjectnames);
-  save(['/project/3011020.09/nielam/groupresults/coh/speechenv/sensordata_speechenvcoh_',suff],...
+  suff      = [suff,'_',nsubj,'subjs'];
+  save(['/project/3011020.09/nielam/groupresults/coh/speechenvelope/sensordata_speechenvcoh_',suff],...
         'cohgrpavg','-v7.3');        
 end
 %% do source
