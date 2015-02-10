168fe5f0298d49a2bfbf92d8cf1711edca8dc0bb
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2015-02-10 14:33:03 +0100

    dosensavg updated
    Former-commit-id: 4bf6bbcb3cc5035300dc5cfe2d8fa60d245e3ac7

--- meg/analysis_scripts/mous_neuralspeechcoherence_pipeline.m
+++ meg/analysis_scripts/mous_neuralspeechcoherence_pipeline.m
@@ -1,3 +1,5 @@
+mous_neuralspeechcoherence_pipelineoptions;
+
 % This scripts allows user to toggle the options to calculate coherence
 % between the MEG signal and the speech signal
 
@@ -6,32 +8,36 @@
 % 2. phase  - not yet developed
 
 tmp  = tokenize(foi,'');
-suff = [num2str(tmp{1}(1)),'_',num2str(tmp{1}(2)),'Hz']; 
+suff = [num2str(tmp{1}(1)),'-',num2str(tmp{1}(2)),'Hz']; 
 
-%% do sensor
+%% compute sensor-level coherence
 %  Determine which frequencies have coherence
 if dosens
   [sentcohAX, wlcohAX, sentcohPL, wlcohPL] = mous_neuralspeechcoherence(subjectname, foi);
   
-  sentcoh = sencohAX;  wlcoh = whcohAX;
-  mous_db_putdata(subjectname, ['meg_coh_sensor',suff,'_axial'],'sentcoh','wlcoh',rootdir);
+  sentcoh = sentcohAX;  wlcoh = wlcohAX;
+  mous_db_putdata(subjectname, ['meg_coh_sensor_',suff,'_axial'],'sentcoh','wlcoh',rootdir);
   
-  sentcoh = sencohPL;  wlcoh = whcohPL;
-  mous_db_putdata(subjectname, ['meg_coh_sensor',suff,'_planar'],'sentcoh','wlcoh',rootdir);
+  sentcoh = sentcohPL;  wlcoh = wlcohPL;
+  mous_db_putdata(subjectname, ['meg_coh_sensor_',suff,'_planar'],'sentcoh','wlcoh',rootdir);
 
 end
 
-%% do average at sensor level
+%% average across subjects (sensor level)
+% N.B. 55 subjects have an additional sensor, 274, instead of 273 
+% fix ft_appendfreq to deal with labelcmb instead of label
 if dosensavg
-  nsubj     = num2str(numel(subjectnames));
-  filename  = ['meg_coh_sensor',suff,'_planar'];
-  cohgrpavg = mous_neuralspeechcoherence_grpavg(subjectnames, filename, rootdir);
-  
+  [subjectnames, ~ ] = mous_db_getfilename('allA','subjectname');
+  nsubj              = num2str(numel(subjectnames));
+  filename           = ['meg_coh_sensor_',suff,'_planar'];
+  [cohgrpavg]        = mous_neuralspeechcoherence_grpavg(subjectnames, filename, rootdir);
+
+  cohgrpavg = rmfield(cohgrpavg,'cfg'); % decrease memory
   suff      = [suff,'_',nsubj,'subjs'];
-  save(['/project/3011020.09/nielam/groupresults/coh/speechenvelope/sensordata_speechenvcoh_',suff],...
-        'cohgrpavg','-v7.3');        
+  save(['/project/3011020.09/nielam/groupresults/coh/speechenvelope/sensordata_coh_',suff],...
+        'cohgrpavg');        
 end
-%% do source
+%% compute source-level data
 % Call once for each frequency of interest
 if dosource
   [x]   = mous_neuralspeechcoherence_sourcedata(subjectname,foi);
