7363cf533c165aee37806b2d4e75767bd29bb6f9
Nietzsche Ho Lok Lam <nielam@mentat001.dccn.nl>
2015-02-16 19:29:36 +0100

    completed code for 2nd round of peak detection

--- meg/analysis_functions/mous_neuralspeechcoherence_peakdetect.m
+++ meg/analysis_functions/mous_neuralspeechcoherence_peakdetect.m
@@ -1,4 +1,4 @@
-%% mous_neuralspeechcoherence_peakdetect
+%%% mous_neuralspeechcoherence_peakdetect
 %  This function searches for peaks in each channel, peaks that are most frequently found
 %  across all channels (>25) are considered to be true peaks
 
@@ -24,6 +24,13 @@
       % standardization: places all frequencies on the same coherence
       % level, making the lower frequency peaks (with high coherence values)
       % more prominent.
+      % ft_preproc_standardize
+        % standardize data to have zero mean, unit SD
+        % value at each frequency is on same scale -> amplifies peaks
+
+      % ft_preproc_smooth
+        % smooths out peaks. In some cases there are frequencies side by side with
+        % prominent peaks which are smoothed into one.
 
 
 [subj,s] = mous_db_getfilename('allA','subjectname');
@@ -52,8 +59,7 @@ for k = 1:102
   fcohpeak = zeros(numchan,numel(sentcoh.freq)); % binary matrix to record pre/absence of peak
 
   % Difficult to determine subject specific threshold
-  % currently using heuristic of coherence above 0.1
-  % there is also the option to not set a threshold. 
+  % using heuristic of 0.1; option to not set a threshold. 
   thres    = 0.1;
 
   for chancnt = 1:numchan
@@ -75,21 +81,14 @@ end                        % subjloop
 
 save('/home/language/nielam/MOUS_AnalysisNotes/Coherence/coherencePeakdetect','numpeak');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% standardize and smooth at the single-subject level, then do a second round of peak detection
 %  after first round, the peaks are still not that clear
 
-% ft_preproc_standardize
-% standardize data to have zero mean, unit SD
-% value at each frequency is on same scale -> amplifies peaks
-
-% ft_preproc_smooth
-% smooths out peaks. In some cases there are frequencies side by side with
-% prominent peaks which are smoothed into one.
-
-[subj,s]         = mous_db_getfilename('allA','subjectname');
-freq             = 0:0.5:100;
-allsubjpeak      = zeros(numel(subj),numel(freq)); % exclude 0 Hz
-allsubjcohspctrm = zeros(numel(subj),numel(freq));
+[subj,s]       = mous_db_getfilename('allA','subjectname');
+freq           = 0:0.5:100;
+peakallsubj    = zeros(numel(subj),numel(freq)); % exclude 0 Hz
+cohallsubj     = zeros(numel(subj),numel(freq));
 for k = 1:102
   mous_db_getdata(subj{k},'meg_coh_sensor_0-100Hz_axial','/project/3011020.09/MEG/');
   
@@ -104,29 +103,112 @@ for k = 1:102
   end
   
   sentcoh.cohspctrm = ft_preproc_smooth(numpeak(k,:).*mean(ft_preproc_standardize(sentcoh.cohspctrm)),2); 
-  allsubjcohspctrm(k,:) = sentcoh.cohspctrm;
+  cohallsubj(k,:)   = sentcoh.cohspctrm; % smoothed and standardized data from all subjs
   
-  % allocate memory
-  peak     = zeros(1,12); % assume not more than 12 peaks detected per channel
-  val      = zeros(1,12);
-  fcohpeak = zeros(1,numel(sentcoh.freq)); % binary matrix to record pre/absence of peak
-  % use a low threshold (2)
-  % if peaks are next to each other within 1 frequency, pick max
   thres    = 2;  
-
   [p,v]  = peakdetect2NL(sentcoh.cohspctrm,thres,1); % p = peak index, v = value of peak
   tmp    = sentcoh.freq(p); % determine frequency from peak value
   if ~isempty(p)            % keep count which frequencies have peaks in a binary matrix (1 = peak)
-    fcohpeak = ismember(sentcoh.freq,tmp);
+    peakallsubj(k,:) = ismember(sentcoh.freq,tmp);
   end
-
-  if ~isempty(p)            % store all peaks and values
-    len = numel(p);
-    peak(1,1:len) = p;
-    val(1,1:len)  = v;
-  end
-  allsubjpeak(k,:)   = fcohpeak;
+    
 end                         % subj loop
+
+%% determine peaks after 2nd round
+%  Subjects with a peak within the prescribed frequency ranges, log peak
+%  i.e. not every has a peak in each frequency range
+%  H1: specific spatial topography for each frequency range; 
+%      including subjs w/o peak may dilute topography
+% delta 0   - 3.5  (0.5 - 3)
+% theta 3.5 - 7.5  (4   - 7)
+% alpha 7.5 - 12.5 (8   - 12)
+% beta  13  - 30   (13  - 30)
+% gamma 31  - 100
+
+allfreq = 0:0.5:100;
+peakfreqfirst  = nan(102,5); % matrix to store subject specific peak for each frequency range
+peakfreqsecond = nan(102,5); % use 'nan', can differentiate between subjs with no peaks vs. peak at sentcoh.freq(1) i.e. 0 Hz
+delta   = 1:7;    % indices of sentcoh.freq
+theta   = 9:15;
+alpha   = 17:25;
+beta    = 27:61;
+gamma   = 64:201; % gamma peaks are not reliable, but computed for completeness sake; descriptive data
+
+for sc = 1:102
+  for fb = 1:5
+    % assign frequency range 
+      if fb == 1
+        frange = delta;
+      elseif fb == 2
+        frange = theta;
+      elseif fb == 3
+        frange = alpha;
+      elseif fb == 4
+        frange = beta;
+      elseif fb == 5
+        frange = gamma;
+      end   
+
+      % If peak is present in frequency range, assign it to freqrange
+      % 1. max peak in one matrix
+      % 2. record subjs with >1 max peak.  
+      idx = find(peakallsubj(sc,frange));  % get data in foi
+      [v,i] = sort(idx,'descend');
+      if ~isempty(idx)
+        peakfreqfirst(sc,fb) = allfreq(frange(idx(1)));
+
+        if numel(idx) > 1              % data with >1 max peak:
+          [v,i] = sort(idx,'descend'); % i(2) is always second largest peak (assuming >2 peaks found)
+          peakfreqsecond(sc,fb) = allfreq(frange(idx(2)));
+
+        end
+      end
+  end
+end
+
+save('/home/language/nielam/MOUS_AnalysisNotes/Coherence/coherencePeakdetect_stage2','peakfreqfirst','peakfreqsecond');
+
+
+%% plot histogram of peak frequencies
+figure;subplot(5,1,1), hist(peakfreqfirst(:,1),0:0.5:3);
+subplot(5,1,2), hist(peakfreqfirst(:,2),4:0.5:7);
+subplot(5,1,3), hist(peakfreqfirst(:,3),8:0.5:12);
+subplot(5,1,4), hist(peakfreqfirst(:,4),13:0.5:30);
+subplot(5,1,5), hist(peakfreqfirst(:,5),31:0.5:100);
+
+figure;subplot(5,1,1), hist(peakfreqsecond(:,1),0:0.5:3);
+subplot(5,1,2), hist(peakfreqsecond(:,2),4:0.5:7);
+subplot(5,1,3), hist(peakfreqsecond(:,3),8:0.5:12);
+subplot(5,1,4), hist(peakfreqsecond(:,4),13:0.5:30);
+subplot(5,1,5), hist(peakfreqsecond(:,5),31:0.5:100);
+
+[subj,s] = mous_db_getfilename('allA','subjectname');
+allfreq = 0:0.5:100;
+x = 1:10
+figure; subplot(numel(x),1,1), plot(allfreq,cohallsubj(x(1),:));
+for m = x(2):numel(x)
+  subplot(numel(x),1,m), plot(allfreq,cohallsubj(m,:));
+end
+% fprintf('speech-MEG coherence frequency peaks: subj %s - %s',subj{x(1)}, subj{x(end)});
+
+s = 11:20
+figure;subplot(numel(x),1,1), plot(allfreq,cohallsubj(s(1),:));
+for m = 2:10
+  subplot(numel(x),1,m), plot(allfreq,cohallsubj(s(m),:));
+end
+
+s = 61:70
+figure;subplot(numel(x),1,1), plot(allfreq,cohallsubj(s(1),:));
+for m = 2:10
+  subplot(numel(x),1,m), plot(allfreq,cohallsubj(s(m),:));
+end
+
+s = 91:100
+figure;subplot(numel(x),1,1), plot(allfreq,cohallsubj(s(1),:));
+for m = 2:10
+  subplot(numel(x),1,m), plot(allfreq,cohallsubj(s(m),:));
+end
+
 
 %% notes
 
@@ -141,114 +223,6 @@ end                         % subj loop
 % (specific, not blobs everywhere)
 
 
-%% determine peaks after 2nd round
-%  If a peak is found within one of the prescribed frequency ranges, then
-%  log that subject to have a peak in a particular frequency range
-%  i.e. not every subject's dataset may contribute to having a peak in each frequency range
-% delta 0   - 3.5  (0.5 - 3)
-% theta 3.5 - 7.5  (4   - 7)
-% alpha 7.5 - 12.5 (8   - 12)
-% beta  13  - 30   (13  - 30)
-% gamma 31  - 100
-
-subjpeakmax = zeros(102,5); % matrix to store subject specific peak for each frequency range
-subjpeaksecond = zeros(102,5); % matrix to store subject specific peak for each frequency range
-allfreq = 0:0.5:100;
-delta   = 2:7;
-theta   = 9:15;
-alpha   = 17:25;
-beta    = 27:61;
-gamma   = 64:201;
-
-for sc = 1:102
-    for fb = 1:5
-    
-    % assign frequency range 
-    if fb == 1
-      frange = delta;
-    elseif fb == 2
-      frange = theta;
-    elseif fb == 3
-      frange = alpha;
-    elseif fb == 4
-      frange = beta;
-    elseif fb == 5
-      frange = gamma;
-    end   
-    
-    % If peak is present in frequency range, assign it to freqrange
-    % 1. max peak in one matrix
-    % 2. record subjs with >1 max peak.
-    
-    dat = find(allsubjpeak(sc,frange));  % get data in foi
-    if ~isempty(dat)
-         [~,subjpeakmax]    = max(allsubjcohspctrm(sc,frange)) % [value, idx]
-      if numel(dat > 1)
-         % get second largest value
-         [~,subjpeaksecond] = 
-    end
-    
-    end
-end
-
-
-%% determine peak after 1st round within each frequency range
-% delta 0   - 3.5  (0.5 - 3)
-% theta 3.5 - 7.5  (4   - 7)
-% alpha 7.5 - 12.5 (8   - 12)
-% beta  13  - 30   (13  - 30)
-% gamma 31  - 100
-
-subjspecpeak = zeros(102,5); % matrix to store subject specific peak for each frequency range
-allfreq = 0:0.5:100;
-delta   = 2:7;
-theta   = 9:15;
-alpha   = 17:25;
-beta    = 27:61;
-gamma   = 64:201;
-
-below25sensors = zeros(102,5); % record how many sensors for peak 
- 
-for sc = 1:102 %numel(subj)
-  for fb = 1:5
-    
-    % assign frequency range 
-    if fb == 1
-      frange = delta;
-    elseif fb == 2
-      frange = theta;
-    elseif fb == 3
-      frange = alpha;
-    elseif fb == 4
-      frange = beta;
-    elseif fb == 5
-      frange = gamma;
-    end   
-    
-    % search for peak in current frequency range
-    [count,freq] = max(numpeak(sc,frange));  % get peak found in highest number of sensors
-    if count > 25                          % if in more than 25 sensors
-     subjspecpeak(sc,fb)   = allfreq(frange(freq));            % it is subj specific peak in current frequency range
-    else % just take highest peak, but make a note
-     subjspecpeak(sc,fb)   = allfreq(frange(freq));
-     below25sensors(sc,fb) = count;
-    end
-
-  end % freqband  loop
-end   % subj loop
-
-
-figure;subplot(5,1,1), hist(subjspecpeak(:,1),0:0.5:3);
-subplot(5,1,2), hist(subjspecpeak(:,2),4:0.5:7);
-subplot(5,1,3), hist(subjspecpeak(:,3),8:0.5:12);
-subplot(5,1,4), hist(subjspecpeak(:,4),13:0.5:30);
-subplot(5,1,5), hist(subjspecpeak(:,5),31:0.5:100);
-
-
-
-
-
-%% notes
 % define trial
 % FIXME: do we need a long baseline? No, just highest value from zero
 % FIXME: vanPelt et al.(2012) excluded beginning of trial to avoid 
@@ -290,27 +264,27 @@ cfg.overlap = 0.5;
 %%% when using all sensors %%%
 % retain peaks that are in >5 sensors (and within 1 Hz of each other?)
 % leads to 0:0.5:10 all are consistent peaks
-
-numpeak = sum(fcohpeak,1);
-idx     = find(numpeak >5); 
-consistentpeak = sentcoh.freq(idx)
-
-% retina peaks that are in >10 sensors
-numpeak = sum(fcohpeak,1);
-idx     = find(numpeak >10); 
-consistentpeak = sentcoh.freq(idx)
-% axialgrad:  61 peaks; almost every peak from 0 to 11
-% planargrad:  0 0.5 1.5 2 | 4 4.5 5 5.5. 6.5 | 9 10 11.5 12 | 36.5
-
-numpeak = sum(fcohpeak,1);
-idx     = find(numpeak >20); 
-consistentpeak = sentcoh.freq(idx)
-%axial: 0 0.5 1.5 2 | 4 5.5. 6.5 7 8 9 11 12 | 21.5 22.5 26 31.5 |and more
-%olanar: 0.5 1.5  | 4  6.5 | 9 | nothing
-
-numpeak = sum(fcohpeak,1);
-idx     = find(numpeak >25); 
-consistentpeak = sentcoh.freq(idx)
+% 
+% numpeak = sum(fcohpeak,1);
+% idx     = find(numpeak >5); 
+% consistentpeak = sentcoh.freq(idx)
+% 
+% % retina peaks that are in >10 sensors
+% numpeak = sum(fcohpeak,1);
+% idx     = find(numpeak >10); 
+% consistentpeak = sentcoh.freq(idx)
+% % axialgrad:  61 peaks; almost every peak from 0 to 11
+% % planargrad:  0 0.5 1.5 2 | 4 4.5 5 5.5. 6.5 | 9 10 11.5 12 | 36.5
+% 
+% numpeak = sum(fcohpeak,1);
+% idx     = find(numpeak >20); 
+% consistentpeak = sentcoh.freq(idx)
+% %axial: 0 0.5 1.5 2 | 4 5.5. 6.5 7 8 9 11 12 | 21.5 22.5 26 31.5 |and more
+% %olanar: 0.5 1.5  | 4  6.5 | 9 | nothing
+% 
+% numpeak = sum(fcohpeak,1);
+% idx     = find(numpeak >25); 
+% consistentpeak = sentcoh.freq(idx)
 
 
 
