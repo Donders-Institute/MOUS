3a7ffafa73d7cef492a21d3562b5ef9dd8a80ce5
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2015-04-16 11:10:45 +0200

    added some functionality to do a stratification of trials in a somewhat generic way.

---
+++ meg/analysis_functions/mous_stratify.m
@@ -0,0 +1,33 @@
+function varargout = mous_stratify(cfg, varargin)
+
+% MOUS_STRATIFY stratifies the input data arguments with respect to a
+% certain quantity
+%
+% Use as [datout1, datout2] = mous_stratify(cfg, {datin1 v1}, {datin2 v2})
+%
+% Where the input data arguments should be organised in cell-array (each
+% cell having two elements, where the first element is the data structure,
+% and the second element is a vector with length numel(datain.trial), with
+% the quantity to be stratified
+
+if numel(varargin)>2
+  error('more than 2 data input arguments are not supported');
+end
+
+cfg.equalbinavg = ft_getopt(cfg, 'equalbinavg', 'no');
+
+for k = 1:numel(varargin)
+  data{k} = varargin{k}{1};
+  v{k}    = varargin{k}{2};
+  if size(v{k},1)>size(v{k},2)
+    v{k} = v{k}';
+  end
+end
+
+out = ft_stratify(cfg, v{1}, v{2});
+
+tmpcfg = [];
+tmpcfg.trials = find(isfinite(out{1}));
+varargout{1}  = ft_selectdata(tmpcfg, data{1});
+tmpcfg.trials = find(isfinite(out{2}));
+varargout{2}  = ft_selectdata(tmpcfg, data{2});
