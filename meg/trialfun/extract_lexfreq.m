3e2ac46e5cb5daeab7fb3463c86937eed3f00a80
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2015-04-16 11:10:45 +0200

    added some functionality to do a stratification of trials in a somewhat generic way.
    Former-commit-id: 3a7ffafa73d7cef492a21d3562b5ef9dd8a80ce5

---
+++ meg/trialfun/extract_lexfreq.m
@@ -0,0 +1,19 @@
+function [lexfreq] = extract_lexfreq(trialinfo)
+
+% EXTRACT_LEXFREQ extracts the lexical frequencies of the items presented,
+% as represented in the input trialinfo matrix.
+
+
+% load in the stimuli and fix the dependencies.
+stimuli = fix_dependency; % dependency fixing is not necessary, but does not hurt
+lexfreq = nan+zeros(size(trialinfo,1),1);
+
+% assign for each of the sentence words
+for k = 1:size(trialinfo,1)
+ tmp    = trialinfo(k,:);
+  sentid = tmp(6);
+  wordid = tmp(5);
+  
+  lf = [stimuli(sentid).words.lexfreq];
+  lexfreq(k) = lf(wordid);
+end
