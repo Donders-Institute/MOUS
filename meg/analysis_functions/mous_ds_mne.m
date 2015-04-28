02c3ef1d9e89cf255015c1cf150a3e4f833a08c7
Annika Hulten <annhul@mentat001.dccn.nl>
2015-04-28 17:04:21 +0200

    create average timebins
    Former-commit-id: 5bd99fec54fa5c5ed8333d60c7a3a098e11e56e7

---
+++ meg/analysis_functions/mous_ds_mne.m
@@ -0,0 +1,28 @@
+function [dataout] = mous_ds_mne(data, varargin)
+
+dataout = data;
+param = ft_getopt(varargin, 'parameter', 'avg.pow');
+range = ft_getopt(varargin, 'range', [50 550]);
+timepoints = ft_getopt(varargin, 'timepoints', 5);
+
+range = range *0.001; 
+
+tmpdat   = getsubfield(data, param);
+
+tmpout = nan(size(tmpdat,1),timepoints);
+tmptime = zeros(1,timepoints);
+
+
+for i = 1:timepoints
+    start = nearest(data.time, range(1));
+    stop = nearest(data.time, range(1)+0.1);
+    tmpout(:,i) = nanmean(tmpdat(:,start:stop),2);
+    tmptime(1,i) = nanmean(data.time(start:stop));
+    range(1) = range(1) + 0.1;
+    
+end
+
+dataout   = setsubfield(dataout, param, tmpout);    
+dataout   = setsubfield(dataout, 'time', tmptime);    
+    
+end
