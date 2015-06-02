071d21bdd356d483035f53dc3ab595cac8fdc038
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2015-06-02 15:01:09 +0200

    updated section for preprocessing and varargin/out
    Former-commit-id: 224724d9b3c2ea985191a6985ae033c39b0ea2e4

--- meg/analysis_functions/mous_neuralspeechcoherence_sourcedata_freqhigh.m
+++ meg/analysis_functions/mous_neuralspeechcoherence_sourcedata_freqhigh.m
@@ -1,129 +1,205 @@
-function [coherence] = mous_neuralspeechcoherence_sourcedata_freqhigh(subjectname,gamfoi,foi,sourcerange)
+function [varargout] = mous_neuralspeechcoherence_sourcedata_freqhigh(subjectname,gamfoi,foi,sourcerange,varargin)
 % mous_neuralspeechcoherence_sourcedata_high computes coherence at the
 % source-level between the gamma enveloep and the speech signal
-% Raw data is preprocessed and bandpass filtered
+% Definition of preprocessed data includes all processing steps that are
+% common across frequencies chosen for the speech signal to use in
+% cross-frequency coupling
+  % filtering
+  % dividing data into 3 conditions (sent, wl, all)
+  % computing spatial filter
+  % combining channels (megdata and speech)
+  % cutting data into segments 
+  
 % bandpass filtered data is project into source-space,
 % calculate fourier (cross-spectra) on virtual channel data 
 % calculate coherence (Ft_connectivity) on virtual channel data
 
-% gamfoi = gamma frequencies of interest
+% gamfoi = gamma frequencies of interest. set to [30 50]
+% gamma bandpass properties
+  % cfg.bpfiltdir  = % onepass-zerophase is set in ft_preproc_bandpassfilter.m
+  % cfg.bpfiltord  = % determined in ft_preproc_bandpassfilter.m
+  % cfg.bpfiltwintype = % default is hamming 
+  % cfg.bpfiltdf   = % default width heuristic used fir_df.m
+  % cfg.hilbert    = 'abs';  % DO NOT calculate envelope at sensor-level 
+
 % foi = frequencies at which to calculate coherence between gamma envelope and speech envelope
 % NL 02-04-2015
 
-%% PREPROCESS DATA
-  % load raw data
-dataset   = mous_db_getfilename(subjectname, 'meg_raw_task');
+if nargin < 5
+  sourcedata1 = [];
+  sourcedata2 = [];
+  sourcedata3 = []; 
+elseif nargin == 7
+  sourcedata1 = varargin{1};
+  sourcedata2 = varargin{2};
+  sourcedata3 = varargin{3};
+end
 
-% define trials, remove artifacts, preprocess data
-if numel(dataset) == 1
-  mous_db_getdata(subjectname,'meg_artifact_cfg','/project/3011020.09/MEG/');
-  artfctcfg      = {cfgeog1 cfgeog2 cfgjump cfgmuscle};
-  [datpp, speech] = computedata(dataset{1}, artfctcfg);
+if isempty(sourcedata1)
+  %% PREPROCESS DATA
+    % load raw data
+  dataset   = mous_db_getfilename(subjectname, 'meg_raw_task');
 
-elseif numel(dataset) > 1
-  for k = 1:numel(dataset)
-    tmpdataset = dataset{k};
-    mous_db_getdata(subjectname, ['meg_artifact_cfg_pt',num2str(k)]);  % separate artifact cfg for each task file
-    tmpartfctcfg         = {cfgeog1 cfgeog2 cfgjump cfgmuscle};
-    [tmpdata, tmpspeech] = computedata(tmpdataset, tmpartfctcfg);
+  % define trials, remove artifacts, preprocess data
+  if numel(dataset) == 1
+    mous_db_getdata(subjectname,'meg_artifact_cfg','/project/3011020.09/MEG/');
+    artfctcfg      = {cfgeog1 cfgeog2 cfgjump cfgmuscle};
+    [datpp, speech] = computedata(dataset{1}, artfctcfg);
 
-    if k==1,
-      tmpsens1(k) = tmpdata.grad;
-      weights1(k) = numel(tmpdata.trial);
-      datpp       = tmpdata;
+  elseif numel(dataset) > 1
+    for k = 1:numel(dataset)
+      tmpdataset = dataset{k};
+      mous_db_getdata(subjectname, ['meg_artifact_cfg_pt',num2str(k)]);  % separate artifact cfg for each task file
+      tmpartfctcfg         = {cfgeog1 cfgeog2 cfgjump cfgmuscle};
+      [tmpdata, tmpspeech] = computedata(tmpdataset, tmpartfctcfg);
 
-      tmpsens2(k) = tmpdata.grad;
-      weights2(k) = numel(tmpdata.trial);
-      speech     = tmpspeech;
-    else
-      % update the sentence counter
-      tmpdata.trialinfo(:,1)  = tmpdata.trialinfo(:,1)   + datpp.trialinfo(end,1);
-      tmpsens1(k)             = tmpdata.grad;
-      weights1(k)             = numel(tmpdata.trial);
-      datpp                   = ft_appenddata([], datpp, tmpdata);
+      if k==1,
+        tmpsens1(k) = tmpdata.grad;
+        weights1(k) = numel(tmpdata.trial);
+        datpp       = tmpdata;
 
-      tmpspeech.trialinfo(:,1) = tmpspeech.trialinfo(:,1) + speech.trialinfo(end,1);
-      tmpsens2(k)             = tmpdata.grad;
-      weights2(k)             = numel(tmpdata.trial);
-      speech                 = ft_appenddata([], speech, tmpspeech);
+        tmpsens2(k) = tmpdata.grad;
+        weights2(k) = numel(tmpdata.trial);
+        speech     = tmpspeech;
+      else
+        % update the sentence counter
+        tmpdata.trialinfo(:,1)  = tmpdata.trialinfo(:,1)   + datpp.trialinfo(end,1);
+        tmpsens1(k)             = tmpdata.grad;
+        weights1(k)             = numel(tmpdata.trial);
+        datpp                   = ft_appenddata([], datpp, tmpdata);
+
+        tmpspeech.trialinfo(:,1) = tmpspeech.trialinfo(:,1) + speech.trialinfo(end,1);
+        tmpsens2(k)             = tmpdata.grad;
+        weights2(k)             = numel(tmpdata.trial);
+        speech                 = ft_appenddata([], speech, tmpspeech);
+      end
+
     end
-
+    datpp.grad   = ft_average_sens(tmpsens1, 'weights', weights1);   
+    speech.grad  = ft_average_sens(tmpsens2, 'weights', weights2);   
   end
-  datpp.grad   = ft_average_sens(tmpsens1, 'weights', weights1);   
-  speech.grad  = ft_average_sens(tmpsens2, 'weights', weights2);   
+  
+  %% bandpass gamma band activity
+
+  cfg = [];
+  cfg.bpfilter   = 'yes';    
+  cfg.bpfreq     = [gamfoi(1) gamfoi(2)];  % coupling btw 35-45; lateralization 40 - 70 from Gross et al., 2014
+  cfg.bpfilttype = 'firws';
+  datpp          = ft_preprocessing(cfg,datpp);  % Axial, preprocessed(pp) data
+
+  %% divide conditions
+    cfg = [];
+    cfg.trials = find(ismember(datpp.trialinfo(:,2),[1 5])); % sent
+    data1      = ft_selectdata(cfg,datpp); 
+    speech1    = ft_selectdata(cfg,speech);
+
+    cfg.trials = find(ismember(datpp.trialinfo(:,2),[3 7])); % WL
+    data2      = ft_selectdata(cfg,datpp); 
+    speech2    = ft_selectdata(cfg,speech);
+
+  %% compute spatial filter
+  % covariance matrix 
+    % for LCMV: preprocessing should have cfg.demean = 'yes';
+    % data covariance matrix compared to transfer matrix (forward solution)
+    % use common-filter approach
+  cfg = [];
+  cfg.covariance   = 'yes';
+  cfg.channel      = 'MEG';
+  cfg.vartrllength     = 2;    % all trial lengths
+  cfg.covariancewindow = 'all';
+  tlck                 = ft_timelockanalysis(cfg,datpp); 
+
+  % load forward model (headmodel)
+  headmodel   = mous_db_getdata(subjectname, 'meg_anatomy_headmodel');
+
+  % % load sourcemodel   (grid); stick with 5798
+  % mous_db_getdata(subjectname, 'meg_bfica_leadfield8mm', '/project/3011020.09/nielam/');
+  % load sourcemodel surfreg
+  sourcemodel = mous_db_getdata(subjectname, 'meg_anatomy_sourcemodel2D_surfreg');
+
+
+  % source reconstruct bandpass filtered gamma (lcmv; keep spatial filter)
+  cfg = [];
+  cfg.method           = 'lcmv';  % time domain, no need cfg.foi
+  cfg.vol              = headmodel;
+  cfg.grid             = sourcemodel;
+  cfg.lcmv.keepfilter  = 'yes';
+  cfg.lcmv.fixedori    = 'yes'; % project on axis with most variance using SVD
+  source               = ft_sourceanalysis(cfg, tlck);
+
+  %% project bandpass filtered data through spatial filter
+  % turn sensor-data into source-data
+  % multiply sensor-level data by source.avg.filter of each voxel (inside)
+  % aka. extract virtual-channel time-series
+
+  % sourcedata mimics raw data structure (need time,trial,label fields)
+  sourcedata1      = [];
+  sourcedata1.time = data1.time;
+  for k = sourcerange(1):sourcerange(2)
+    sourcedata1.label{k,1} = ['chan',num2str(k,'%04d')];
+  end
+  idx = cellfun(@isempty,sourcedata1.label);
+  sourcedata1.label(idx) = [];
+
+  sourcedata2 = sourcedata1; sourcedata2.time = data2.time;
+  sourcedata3 = sourcedata1; sourcedata3.time = datpp.time;
+
+  % select subset of sources
+  filt = cat(1,source.avg.filter{source.inside}); % one filter per voxel(grid): 5798 x 273datdd
+  filt = filt(sourcerange(1):sourcerange(2),:);
+
+  for trialloop = 1:length(data1.trial)
+    sourcedata1.trial{trialloop} = abs(filt*data1.trial{trialloop});   % take abs for envelope
+  end
+
+  for trialloop = 1:length(data2.trial)
+    sourcedata2.trial{trialloop} = abs(filt*data2.trial{trialloop});   % take abs for envelope
+  end
+
+  for trialloop = 1:length(datpp.trial)
+    sourcedata3.trial{trialloop} = abs(filt*datpp.trial{trialloop});   % take abs for envelope
+  end
+
+  %% combine channels 
+  sourcedata1  = ft_appenddata([],sourcedata1,speech1); % concatenate into one dataset; axial gradiometers for subj-specific frequency search
+  sourcedata2  = ft_appenddata([],sourcedata2,speech2); % concatenate into one dataset; axial gradiometers for subj-specific frequency search
+  sourcedata3  = ft_appenddata([],sourcedata3,speech); % concatenate into one dataset; axial gradiometers for subj-specific frequency search
+
+
+  %% cut the data into fragments with overlap (increase data - like welch method)
+  cfg = [];
+  cfg.length      = 2;  
+  cfg.overlap     = 0.5; % 0 to 1 (exclusive)
+  sourcedata1 = ft_redefinetrial(cfg, sourcedata1);
+  sourcedata2 = ft_redefinetrial(cfg, sourcedata2);
+  sourcedata3 = ft_redefinetrial(cfg, sourcedata3);
+
 end
 
-
-%% bandpass gamma band activity
-% cfg.bpfiltdir  = % onepass-zerophase is set in ft_preproc_bandpassfilter.m
-% cfg.bpfiltord  = % determined in ft_preproc_bandpassfilter.m
-% cfg.bpfiltwintype = % default is hamming 
-% cfg.bpfiltdf   = % default width heuristic used fir_df.m
-% cfg.hilbert    = 'abs';  % DO NOT calculate envelope at sensor-level 
-% gamfoi = [30 50];
-cfg = [];
-cfg.bpfilter   = 'yes';    
-cfg.bpfreq     = [gamfoi(1) gamfoi(2)];  % coupling btw 35-45; lateralization 40 - 70 from Gross et al., 2014
-cfg.bpfilttype = 'firws';
-datpp          = ft_preprocessing(cfg,datpp);  % Axial, preprocessed(pp) data
-
-
-%% compute covariance matrix for LCMV
-% for LCMV: preprocessing should have cfg.demean = 'yes';
-% data covariance matrix compared to transfer matrix (forward solution)
-cfg = [];
-cfg.covariance   = 'yes';
-cfg.channel      = 'MEG';
-cfg.vartrllength = 2;    % all trial lengths
-cfg.covariancewindow = 'all';
-tlck                 = ft_timelockanalysis(cfg,datpp);
-
-% load forward model (headmodel)
-headmodel   = mous_db_getdata(subjectname, 'meg_anatomy_headmodel');
-
-% load sourcemodel   (grid); stick with 5798
-mous_db_getdata(subjectname, 'meg_bfica_leadfield8mm', '/project/3011020.09/nielam/');
-
-% source reconstruct bandpass filtered gamma (lcmv; keep spatial filter)
-cfg = [];
-cfg.method           = 'lcmv';  % time domain, no need cfg.foi
-cfg.vol              = headmodel;
-cfg.grid             = sourcemodel;
-cfg.lcmv.keepfilter  = 'yes';
-cfg.lcmv.fixedori    = 'yes'; % project on axis with most variance using SVD?
-source               = ft_sourceanalysis(cfg, tlck);
-
-%% project bandpass filtered data through spatial filter
-% turn sensor-data into source-data
-% multiply sensor-level data by source.avg.filter of each voxel (inside)
-% aka. extract virtual-channel time-series
-
-% sourcedata mimics raw data structure (time,trial,label)
-sourcedata = [];
-sourcedata.time = datpp.time;
-for k = sourcerange(1):sourcerange(2)
-  sourcedata.label{k,1} = ['chan',num2str(k,'%04d')];
-end
-idx = cellfun(@isempty,sourcedata.label);
-sourcedata.label(idx) = [];
-
-% select subset of sources
-filt = cat(1,source.avg.filter{source.inside}); % one filter per voxel(grid): 5798 x 273datdd
-filt = filt(sourcerange(1):sourcerange(2),:);
-for trialloop = 1:length(datpp.trial)
-     sourcedata.trial{trialloop} = abs(filt*datpp.trial{trialloop});   % take abs for envelope
-end
-
-%% combine channels 
-sourcedata  = ft_appenddata([],sourcedata,speech); % concatenate into one dataset; axial gradiometers for subj-specific frequency search
-
-%% cut the data into fragments with overlap (increase data - like welch method)
-cfg = [];
-cfg.length      = 2;  
-cfg.overlap     = 0.5; % 0 to 1 (exclusive)
-sourcedata = ft_redefinetrial(cfg, sourcedata);
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% fourier and source-localization (frequency band specific) %%%
 %% ft_freqanalysis on virtual channel data
+
+% get peakfrequency for specified frequency band
+% subj x freq range matrix
+[subj,~] = mous_db_getfilename('allA','subjectname');
+load('/home/language/nielam/MOUS_AnalysisNotes/Coherence/coherencePeakdetect_stage2_thres5_pd3');
+idx      = find(ismember(subj,subjectname));
+
+% get foi
+switch foi
+  case 'delta'
+    freqcol = 1;
+  case 'theta'
+    freqcol = 2;
+  case 'alpha'
+    freqcol = 3;
+  case 'beta'
+    freqcol = 4;
+end
+foi         = peakfreqfirst(idx,freqcol);
+
+% calculate fourier data
 cfg = [];
 cfg.method     = 'mtmfft';  % assumes stable power, but we know this isn't true
 cfg.output     = 'fourier'; % not 'powandcsd; compute csd online'
@@ -132,13 +208,33 @@ cfg.tapsmofrq  = 2;         % 2 Hz smoothing
 cfg.taper      = 'dpss';
 cfg.keeptrials = 'yes';
 cfg.channel    = {'all','-UADC003'};
-fourier        = ft_freqanalysis(cfg, sourcedata);
+fourier1        = ft_freqanalysis(cfg, sourcedata1);
+fourier2        = ft_freqanalysis(cfg, sourcedata2);
+fourier3        = ft_freqanalysis(cfg, sourcedata3);
+
+% normalize fourier output of speech with speech amplitude
+selchan = match_str(fourier1.label, {'audio_avg'});
+fourier1.fourierspctrm(:,selchan,:) = fourier1.fourierspctrm(:,selchan,:)./abs(fourier1.fourierspctrm(:,selchan,:));
+fourier2.fourierspctrm(:,selchan,:) = fourier2.fourierspctrm(:,selchan,:)./abs(fourier2.fourierspctrm(:,selchan,:));
+fourier3.fourierspctrm(:,selchan,:) = fourier3.fourierspctrm(:,selchan,:)./abs(fourier3.fourierspctrm(:,selchan,:));
+
 
 %% ft_connectivity analysis on spectral virtual channel data
 cfg = [];
 cfg.method = 'coh';
-coherence  = ft_connectivityanalysis(cfg,fourier);
+coherence1  = ft_connectivityanalysis(cfg,fourier1);
+coherence2  = ft_connectivityanalysis(cfg,fourier2);
+coherence3  = ft_connectivityanalysis(cfg,fourier3);
 
+%% return results
+varargout{1} = coherence1; % sen
+varargout{2} = coherence2; % wl 
+varargout{3} = coherence3; % all
+if nargout > 3
+  varargout{4} = sourcedata1;
+  varargout{5} = sourcedata2;
+  varargout{6} = sourcedata3;
+end
 %% place into source-level structure for visualization purposes
 %  get sourcemodel for 3D grid structure
 % mous_db_getdata(subjectname, 'meg_bfica_leadfield8mm', '/project/3011020.09/nielam/');
@@ -179,7 +275,7 @@ trl(:,3) = 0;
 trl = mous_artifact_remove(trl, dataset, artfctcfg, 'partial', 1); 
 
 %% preprocess neural data and speech audio file
-cfg.trl        = trl;
+cfg.trl        = trl(1:10,:);
 cfg.continuous = 'yes';
 cfg.demean     = 'yes';
 cfg.channel    = 'MEG';
@@ -201,7 +297,7 @@ for k = 1:numel(data.trial)
   sentid = num2str(data.trialinfo(k,5),'%03d');
   if ~strcmp(previous_sentid,sentid)
     load(fullfile('/project/3011020.09/MEG/misc/audiostimuli',['audiodata_envelope',sentid]));
-  end
+  end 
   i1 = nearest(audio.time{1},data.time{k}(1));
   i2 = nearest(audio.time{1},data.time{k}(end));
   i3 = nearest(data.time{k},audio.time{1}(1));
@@ -209,7 +305,6 @@ for k = 1:numel(data.trial)
   speech.trial{k}(2,:) = 0;
   speech.trial{k}(2,i3:i4) = audio.trial{1}(end,i1:i2);
   previous_sentid = sentid;
-  clear audio 
 end
 speech.label = [speech.label;{'audio_avg'}];
 
