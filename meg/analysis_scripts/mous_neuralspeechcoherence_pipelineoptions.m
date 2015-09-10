d60952912dfbdcd4c243d31e68bb1fb4d7be598c
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2015-09-10 15:47:21 +0200

    have more sourcelevel options

--- meg/analysis_scripts/mous_neuralspeechcoherence_pipelineoptions.m
+++ meg/analysis_scripts/mous_neuralspeechcoherence_pipelineoptions.m
@@ -1,7 +1,11 @@
 % set the defaults for the possible pipeline parts to be executed
-if ~exist('dosens',             'var'), doecg                  = 0; end  
-if ~exist('dosensavg',          'var'), dodss                  = 0; end  
-if ~exist('dosource',           'var'), dofreq                 = 0; end 
+if ~exist('dosens',              'var'), dosens                 = 0; end 
+if ~exist('dosensavg',           'var'), dosensavg              = 0; end  
+if ~exist('dosource_low',        'var'), dosource_low           = 0; end 
+if ~exist('dosource_high',       'var'), dosource_high          = 0; end 
+if ~exist('doshuffle_low',       'var'), doshuffle_low          = 0; end 
+if ~exist('dosource_low_regAC',  'var'), dosource_low_regAC     = 0; end 
+
 
 % set the rootdir variable
 if ~exist('rootdir', 'var'), rootdir = '/project/3011020.09/MEG'; end 
