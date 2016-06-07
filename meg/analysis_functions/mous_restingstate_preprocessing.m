d29f34ae5bd4b0fcde5bd590fbcff2d032bcb01a
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2016-06-07 14:50:46 +0200

    built in more preprocessing options

--- meg/analysis_functions/mous_restingstate_preprocessing.m
+++ meg/analysis_functions/mous_restingstate_preprocessing.m
@@ -9,14 +9,22 @@ if nargin<3
 else
 end
 resamplefs = ft_getopt(options, 'resamplefs', 200);
+hpfilter   = ft_getopt(options, 'hpfilter'  , 'yes');
+hpfreq     = ft_getopt(options, 'hpfreq'    , 0.5);
+hpfiltord  = ft_getopt(options, 'hpfiltord' , 2); 
 
 % get data and apply artifact rejection
 dataset   = mous_db_getfilename(subjectname, 'meg_raw_rest');
 mous_db_getdata(subjectname, 'meg_artifact_cfg_restingstate',rootdir);
 
+% deal with resting state datasets that are smaller than average (usually
+% because the recording was divided into 1 or more files)
+hdr         = ft_read_header(dataset{1});
+
 cfg          = [];
 cfg.dataset  = dataset{1};
-trl          = [2401 357600 0];
+endsample    = min(357600,hdr.nSamples*hdr.nTrials);
+trl          = [2401 endsample 0];
 trl          = mous_artifact_remove(trl, dataset{1}, {cfgeog1 cfgeog2 cfgjump cfgmuscle}, 'partial', 1); % don't do the horizontal EOG
 
 cfg            = [];
@@ -27,9 +35,9 @@ cfg.demean     = 'yes';
 cfg.channel    = 'MEG';
 %cfg.dftfilter  = 'yes';
 %cfg.dftfreq    = [50 100 150 200 250];
-cfg.hpfilter   = 'yes';
-cfg.hpfreq     = 0.5;
-cfg.hpfiltord  = 2;
+cfg.hpfilter   = hpfilter; 
+cfg.hpfreq     = hpfreq;
+cfg.hpfiltord  = hpfiltord;
 cfg.padding    = 5;
 data           = ft_preprocessing(cfg);
 cfg.channel    = {'EEG059'};
@@ -42,9 +50,10 @@ for k = 1:numel(data.trial)
 end
 
 % downsample
-cfg            = [];
-cfg.demean     = 'yes';
-cfg.detrend    = 'no';
-cfg.resamplefs = resamplefs;
-data           = ft_resampledata(cfg, data);
-ecg            = ft_resampledata(cfg, ecg);
+cfg             = [];
+cfg.demean      = 'yes';
+cfg.detrend     = 'no';
+cfg.resamplefs  = resamplefs;
+cfg.sampleindex = 'no';  % 
+data            = ft_resampledata(cfg, data);
+ecg             = ft_resampledata(cfg, ecg);
