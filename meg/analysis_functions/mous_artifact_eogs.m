3e8304e03fcf66d280312ec4fd66f206f4981a17
Nietzsche Ho Lok Lam <nielam@mentat002.dccn.nl>
2016-06-07 14:55:01 +0200

    updates
    Former-commit-id: a211dbe84ab21efb90e1a4f57c089c2f22acb361

--- meg/analysis_functions/mous_artifact_eogs.m
+++ meg/analysis_functions/mous_artifact_eogs.m
@@ -2,7 +2,7 @@ function [cfgout] = mous_artifact_eogs(filename, trl)
 
 % $Id: mous_artifact_eogs.m 44 2012-05-16 10:42:21Z jansch $
 
-% vEOG
+% hEOG
 cfg                          = [];
 cfg.trl                      = trl;
 cfg.continuous               = 'yes';
@@ -30,19 +30,25 @@ cfg    = ft_checkconfig(cfg, 'dataset2files', 'yes');
 cfgout = ft_artifact_zvalue(cfg);
 
 
-% % hEOG  - uses new algorithm which gives " boxes" in the averaged-signal
-% % cfg                          = [];
-% % cfg.trl                      = trl;
-% % cfg.continuous               = 'yes';
-% % cfg.dataset                  = filename;
-% % cfg    = ft_checkconfig(cfg, 'dataset2files', 'yes');
-% % cfg.artfctdef.zvalue.channel    = {'EEG057'};
-% % cfg.artfctdef.zvalue.cutoff     = 4;
-% % cfg.artfctdef.zvalue.fltpadding = 0;
-% % cfg.artfctdef.zvalue.trlpadding = 0.1;
-% % cfg.artfctdef.zvalue.artpadding = 0.1;
-% % cfg.artfctdef.zvalue.interactive= 'yes';
-% % cfg.artfctdef.zvalue.custom.funhandle = @mous_preproc_saccades;
-% % cfg.artfctdef.zvalue.custom.varargin = [];
+%hEOG  - uses new algorithm which gives " boxes" in the averaged-signal
+% cfg                          = [];
+% cfg.trl                      = trl;
+% cfg.continuous               = 'yes';
+% cfg.dataset                  = filename;
+% cfg    = ft_checkconfig(cfg, 'dataset2files', 'yes');
+% cfg.artfctdef.zvalue.channel    = {'EEG057'};
+% cfg.artfctdef.zvalue.cutoff     = 4;
+% cfg.artfctdef.zvalue.fltpadding = 0;
+% cfg.artfctdef.zvalue.trlpadding = 0.1;
+% cfg.artfctdef.zvalue.artpadding = 0.1;
+% cfg.artfctdef.zvalue.interactive= 'yes';
+% cfg.artfctdef.zvalue.custom.funhandle = @mous_preproc_saccades;
+% cfg.artfctdef.zvalue.custom.varargin = [];
+% 
+% cfg.artfctdef.type           = 'zvalue';
+% cfg.artfctdef.reject         = 'partial';
+% 
+% cfg    = ft_checkconfig(cfg, 'dataset2files', 'yes');
+% cfgout = ft_artifact_zvalue(cfg);
 
 
