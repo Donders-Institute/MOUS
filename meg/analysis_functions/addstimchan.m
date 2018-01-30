8e9fd459f712127e9138c9818c45a1c5f5c515dc
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-01-30 13:56:45 +0100

    ENH - fixed function

--- meg/analysis_functions/addstimchan.m
+++ meg/analysis_functions/addstimchan.m
@@ -1,4 +1,4 @@
-function dataout = addstimchan(data,modality,lags)
+function out = addstimchan(data,modality,lags)
 
 load mous_stimuli;
 
@@ -30,45 +30,37 @@ for k = 1:numel(out.time)
   end
 end
 
-out.trial = cellshift(out.trial, lags, 2, [], 'nan');
-out.time  = cellshift(out.time,  lags, 2, [], 'nan');
+if lags(1)~=0 || numel(lags)>1
+  out.trial = cellshift(out.trial, lags, 2, [], 'nan');
+  out.time  = cellshift(out.time,  lags, 2, [], 'nan');
 
-selx     = nearest(lags,0);
-out.time = cellrowselect(out.time, selx);
+  selx     = nearest(lags,0);
+  out.time = cellrowselect(out.time, selx);
+
+   
+  for k = 1:numel(out.time)
+    selx = isfinite(out.time{k});
+    out.time{k} = out.time{k}(:,selx);
+    out.trial{k} = out.trial{k}(:,selx);
+  end
+  if isequal(out.time,data.time)
+    out.time = data.time;
+  end
+  %out.fsample = data.fsample;
+  
+  for k = 1:numel(out.trial)
+    out.trial{k}(:,sum(isfinite(out.trial{k}),1)<numel(lags)) = 0;
+    out.trial{k}(:,~isfinite(data.trial{k}(1,:))) = nan;
+  end
+  
+  cfg = [];
+  cfg.method = 'acrosschannel';
+  out = ft_channelnormalise(cfg, out);
+end
 
 label = cell(numel(lags),1);
-for k = 1:numel(lags)
+ for k = 1:numel(lags)
   label{k} = sprintf('stim_on_lagged%0.3d',k);
 end
 out.label = label;
-
-for k = 1:numel(out.time)
-  selx = isfinite(out.time{k});
-  out.time{k} = out.time{k}(:,selx);
-  out.trial{k} = out.trial{k}(:,selx);
-end
-if isequal(out.time,data.time)
-  out.time = data.time;
-end
-out.fsample = data.fsample;
-
-for k = 1:numel(out.trial)
-  out.trial{k}(:,sum(isfinite(out.trial{k}),1)<numel(lags)) = 0;
-  out.trial{k}(:,~isfinite(data.trial{k}(1,:))) = nan;
-end
-
-cfg = [];
-cfg.method = 'acrosschannel';
-out = ft_channelnormalise(cfg, out);
-
-dataout           = ft_appenddata([],data,out);
-dataout.topolabel = cat(1, data.label, out.label);
-dataout.topo      = blkdiag(data.topo, eye(numel(lags)));
-dataout.unmixing  = blkdiag(data.unmixing, eye(numel(lags)));
-dataout.topodimord = data.topodimord;
-dataout.unmixingdimord = data.unmixingdimord;
-dataout.trialinfo = data.trialinfo;
-
-
-
-
+ 
