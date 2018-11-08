8d05f65fdc395ea73c51dca2e35a75de03699219
Sophie Arana <arana.sophie@gmail.com>
2018-11-08 15:10:25 +0100

    use implementation by allefeld available on github

--- meg/analysis_functions/mous_multisetcca_prevalence.m
+++ meg/analysis_functions/mous_multisetcca_prevalence.m
@@ -1,41 +1,34 @@
 %This function takes the max-corrected statistical maps for a given
-%scenario as well as the first-level permutation statistics and computes a 
-%prevalence statistic for each voxel across scenarios
+%scenario as well as the first-level permutation statistics and computes a
+%prevalence statistic for each voxel across scenarios using implementation from
+%Allefeld 2016
 
 function [stats,combimask,confirmmask,T,Tshuf] = mous_multisetcca_prevalence(numsce,varargin)
 
 %step 1: load in all per-scenario (first-level) statistics
-% end up with one vector containing mxtxn correlations (m is sscenarios, t 
-%is time and n is voxels) and one vector containing mxnxr correlations 
+% end up with one vector containing mxtxn correlations (m is sscenarios, t
+%is time and n is voxels) and one vector containing mxnxr correlations
 %(where r is permutation)
-load atlas_conte69_8196reg_LR_brodmann_subparc.mat
 for sce = 1:numsce
     datadir = sprintf('/project/3011020.09/jansch/mscca_group/scenario%d',sce);
     load(fullfile(datadir, sprintf('scenario%d_max_results',sce)),'T','Tshuf');
-    allT(sce,:,:)   = T;
-    allTshuf(sce,:,:,:) = Tshuf;
+    allT(sce,:,:,1)     = T;
+    allT(sce,:,:,2:501) = Tshuf;
 end
 
 %not sure how to deal with negative correlations, generally not interested
 %in those, as difficult to interpret?
 allT(allT<0)            = 0;
-allTshuf(allTshuf<0)    = 0;
-%step 2: choose the minimum correlation across scenarios per voxel and time
-%same for each permutation, if too many permutations make subselection
-mT       = squeeze(min(allT));
-mTshuf   = squeeze(min(allTshuf)); %FIXME is this correct or do I need to concat permutations??
+[s,v,t,i]               = size(allT);
+a                    = reshape(permute(allT,[2 3 1 4]),[v*t s i]);
 
-%step 3: determine uncorrected p-value for global null hypothesis at each voxel, i.e. the
-%fraction of combined-permutation values being larger or equal to the
-%actual value (1/r sum(permutation is bigger than actual value))
-[m,n,z] = size(mTshuf);
-p = sum(reshape(mTshuf,[m*n z])>mT(:),2)/z;
-p = reshape(p,[m,n]);
+[results, params] = prevalenceCore(a);
 
-%step 4: to correcct for multiple comparison,take maximum statistic across
-%voxels for each combined permutation. compute p-value for spatially
-%extended global null hypotehsis, i.e. the fraction of combined permutation
-%maximum values beging larger or equal actual value.
+%reshape resulting matrix into voxelXtime matrix
+mT      = squeeze(mean(squeeze(allT(:,:,:,1))));
+nanind  = ~isfinite(mT);
+results = structfun(@(x)reshape(x,[v t]),results,'UniformOutput',0);
+results.gamma0c(nanind) = nan;
+results.pcMN(nanind)    = nan;
+results.pcGN(nanind)    = nan;
 
-%step 5: prevalence
-%
